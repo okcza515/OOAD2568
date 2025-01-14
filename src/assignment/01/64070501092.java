449b8aaced7912fa76204cbb9cab32cbea44aa9c
okcza515 <68801197+okcza515@users.noreply.github.com>
2025-01-14 15:50:02 +0700

    1092 assignment1 push

---
+++ src/assignment/01/64070501092.java
@@ -0,0 +1,24 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class HelloTime {
+    public static void main(String[] args) {
+        System.out.println("Hello! from 64070501092!");
+
+        while (true) {
+            // Get current time
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+            String currentTime = sdf.format(new Date());
+
+            // Print current time
+            System.out.println("Current Time: " + currentTime);
+
+            // Wait for 10 seconds
+            try {
+                Thread.sleep(1000); // 1 seconds in milliseconds
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
