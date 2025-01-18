28ae02d3dede2b333b837da28e6ace3bffa7db7e
Kanasorn Sudyodbunphot <masorn10110@gmail.com>
2025-01-18 23:48:51 +0700

    feat: my assignment 1 by id 65070501067

---
+++ src/assignment/01/65070501067.java
@@ -0,0 +1,7 @@
+class KanasornHello {
+  public static void main (String[] args) {
+    System.out.println("Hello, World");
+    System.out.println("My name is Kanasorn Sudyodbunphot");
+    System.out.println("My ID is 65070501067");
+  }
+}
