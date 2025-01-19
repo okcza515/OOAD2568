72f85d9b505d3290ed6530b0e3f8c025a1af7301
kmpoppp <106766720+kmpoppp@users.noreply.github.com>
2025-01-19 19:46:33 +0700

    feat : 65070501065 assignment 1

---
+++ src/assignment/01/65070501065.java
@@ -0,0 +1,8 @@
+class HelloFromCakie {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("From cakie jaja");
+    }
+}
+
+// 65070501065 Kamolpop Poonsawat
