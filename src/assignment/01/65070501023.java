bf0d6822504f5098af9f8df36426e8bf816350dd
Thanaphol <thanaphol.iqtha@gmail.com>
2025-01-19 19:57:35 +0700

    ✨ feat: add HelloThanaphol class for assignment 1

---
+++ src/assignment/01/65070501023.java
@@ -0,0 +1,7 @@
+class HelloThanaphol {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Thanaphol Thangthaweesuk 65070501023.");
+        System.out.println("Please no bugs in whole semester.");
+    }
+}
