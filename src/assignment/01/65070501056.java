572740528627041d6e9be9dc75284af2f47941f6
Sirawit Arsanok <95339940+sirawitGOGO@users.noreply.github.com>
2025-01-20 23:41:43 +0700

    Update 65070501056.java

--- src/assignment/01/65070501056.java
+++ src/assignment/01/65070501056.java
@@ -1,5 +1,5 @@
 public class HelloFromSirawit{
-    public class main(String[] args){
+    public class void main(String[] args){
         System.out.printIn("Hello World");
         System.out.printIn("I'm Sirawit Arsanok");
     }
