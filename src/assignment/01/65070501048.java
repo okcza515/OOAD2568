3b1e59ad7a626404bf6c18804378dbef11346204
Rattipong Sakunjeen <rattipong.mark@gmail.com>
2025-01-20 21:43:05 +0700

    65070501048

---
+++ src/assignment/01/65070501048.java
@@ -0,0 +1,6 @@
+class Rattipong {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("My name is Rattipong Sakunjeen 65070501048");
+    }
+}
