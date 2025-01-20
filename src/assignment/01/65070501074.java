eafaeed9659b6ffe8b4080658aff5458ec1a820f
tatamaZZ <kunung321@gmail.com>
2025-01-20 22:22:22 +0700

    Create 65070501074.java

---
+++ src/assignment/01/65070501074.java
@@ -0,0 +1,6 @@
+class Napat {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("My name is Napat Sinjindawong 65070501074");
+    }
+}
