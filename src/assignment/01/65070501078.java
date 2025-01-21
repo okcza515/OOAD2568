c6fd041f40f951ea98d88b94259826379f995e87
CowGirl1830 <nawapon.promnan0@gmail.com>
2025-01-21 15:41:55 +0700

    feat :65070501078

---
+++ src/assignment/01/65070501078.java
@@ -0,0 +1,7 @@
+package OOAD2568;
+class nawaphon {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("Nawaphon Promnan 65070501078");
+  }
+}
