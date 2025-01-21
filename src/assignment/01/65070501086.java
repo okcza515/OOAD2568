07b934e2cebaaf0dad5bb493cc2fb1be52e1cef7
Manassanan Nilruang <manatsanan.ninrueang@gmail.com>
2025-01-21 13:37:28 +0700

    feat: 65070501086 Manassanan

---
+++ src/assignment/01/65070501086.java
@@ -0,0 +1,6 @@
+class Manassanan {
+    public static void main(String[] args) {
+        System.out.println("Hello Kub Phom !!");
+        System.out.println("I'm Manassanan 65070501086 !");
+    }
+}
