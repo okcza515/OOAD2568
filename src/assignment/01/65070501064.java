b447e68a71cee9183333956281417be3c1a8388b
wavekanit <wavekanit@hotmail.com>
2025-01-21 13:29:36 +0700

    feat: Kanit

---
+++ src/assignment/01/65070501064.java
@@ -0,0 +1,7 @@
+class WaveKanit {
+    public static void main(String[] args) {
+        System.out.println("Hello World !!");
+        System.out.println("My name is Kanit Bunyinkgool !");
+        System.out.println("My Student ID is 65070501064");
+    }
+}
