1d9addc3fc4d5fb6d570c6fe51f23e100dde2463
kasinphatspam <kasinphat@icloud.com>
2025-01-21 12:34:50 +0700

    64070501002 Assignment 1

--- src/assignment/01/64070501002.java
+++ src/assignment/01/64070501002.java
@@ -1,4 +1,4 @@
-public class Kasinphat {
+class Kasinphat {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Kasinphat Ketchom 64070501002");
