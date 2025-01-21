9de9209717fee1c026ec768554d7f1c87f122197
SK-Tonhom <sopidar25@gmail.com>
2025-01-21 10:52:37 +0700

    65070501088 Assignment1

---
+++ src/assignment/01/65070501088.java
@@ -0,0 +1,6 @@
+class HiSopida {
+    public static void main(String[] args) {
+        System.out.println("Hello Tonhom!");
+        System.out.println("65070501088 Sopida Keawjongkool");
+    }
+}
