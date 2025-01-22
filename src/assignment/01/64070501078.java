b0c87e1e57aadb265e9a178a975f46de971cdd58
cinnamonjs <p.thunwattanakul@gmail.com>
2025-01-22 20:08:13 +0700

    feat: 64070501078

---
+++ src/assignment/01/64070501078.java
@@ -0,0 +1,6 @@
+class Phutsakorn {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Phutsakorn Thunwattanakul 64070501078");
+    }
+}
