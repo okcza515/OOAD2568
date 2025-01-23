08194b8f765f3a044489bbf616999bb4447511e0
NattyHam <pnbookclub@gmail.com>
2025-01-23 22:12:38 +0700

    Add Chicken.java

---
+++ src/assignment/02/Group8/Chicken.java
@@ -0,0 +1,22 @@
+class Chicken extends Animal{
+    public Chicken(int age, String gender, int weightInLbs)
+    {
+        super(age, gender, weightInLbs);
+    }
+    @Override
+    public void eat()
+    {
+        System.out.println("The chicken is eating");
+    }
+    @Override
+    public void sleep()
+    {
+        System.out.println("The chicken is sleeping");
+    }
+    @Override
+    public void move()
+    {
+        System.out.println("The chicken is moving");
+    }
+}
+//65070503415 Natcha Trairattanasak
