b603a630198ae5b954f56b8e05e98d7ac8ffffe2
Danai Saengbuamad <danaiseangbourmad@gmail.com>
2025-01-25 22:22:04 +0700

    feat: bird class

--- src/assignment/02/Group10/Zoo.java
+++ src/assignment/02/Group10/Zoo.java
@@ -36,6 +36,24 @@ class Fish extends Animal {
 	}
   }
 
+class Bird extends Animal {
+	public Bird(int age, String gender, int weightInLbs){
+		super(age, gender, weightInLbs);
+	}
+	@Override
+	public void move(){
+		System.out.println("Bird Moving Pub Pub");
+	}
+	@Override
+	public void eat(){
+		System.out.println("Bird Eating");
+	}
+	@Override
+	public void sleep(){
+		System.out.println("Bird Sleeping zzZ");
+	}
+}
+
 public class Zoo {
 
 	public static void main(String[] args) {
