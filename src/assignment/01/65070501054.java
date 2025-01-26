c72496fa6f1ba0d909e541f3e26002a2eba2d87b
Supajit Chantamul <supajit.tttt@gmail.com>
2025-01-26 23:00:25 +0700

    Add 65070501054.java

---
+++ src/assignment/01/65070501054.java
@@ -0,0 +1,6 @@
+class Supajit {
+    public static void main(String[] args) {
+        System.out.println("Name: Supajit Chantamul");
+        System.out.println("StudentID: 65070501054");
+    }
+}
