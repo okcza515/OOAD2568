f62fe90eeb87f2949a09f817d715b625655ae66a
Manassanan Nilruang <manatsanan.ninrueang@gmail.com>
2025-01-27 23:45:04 +0700

    fix: moving

--- src/assignment/02/Group12/Bird.java
+++ src/assignment/02/Group12/Bird.java
@@ -3,8 +3,9 @@ class Bird extends Animal{
     public Bird(int age, String gender, int weightInLbs) {
         super( age, gender, weightInLbs);
     }
+    @Override
     public void move() {
-        System.out.println("🕊️🕊️ Bird is flying 🕊️🕊️.");
+        System.out.println(" Bird is moving 🦅.");
     }
 
 }
