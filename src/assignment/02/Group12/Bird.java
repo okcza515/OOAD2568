47ba0392a892d7721220d46edf7e536317fa3951
Manassanan Nilruang <manatsanan.ninrueang@gmail.com>
2025-01-27 23:38:00 +0700

    feat: add Bird class

---
+++ src/assignment/02/Group12/Bird.java
@@ -0,0 +1,10 @@
+// Manassanan Nilruang 65070501086
+class Bird extends Animal{
+    public Bird(int age, String gender, int weightInLbs) {
+        super( age, gender, weightInLbs);
+    }
+    public void move() {
+        System.out.println("🕊️🕊️ Bird is flying 🕊️🕊️.");
+    }
+
+}
