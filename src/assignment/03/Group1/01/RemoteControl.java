9993c43c64a552ca4096065c42ad7a0bc4191b6c
Patnakub <thanapatthaiking@gmail.com>
2025-01-28 17:24:12 +0700

    fix: แก้ตัวแปรให้เป็น caMel case

--- src/assignment/03/Group1/01/RemoteControl.java
+++ src/assignment/03/Group1/01/RemoteControl.java
@@ -1,9 +1,9 @@
 public class RemoteControl {
     IDevice device;
 
-    static public RemoteControl connectDevice(IDevice connected_device) {
+    static public RemoteControl connectDevice(IDevice connectedDevice) {
         RemoteControl remote = new RemoteControl();
-        remote.device = connected_device;
+        remote.device = connectedDevice;
         System.out.println("Connect to device " + remote.device);
         return remote;
     };
