d93fec1a2662a6906b7561c3e6b758af5fd8e3ea
Rattipong Sakunjeen <rattipong.mark@gmail.com>
2025-01-28 22:30:13 +0700

    Group5 : add Smartphone

---
+++ src/assignment/03/Group5/asm4/SmartphoneManufacturingProcess.java
@@ -0,0 +1,22 @@
+public class SmartphoneManufacturingProcess extends GenralManu{
+    @Override
+    public void assembly() {
+        System.out.println("Assembly smartphone is running");
+    }
+
+    @Override
+    public void testing() {
+        System.out.println("Testing smartphone now !!");
+    }
+
+    @Override
+    public void packaging() {
+        System.out.println("packaging smartphone in a container");
+    }
+
+    @Override
+    public void storage() {
+        System.out.println("storage smartphone pupippupip");
+    }
+}
+// 65070501048 Rattipong Sakunjeen
