bc33f7c69483dc50206345319170f6d0542373b8
kasinphatspam <kasinphat@icloud.com>
2025-01-28 17:17:51 +0700

    edit: add override tag

--- src/assignment/03/Group1/01/TV.java
+++ src/assignment/03/Group1/01/TV.java
@@ -1,9 +1,11 @@
 public class TV implements IDevice {
 
+    @Override
     public void turnOn() {
         System.out.println("TV turn on...");
     }
 
+    @Override
     public void turnOff() {
         System.out.println("TV turn off...");
     }
