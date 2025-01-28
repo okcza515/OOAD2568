02388ef1daf567379c0902e24f3a8ed450b78b78
kasinphatspam <kasinphat@icloud.com>
2025-01-28 16:53:54 +0700

    edit: add toString function

--- src/assignment/03/Group1/01/TV.java
+++ src/assignment/03/Group1/01/TV.java
@@ -1,13 +1,15 @@
 public class TV implements IDevice {
 
-    @Override
     public void turnOn() {
         System.out.println("TV turn on...");
     }
 
-    @Override
     public void turnOff() {
         System.out.println("TV turn off...");
+    }
+
+    public String toString() {
+        return "TV";
     }
 
 }
