47cc83e2ee7f7ab36051d1a6b7909b3456f469de
Ken <kenza.ke15@gmail.com>
2025-01-28 22:36:07 +0700

    Group5 : test smartphoneMenu method

--- src/assignment/03/Group5/asm4/DeviceFactory.java
+++ src/assignment/03/Group5/asm4/DeviceFactory.java
@@ -1,5 +1,11 @@
 public class DeviceFactory {
     public static void main(String[] args) {
+        GenralManu smartphone = new SmartphoneManufacturingProcess();
+        smartphone.assembly();
+        smartphone.testing();
+        smartphone.packaging();
+        smartphone.storage();
+
         GenralManu laptop = new LaptopManufacturingProcess();
         laptop.assembly();
         laptop.testing();
