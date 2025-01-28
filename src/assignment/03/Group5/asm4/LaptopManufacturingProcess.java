f277b198579b23e36b0a035f2caeb1bc9588ce93
Ken <kenza.ke15@gmail.com>
2025-01-28 22:25:21 +0700

    Group5 : add LaptopManu class

---
+++ src/assignment/03/Group5/asm4/LaptopManufacturingProcess.java
@@ -0,0 +1,21 @@
+public class LaptopManufacturingProcess extends GenralManu{
+    @Override
+    public void assembly() {
+        System.out.println("Laptop assembly method");
+    }
+
+    @Override
+    public void testing() {
+        System.out.println("Laptop testing method");
+    }
+
+    @Override
+    public void packaging() {
+        System.out.println("Laptop packaging method");
+    }
+
+    @Override
+    public void storage() {
+        System.out.println("Laptop storage method");
+    }
+}
