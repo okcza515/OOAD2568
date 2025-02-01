45d0cc9f1257bca1c93f5ccd0a263693944f0ba3
chaiyapatoam <chaiyapatoam12@gmail.com>
2025-02-01 21:32:07 +0700

    implement surroundSoundSystem in group 4

---
+++ src/assignment/03/Group4/01/SurroundSoundSystem.java
@@ -0,0 +1,11 @@
+public class SurroundSoundSystem implements IDevice {
+    @Override
+    public void turnOn() {
+        System.out.println("Sound system is On. : Kreung seang perd leaw");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Sound system is Off. :Kreung seang pid leaw");
+    }
+}
