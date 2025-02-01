292ae502dd325c860719d7bbe20a88e667b7d7a4
Kanasorn Sudyodbunphot <masorn10110@gmail.com>
2025-02-01 21:51:24 +0700

    feat: add projector

---
+++ src/assignment/03/Group3/Assignment1/Projector.java
@@ -0,0 +1,12 @@
+public class Projector implements Device {
+  @Override
+  public void turnOff(){
+    System.out.println("Projector is turned Off.");
+  }
+
+  @Override
+  public void turnOn(){
+    System.out.println("Projector is turned On.");
+  }
+}
+// 65070501067 Kanasorn Sudyodbunphot 
