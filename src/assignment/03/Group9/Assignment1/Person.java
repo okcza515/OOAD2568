b82476795ff1778278c6b85a109ad149d9cb1ab7
Emma Paris <Bewantza33@gmail.com>
2025-02-01 13:41:49 +0700

    Add interface, abstract class

--- src/assignment/03/Group9/Assignment1/Person.java
+++ src/assignment/03/Group9/Assignment1/Person.java
@@ -8,3 +8,39 @@ interface VolumeControl {
     void volumeUp();
     void volumeDown();
 }
+
+// Paratthakon Suksukhon 65070503457
+// Interface Segregation Principle: Devices only implement interfaces they need
+interface BasicDevice extends PowerControl {
+    String getDeviceName();
+    boolean isOn();
+}
+// Paratthakon Suksukhon 65070503457
+// Interface for devices that support volume control
+interface AudioDevice extends BasicDevice, VolumeControl {
+    int getVolume();
+}
+// Paratthakon Suksukhon 65070503457
+// Abstract class to reduce code duplication
+abstract class AbstractDevice implements BasicDevice {
+    private boolean isOn = false;
+    private final String name;
+
+    protected AbstractDevice(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getDeviceName() {
+        return name;
+    }
+
+    @Override
+    public boolean isOn() {
+        return isOn;
+    }
+
+    protected void setOn(boolean on) {
+        this.isOn = on;
+    }
+}
