6ff8ae5182e28217ce5ff6e8822cbdfedd8477ca
Thanaphol <thanaphol.iqtha@gmail.com>
2025-02-01 22:30:27 +0700

    ✨ feat: implement ITeacherThatActuallyTeach in ScienceTeacher class

--- src/assignment/03/Group3/Assignment2/ScienceTeacher.java
+++ src/assignment/03/Group3/Assignment2/ScienceTeacher.java
@@ -1,9 +1,9 @@
-
-public class ScienceTeacher extends Teacher {
+public class ScienceTeacher extends Teacher implements ITeacherThatActuallyTeach {
 
 	@Override
 	public void teach() {
 		System.out.println("taught science");
 	}
-
 }
+
+// 65070501023 Thanaphol Thangthaweesuk
