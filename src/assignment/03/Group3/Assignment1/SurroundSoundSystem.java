7bc4a657074aa8f23382a3e6478738a10f6c5aaf
Thanaphol <thanaphol.iqtha@gmail.com>
2025-02-01 21:42:12 +0700

    ✨ feat: add SurroundSoundSystem class implementing Device interface

---
+++ src/assignment/03/Group3/Assignment1/SurroundSoundSystem.java
@@ -0,0 +1,11 @@
+public class SurroundSoundSystem implements Device {
+    @Override
+    public void turnOn() {
+        System.out.println("Surround Sound System is On.");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Surround Sound System is Off.");
+    }
+}
