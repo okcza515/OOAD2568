685935d8df8ea6eb2a198723b9d1298120fb7287
SK-Tonhom <sopidar25@gmail.com>
2025-02-01 21:45:06 +0700

    ✨ feat : TV

---
+++ src/assignment/03/Group3/Assignment1/TV.java
@@ -0,0 +1,13 @@
+public class TV implements Device {
+    @Override
+    public void turnOn() {
+      System.out.println("TV turn On");
+    }
+  
+    @Override
+    public void turnOff() {
+      System.out.println("TV  Off");
+    }
+  
+  }
+  //65070501088 Sopida Keawjongkool
