51789de516cbab9d08a6cdaa2b7158956c30d74d
ZTPG101 <110453102+ZTPG101@users.noreply.github.com>
2025-02-02 22:14:19 +0700

    Group 8: Add TV

---
+++ src/assignment/03/Group8/Assignment1/TV.java
@@ -0,0 +1,15 @@
+public class TV implements Control {
+    @Override
+    public void turnOn(){
+        System.out.println("Turn on the TV");
+    }
+    @Override
+    public void turnOff(){
+        System.out.println("Turn off the TV");
+    }
+    public void playMovie(){
+        System.out.println("A movie is played on the TV");
+    }
+}
+
+//Kittipob Borisut 65070503407
