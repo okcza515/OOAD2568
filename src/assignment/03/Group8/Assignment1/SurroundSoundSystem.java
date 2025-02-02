2bdb02281161023024adaff1bcf7be4398dabcae
Sinsorn <sinsorn.bn@gmail.com>
2025-02-02 23:47:50 +0700

    Add SurroundSoundSystem.java

--- src/assignment/03/Group8/Assignment1/SurroundSoundSystem.java
+++ src/assignment/03/Group8/Assignment1/SurroundSoundSystem.java
@@ -1,3 +1,18 @@
 public class SurroundSoundSystem implements Control{
-    
+    @Override
+    public void turnOn(){
+        System.out.println("Surround Sound System is turned on");
+    }
+    @Override
+    public void turnOff(){
+        System.out.println("Surround Sound System is turned off");
+    }
+    public void adjustVolume(){
+        System.out.println("Adjusting volume");
+    }
+    @Override
+    public void special(){
+        adjustVolume();
+    }
 }
+//Sinsorn Chaithavornkit 65070503436
