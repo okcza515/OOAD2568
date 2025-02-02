e78100ca3e0d5efd12e8850b3209ba4fd498c0cb
ThanooThanu <thanoozaa@gmail.com>
2025-02-02 23:49:46 +0700

    Group 8: Add math teacher

--- src/assignment/03/Group8/Assignment2/MathTeacher.java
+++ src/assignment/03/Group8/Assignment2/MathTeacher.java
@@ -1,3 +1,7 @@
-public class MathTeacher {
-    
+//Thanoo Thanusuttiyaporn 65070503451
+public class MathTeacher extends Teacher implements teach{
+    @Override
+    public void teach(){
+        System.out.println("Teacher is teaching Mathematic");
+    }
 }
