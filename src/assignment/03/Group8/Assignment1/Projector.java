4feab8e522d91b0020e871afd95ffacf6645b19c
ThanooThanu <thanoozaa@gmail.com>
2025-02-02 22:10:50 +0700

    Group 8: make Person

--- src/assignment/03/Group8/Assignment1/Projector.java
+++ src/assignment/03/Group8/Assignment1/Projector.java
@@ -1,3 +1,3 @@
-public class Projector {
+public class Projector implements Control {
     
 }
--- src/assignment/03/Group8/Assignment1/SurroundSoundSystem.java
+++ src/assignment/03/Group8/Assignment1/SurroundSoundSystem.java
@@ -1,3 +1,3 @@
-public class SurroundSoundSystem {
+public class SurroundSoundSystem implements Control{
     
 }
