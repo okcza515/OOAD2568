89c9cd72ae2148dc54831c178721b8ede72834bb
NattyHam <pnbookclub@gmail.com>
2025-02-02 23:07:13 +0700

    Add Projector.java

--- src/assignment/03/Group8/Assignment1/Projector.java
+++ src/assignment/03/Group8/Assignment1/Projector.java
@@ -1,3 +1,15 @@
+//Natcha Trairattanasak 65070503415
 public class Projector implements Control {
-    
+    @Override
+    public void turnOn(){
+        System.out.println("Projector is turned on");
+    }
+    @Override
+    public void turnOff(){
+        System.out.println("Projector is turned off");
+    }
+    @Override
+    public void special(){
+        System.out.println("Performing auto screen adjustment");
+    }
 }
