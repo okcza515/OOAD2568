96c386503193a514af9de9d682c4df7f02a2fe5f
NattyHam <pnbookclub@gmail.com>
2025-02-02 23:48:06 +0700

    Add SciTeacher.java

--- src/assignment/03/Group8/Assignment2/SciTeacher.java
+++ src/assignment/03/Group8/Assignment2/SciTeacher.java
@@ -1,3 +1,7 @@
-public class SciTeacher {
-    
+//Natcha Trairattanasak 65070503415
+public class SciTeacher extends Teacher implements Teach {
+    @Override
+    public void teach(){
+        System.out.println("Science teacher is teaching");
+    }
 }
