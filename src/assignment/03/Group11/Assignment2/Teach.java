1801862c24ba1b8113f50396fd6fae492d5ea381
TGRZiminiar <82890761+TGRZiminiar@users.noreply.github.com>
2025-02-02 23:34:13 +0700

    feat: init class assigment2

---
+++ src/assignment/03/Group11/Assignment2/Teach.java
@@ -0,0 +1,3 @@
+interface Teach {
+    void teach();
+}
---
+++ src/assignment/03/Group11/Assignment2/Teacher.java
@@ -0,0 +1,23 @@
+// 65070503412
+public abstract class Teacher {
+
+    public void makeAnnouncements() {
+        System.out.println("Making announcements");
+    }
+
+    public void takeAttendance() {
+        System.out.println("Taking attendance");
+    }
+
+    public void collectPaperWork() {
+        System.out.println("Collecting paperwork");
+    }
+
+    public void conductHallwayDuties() {
+        System.out.println("Conducting hallway duties");
+    }
+
+    public void performOtherResponsibilities() {
+        System.out.println("Performing other responsibilities");
+    }
+}
