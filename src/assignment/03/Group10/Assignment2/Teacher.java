d36cc9a391e2176c7a87ffe940b5a1f0c183f6e5
kongkcypw <kongkcypw@gmail.com>
2025-02-03 11:32:12 +0700

    feat assignment2: class Teacher

---
+++ src/assignment/03/Group10/Assignment2/Teacher.java
@@ -0,0 +1,25 @@
+
+public abstract class Teacher {
+
+	// Delete void Teach
+
+	private void makeAnnouncements() {
+		System.out.println("made announcements..");
+	}
+
+	private void takeAttendence() {
+		System.out.println("took attendence..");
+	}
+
+	private void collectPaperWork() {
+		System.out.println("collected paperwork..");
+	}
+
+	private void conductHallwayDuties() {
+		System.out.println("conducted hallway duties..");
+	}
+
+	// Delete void performOtherResponsibilities
+}
+
+// 65070501011 Chayapol Wongpuwarak
