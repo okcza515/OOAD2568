3834dfb1fac9bbf7d8c0009c33c1efe9a5812b88
Ginger <ginger@gmail.com>
2025-02-03 14:34:06 +0700

    Feat: Science Teacher Class

---
+++ src/assignment/03/Group2/Assignment 2/ScienceTeacher.java
@@ -0,0 +1,9 @@
+public class ScienceTeacher extends Teacher implements ITeach{
+    
+    @Override
+    public void teach() {
+        System.out.println("Teaching science...");
+    }
+}
+
+//Korawit Sritotum 65070503402
