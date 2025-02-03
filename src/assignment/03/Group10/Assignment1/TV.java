72115efb710f6f5facc54db16301b79ca0044f37
CowGirl1830 <nawapon.promnan0@gmail.com>
2025-02-03 11:56:45 +0700

    feat: add TV class

---
+++ src/assignment/03/Group10/Assignment1/TV.java
@@ -0,0 +1,10 @@
+public class TV implements Device {
+    public void turnOn() {
+        System.out.println("TV is on");
+    }
+
+    public void turnOff() {
+        System.out.println("TV is off");
+    }
+}
+//65070501078 Nawaphon Promnan
