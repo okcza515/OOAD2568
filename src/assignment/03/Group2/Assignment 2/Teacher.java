c692671293266c2044753ef9544c8df6b79ed319
Tongzkungz <tongs2004@gmail.com>
2025-02-03 14:24:58 +0700

    Create teacher class group 2

---
+++ src/assignment/03/Group2/Assignment 2/Teacher.java
@@ -0,0 +1,25 @@
+public class Teacher {
+
+    private void makeAnnoucements() {
+        System.out.println("The teacher is making an annoucement");
+    }
+
+    private void takeAttendance() {
+        System.out.println("The teacher is taking an attendance");
+    }
+
+    private void collectPaperWork() {
+        System.out.println("The teacher is collecting paperwork");
+    }
+
+    private void conductHallWayDuties() {
+        System.out.println("The teacher is conducting hallway duties");
+    }
+
+    public void performOtherResponsibilities() {
+        System.out.println("The teacher is performing other responsibilities");
+    }
+
+}
+
+//Supanut Wongtanom 65070503437
