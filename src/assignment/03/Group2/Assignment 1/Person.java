e8c5119b70de63aa2be70a33e0a6ba2290dcaadd
Ginger <ginger@gmail.com>
2025-02-03 09:45:39 +0700

    Feat: Person class

--- src/assignment/03/Group2/Assignment 1/Person.java
+++ src/assignment/03/Group2/Assignment 1/Person.java
@@ -1,1 +1,26 @@
-//this one Korawit
+public class Person{
+    public static void main(String[] args) {
+        Device tv = new TV();
+        Device projector = new Projector();
+        Device soundsystem = new SurroundSoundSystem();
+
+        RemoteControl remote = new RemoteControl();
+
+        RemoteControl.pairingDevice(tv);
+        remote.turnOn();
+        remote.turnOff();
+        System.out.println("Connected to TV");
+
+        RemoteControl.pairingDevice(soundsystem);
+        remote.turnOn();
+        remote.turnOff();
+        System.out.println("Connected to Soundsystem");
+
+        RemoteControl.pairingDevice(projector);
+        remote.turnOn();
+        remote.turnOff();
+        System.out.println("Connected to Projector");
+
+    }
+}
+//Korawit Sritotum 65070503402
