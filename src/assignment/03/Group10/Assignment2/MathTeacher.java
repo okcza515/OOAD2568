72b67e0e2bd67fd7c4fe7c2a0fe2fa5107512f9d
Danai Saengbuamad <danaiseangbourmad@gmail.com>
2025-02-03 12:17:21 +0700

    feat: MathTeacher Class

---
+++ src/assignment/03/Group10/Assignment2/MathTeacher.java
@@ -0,0 +1,9 @@
+//65070501076 ดนัย แสงบัวหมัด
+public class MathTeacher extends Teacher implements Teachable{
+
+	@Override
+	public void teach() {
+		System.out.println("Taught Math");
+	}
+    
+}
