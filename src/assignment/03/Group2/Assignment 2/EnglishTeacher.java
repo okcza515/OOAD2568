78e08b3968729779813b8380c5394c3a22bf98d1
Billy19191 <bill.ratchanon@gmail.com>
2025-02-03 14:31:15 +0700

    feat: add EnglishTeacher class with teaching function

---
+++ src/assignment/03/Group2/Assignment 2/EnglishTeacher.java
@@ -0,0 +1,7 @@
+public class EnglishTeacher extends Teacher implements ITeach  {
+	@Override
+	public void teach() {
+        System.out.println("Teacher is teaching Englick!!");
+	}
+}
+//Ratchanon Tarawan 65070503464
