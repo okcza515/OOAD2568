964ed65ba88d152a65023f04916e04af0c387002
kongkcypw <kongkcypw@gmail.com>
2025-02-03 12:07:51 +0700

    feat: add SurroundSoundSystem class

---
+++ src/assignment/03/Group10/Assignment1/SurroundSoundSystem.java
@@ -0,0 +1,9 @@
+public class SurroundSoundSystem implements Device {
+    public void turnOn() {
+        System.out.println("Surround Sound System is on");
+    }
+
+    public void turnOff() {
+        System.out.println("Surround Sound System is off");
+    }
+}
