50c4d04ab5ef2220a0973fe7d44656c9c411e42f
“mmoladaa” <tangmo.n@icloud.com>
2025-02-03 21:48:22 +0700

    feat: add ScienceTeacher class

---
+++ src/assignment/03/Group10/Assignment2/ScienceTeacher.java
@@ -0,0 +1,7 @@
+//Natlada Simasathien 65070501019
+public class ScienceTeacher extends Teacher implements Teachable {
+    @Override
+    public void teach() {
+        System.out.println("Taught Science");
+    }
+}
