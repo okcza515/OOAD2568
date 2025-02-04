c7d77601293b50d222e9b3c555751d7bddd3c88d
Puntharee Roongprasert <puntharee000@gmail.com>
2025-02-04 13:29:22 +0700

    feat : add file and edit Teacher

---
+++ src/assignment/03/Group12/Assignment2/ITeacher.java
@@ -0,0 +1,6 @@
+public interface ITeacher {
+
+    public void teach();
+}
+//65070501038 Puntharee Roongprasert
+
---
+++ src/assignment/03/Group12/Assignment2/ScienceTeacher.java
@@ -0,0 +1,9 @@
+
+public class ScienceTeacher extends Teacher {
+
+	@Override
+	public void teach() {
+		System.out.println("taught science");
+	}
+
+}
