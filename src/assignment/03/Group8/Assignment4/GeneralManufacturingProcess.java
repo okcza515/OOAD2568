b834eaab7cc7af4441fee6fdb62c9d032eefbce6
NattyHam <pnbookclub@gmail.com>
2025-02-04 17:56:49 +0700

    Group 8: Add GeneralManufacturingProcess.java

--- src/assignment/03/Group8/Assignment4/GeneralManufacturingProcess.java
+++ src/assignment/03/Group8/Assignment4/GeneralManufacturingProcess.java
@@ -1,3 +1,15 @@
 public class GeneralManufacturingProcess{
-    
+    public void assemble(){
+        System.out.println("Assembling the device");
+    }
+    public void test(){
+        System.out.println("Testing the device");
+    }
+    public void packageDevice(){
+        System.out.println("Packaging the device");
+    }
+    public void store(){
+        System.out.println("Storing the device");
+    }
 }
+
