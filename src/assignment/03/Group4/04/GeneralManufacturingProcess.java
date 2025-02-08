0be72ae2ebf9d56de24bdf9483459daa1dd9f9fb
Sorn.Udom <sorrawit.udom@kmutt.ac.th>
2025-02-08 15:20:57 +0700

    [Add] implement manufacturingProcess

--- src/assignment/03/Group4/04/GeneralManufacturingProcess.java
+++ src/assignment/03/Group4/04/GeneralManufacturingProcess.java
@@ -1,6 +1,16 @@
-abstract public class GeneralManufacturingProcess {
+abstract public class GeneralManufacturingProcess implements ManufacturingProcess{
     protected abstract void assembly();
     protected abstract void testing();
     protected abstract void packaging();
     protected abstract void storage();
+
+    @Override
+    public void manufacturingProcess(){
+        System.out.println("Gum lung rem");
+        this.assembly();
+        this.testing();
+        this.packaging();
+        this.storage();
+        System.out.println("Sed leaw ja!");
+    }
 }
