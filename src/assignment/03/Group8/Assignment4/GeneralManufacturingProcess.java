c54ddc13ea79da85c485660caef20a9504205faa
Trollface8D <82329632+Trollface8D@users.noreply.github.com>
2025-02-08 15:51:47 +0700

    Group 8: finished generalManufacturingProcess class

--- src/assignment/03/Group8/Assignment4/GeneralManufacturingProcess.java
+++ src/assignment/03/Group8/Assignment4/GeneralManufacturingProcess.java
@@ -1,15 +1,29 @@
-public class GeneralManufacturingProcess{
+abstract public class GeneralManufacturingProcess{
+    private String device;
+
+    //encapsulation
+    public GeneralManufacturingProcess(String device) {
+        this.device = device;
+    }
+    
+    public void startProcess(){
+        assemble();
+        test();
+        packageDevice();
+        store();
+    }
+
     public void assemble(){
-        System.out.println("Assembling the device");
+        System.out.println("Assembling the"+ device);
     }
     public void test(){
-        System.out.println("Testing the device");
+        System.out.println("Testing the" + device);
     }
     public void packageDevice(){
-        System.out.println("Packaging the device");
+        System.out.println("Packaging the"+ device);
     }
     public void store(){
-        System.out.println("Storing the device");
+        System.out.println("Storing the"+ device);
     }
 }
 
