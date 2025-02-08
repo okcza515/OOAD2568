0d82a290d9cee06e2a0933e2a97d8e5e723fbf89
Phakawat <dewmagic2013@gmail.com>
2025-02-08 15:17:05 +0700

    feat: add GeneralManufacturingProcess abstract class for defining manufacturing steps

---
+++ src/assignment/03/Group4/04/GeneralManufacturingProcess.java
@@ -0,0 +1,6 @@
+abstract public class GeneralManufacturingProcess {
+    protected abstract void assembly();
+    protected abstract void testing();
+    protected abstract void packaging();
+    protected abstract void storage();
+}
