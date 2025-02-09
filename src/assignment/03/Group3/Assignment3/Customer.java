36b56d7953597a2d2b4e1897d9146050f44dc41d
kmpoppp <kmpop8@gmail.com>
2025-02-09 22:11:30 +0700

    ✨ Fix Customer Class

--- src/assignment/03/Group3/Assignment3/Customer.java
+++ src/assignment/03/Group3/Assignment3/Customer.java
@@ -1,7 +1,10 @@
-
+//65070501065 Kamolpop Poonsawat
 public class Customer {
 	private String name;
-
+	//Constructor
+	public Customer(String name){
+		this.name = name;
+	}
 	public String getName() {
 		return name;
 	}
