380973d912ba617c050d3999fa5cf221c5ba918a
Just3rd <kantapong.3rd@gmail.com>
2025-02-09 23:19:35 +0700

    ✨ feat: init asm 4

---
+++ src/assignment/03/Group3/Assignment4/DeviceFactory.java
@@ -0,0 +1,6 @@
+
+public class DeviceFactory {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/assignment/03/Group3/Assignment4/GeneralManufacturingProcess.java
@@ -0,0 +1,20 @@
+
+public abstract class GeneralManufacturingProcess {
+
+    abstract void assembly();
+
+    abstract void testing();
+
+    abstract void packaging();
+
+    abstract void storage();
+
+    public void manufacture() {
+        System.out.println("Start building...");
+        assembly();
+        testing();
+        packaging();
+        storage();
+        System.out.println("Done!");
+    }
+}
