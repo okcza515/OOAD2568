501c3fbc7d0ef18263c9f8a6448c4ef9e098cc84
Sikares <sikares.n@gmail.com>
2025-02-09 17:53:34 +0700

    feat: add GeneralManufacturingProcess

--- src/assignment/03/Group2/Assingment 4/GeneralManufacturingProcess.java
+++ src/assignment/03/Group2/Assingment 4/GeneralManufacturingProcess.java
@@ -1,3 +1,20 @@
 public class GeneralManufacturingProcess {
     
+    public void assembly() {
+        System.out.println("Assembling the device...");
+    }
+
+    public void testing() {
+        System.out.println("Testing the device...");
+    }
+
+    public void packaging() {
+        System.out.println("Packaging the device...");
+    }
+
+    public void storage() {
+        System.out.println("Storing the device...");
+    }
 }
+
+//Sikares Nuntipatsakul 65070503439
