ebee290d1e4fea8ca0c004863fc4fa35845926cb
Kanasorn Sudyodbunphot <masorn10110@gmail.com>
2025-02-09 23:46:44 +0700

    feat: create class laptop and method

---
+++ src/assignment/03/Group3/Assignment4/LaptopManufacturingProcess.java
@@ -0,0 +1,14 @@
+class LaptopManufacturingProcess extends GeneralManufacturingProcess {
+  
+    @Override
+    void assembly() {
+        System.out.println("Laptop assembly");
+    }
+
+    @Override
+    void testing() {
+        System.out.println("Smartphone testing");
+    }
+}
+
+// 65070501067 Kanasorn Sudyodbunphot
