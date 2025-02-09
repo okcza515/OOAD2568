6de56c74883e4311a4d2859cfffbc0a74b50ef1b
Pongpon Butsrimart <wavenakab2019@gmail.com>
2025-02-09 21:57:07 +0700

    ✨ Create a Constructor on a Product class

--- src/assignment/03/Group3/Assignment3/Product.java
+++ src/assignment/03/Group3/Assignment3/Product.java
@@ -4,6 +4,11 @@ public class Product {
     private int productId;
     private String productName;
 
+    public Product(int productId, String productName) {
+        this.productId = productId;
+        this.productName = productName;
+    }
+
     public int getProductId() {
         return productId;
     }
