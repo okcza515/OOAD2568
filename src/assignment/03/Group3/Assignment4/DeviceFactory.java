a39ff75dd3a3494e5f41f3d8863d18f73f9babf6
Thanaphol <thanaphol.iqtha@gmail.com>
2025-02-09 23:41:19 +0700

    ✨ feat: DeviceFactory using SmartphoneManufacturingProcess

--- src/assignment/03/Group3/Assignment4/DeviceFactory.java
+++ src/assignment/03/Group3/Assignment4/DeviceFactory.java
@@ -2,5 +2,11 @@
 public class DeviceFactory {
 
     public static void main(String[] args) {
+        GeneralManufacturingProcess smartphoneFactory = new SmartphoneManufacturingProcess();
+
+        smartphoneFactory.manufacture();
+        System.out.println();
     }
 }
+
+// 65070501023 Thanaphol Thangthaweesuk
