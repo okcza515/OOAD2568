20b472b73f6d920d124fc96059515345d4fab176
SK-Tonhom <sopidar25@gmail.com>
2025-02-09 23:48:34 +0700

    ✨ feat: laptopFactory

--- src/assignment/03/Group3/Assignment4/DeviceFactory.java
+++ src/assignment/03/Group3/Assignment4/DeviceFactory.java
@@ -3,10 +3,13 @@ public class DeviceFactory {
 
     public static void main(String[] args) {
         GeneralManufacturingProcess smartphoneFactory = new SmartphoneManufacturingProcess();
+        GeneralManufacturingProcess laptopFactory = new LaptopManufacturingProcess();
 
         smartphoneFactory.manufacture();
         System.out.println();
+        laptopFactory.manufacturingProcess();
     }
 }
 
 // 65070501023 Thanaphol Thangthaweesuk
+// 65070501088 Sopida Keawjongkool
