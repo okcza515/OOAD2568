8fd5aec9985872b06363b92f77a2d2e0c6cf4ffb
Danai Saengbuamad <danaiseangbourmad@gmail.com>
2025-02-10 23:59:51 +0700

    feat: add CreateDevice

--- src/assignment/03/Group10/Assignment4/GeneralManufacturingProcess.java
+++ src/assignment/03/Group10/Assignment4/GeneralManufacturingProcess.java
@@ -4,4 +4,14 @@ public abstract class GeneralManufacturingProcess {
     public abstract void testing();
     public abstract void packaging();
     public abstract void storage();
+
+    // 65070501076 Danai Saengbuamad
+    public final void createDevice(){
+        System.out.println("Start Building Device Process");
+        assembly();
+        testing();
+        packaging();
+        storage();
+        System.out.println("Device Builded");
+    }
 }
