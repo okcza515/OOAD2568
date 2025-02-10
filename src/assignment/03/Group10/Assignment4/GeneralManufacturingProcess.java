ccd4b4d92fb6ca20c03c910ae2e68b19aa73f40e
Faan <rudfanno1@gmail.com>
2025-02-10 23:51:28 +0700

    feat: add General Manufactoring class

---
+++ src/assignment/03/Group10/Assignment4/GeneralManufacturingProcess.java
@@ -0,0 +1,7 @@
+public abstract class GeneralManufacturingProcess {
+    // 65070501049 Roodfan Maimahad
+    public abstract void assembly();
+    public abstract void testing();
+    public abstract void packaging();
+    public abstract void storage();
+}
