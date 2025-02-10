ac4e4033d99f6bcc963e6d7c2297cbd665647958
kongkcypw <kongkcypw@gmail.com>
2025-02-11 00:01:13 +0700

    feat: add smartphone class

---
+++ src/assignment/03/Group10/Assignment4/SmartphoneManufacturingProcess.java
@@ -0,0 +1,14 @@
+class SmartphoneManufacturingProcess extends GeneralManufacturingProcess{
+
+    // 65070501011 Chayapol Wongpuwarak
+    @Override
+    public void assembly(){
+        System.out.println("Assembly -> Smartphone");
+    }
+
+    @Override
+    public void testing(){
+        System.out.println("Testing -> Smartphone");
+    }
+
+}
