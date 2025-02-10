b5a134ba264860d5948713cc91b3f2c3389cd3c6
Danai Saengbuamad <danaiseangbourmad@gmail.com>
2025-02-11 01:02:09 +0700

    feat: add Main Class

---
+++ src/assignment/03/Group10/Assignment3/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+        Customer customer = new Customer("Chayapol");
+        List<Product> products = new ArrayList<Product>();
+        products.add(new Product(1, "T-Shirt"));
+        products.add(new Product(2, "Jeans"));
+
+        CustomerTransaction transaction = new CustomerTransaction(customer, products);
+
+        System.out.println(transaction.getName());
+        System.out.println(transaction.productBreakDown());
+        System.out.println(transaction.getDate());
+
+        AccountsReceivable accountsReceivable = new AccountsReceivable(transaction);
+
+        accountsReceivable.sendInvoice();
+        accountsReceivable.postPayment();
+
+        ReportGenerator report = new ReportGenerator(transaction);
+        report.generateReport();
+    }
+}
