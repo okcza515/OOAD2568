ad886b6ed324422cb0f24c1326d98760519f87af
BaBoon <chitsanucha.onri@gmail.com>
2025-02-11 00:12:27 +0700

    add sportcat and citycar in manufacturer

--- src/assignment/04/Group7/assign2/Manufacturer.java
+++ src/assignment/04/Group7/assign2/Manufacturer.java
@@ -1,5 +1,15 @@
 public class Manufacturer {
-    public static Car CreateSUVcar(){
+    public static Car Sportscar(){
+        CarBuilder Sportsbuilder = new Sportscar();
+        return Sportsbuilder.buildCar();
+    }
+
+    public static Car Citycar(){
+        CarBuilder Citybuilder = new Citycar();
+        return Citybuilder.buildCar();
+    }
+
+    public static Car SUVcar(){
         CarBuilder SUVbuilder = new SUVcar();
         return SUVbuilder.buildCar();
     }
