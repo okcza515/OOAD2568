5448998ccaf5412f3a4ed4e77939be29ca3812e7
TGRZiminiar <82890761+TGRZiminiar@users.noreply.github.com>
2025-02-11 00:32:19 +0700

    feat: implement smartphone

--- src/assignment/03/Group11/Assignment4/DeviceFactory.java
+++ src/assignment/03/Group11/Assignment4/DeviceFactory.java
@@ -1,7 +1,8 @@
 public class DeviceFactory {
     public static void main(String[] args) {
         GeneralManufacturingProcess LaptopProduction = new LaptopManufacturingProcess();
-
+        GeneralManufacturingProcess SmartphoneProduction = new SmartphoneManufacturingProcess();
         LaptopProduction.manufacture();
+        SmartphoneProduction.manufacture();
     }
 }
--- src/assignment/03/Group11/Assignment4/SmartphoneManufacturingProcess.java
+++ src/assignment/03/Group11/Assignment4/SmartphoneManufacturingProcess.java
@@ -1,2 +1,16 @@
-public class SmartphoneManufacturingProcess {
+// 65070503412 Chitsanupong
+public class SmartphoneManufacturingProcess extends GeneralManufacturingProcess {
+    @Override
+    protected void assembly() {
+        System.out.println("Assembling smartphone");
+    }
+    protected void testing() {
+        System.out.println("Testing smartphone");
+    }
+    protected void packaging() {
+        System.out.println("Packaging smartphone");
+    }
+    protected void storage() {
+        System.out.println("Storing smartphone");
+    }
 }
