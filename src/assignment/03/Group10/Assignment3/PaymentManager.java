e4e95b658d1ca0a156bbf030722dd867e328333f
Faan <rudfanno1@gmail.com>
2025-02-11 00:55:17 +0700

    feat: add payment manager class, customer constructor

---
+++ src/assignment/03/Group10/Assignment3/PaymentManager.java
@@ -0,0 +1,10 @@
+public class PaymentManager {
+        
+    public void prepareInvoice(){
+        System.out.println("invoice prepared...");
+    }   
+
+    public void chargeCustomer() {
+		System.out.println("charged the customer");
+	}
+}
