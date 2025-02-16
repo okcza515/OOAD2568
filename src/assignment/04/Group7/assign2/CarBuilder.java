0e800616abafca5df433a9ace599318b985cdaeb
Ken <kenza.ke15@gmail.com>
2025-02-16 20:47:07 +0700

    Group 5 : fixed wrong commit

--- src/assignment/04/Group7/assign2/CarBuilder.java
+++ src/assignment/04/Group7/assign2/CarBuilder.java
@@ -1,29 +1,35 @@
 public abstract class CarBuilder {
     Car car;
-
+  
     public void setCar(Car car) {
-        this.car = car;
-    }
-
-    public Car getCar() {
-        return car;
-    }
-
-    public abstract void createType();
+          this.car = car;
+      }
+      
+      public Car getCar() {
+          return car;
+      }
+      
+      public abstract void createType();
+    public abstract void createFuel();
     public abstract void createSeats();
     public abstract void createEngine();
-    public abstract void createTransmission();
-
-    public final Car buildCar(){
-        Car c = new Car();
-        setCar(c);
-
-        createType();;
-        createSeats();
-        createEngine();
-        createTransmission();
-
-        return c;
-    }
-
-}
+      public abstract void createTransmission();
+    public abstract void createTripComputer();
+    public abstract void createGpsNavigator();
+      
+      public final Car buildCar() {
+          Car c = new Car();
+          setCar(car);
+          createType();
+          createFuel();
+          createSeats();
+          createEngine();
+      createTransmission();
+          createTripComputer();
+      createGpsNavigator();
+  
+          return c;
+      }
+  }
+  
+  //Jaatupoj 65070501070
