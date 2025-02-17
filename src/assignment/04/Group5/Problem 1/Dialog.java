bd3e890f58677bc8d87189ab5a1d8297efe309c8
Nutchanon Boonyato <65450526+Nutchanon-Plotter@users.noreply.github.com>
2025-02-17 09:50:17 +0700

    problem 1 button.java & daialog.java nutchanon 1075

---
+++ src/assignment/04/Group5/Problem 1/Dialog.java
@@ -0,0 +1,8 @@
+public abstract class Dialog {
+    public void renderWindow() {
+        Button okButton = createButton();
+        okButton.render();
+    }
+
+    public abstract Button createButton();
+}
