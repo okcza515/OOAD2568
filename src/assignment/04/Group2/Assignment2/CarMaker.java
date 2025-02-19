8ab286de7b9c122317c279276cd3b910b0898b1e
Ginger <ginger@gmail.com>
2025-02-19 22:26:35 +0700

    Create CarMaker Class

---
+++ src/assignment/04/Group2/Assignment2/CarMaker.java
@@ -0,0 +1,10 @@
+public interface CarMaker {
+    CarMaker setCarType(Type type);
+    CarMaker setSeats(int seats);
+    CarMaker setEngine(Engine engine);
+    CarMaker setTransmission(Transmission transmission);
+    CarMaker setTripComputer(TripComputer tripComputer);
+    CarMaker setGPSNavigator(GPSNavigator gpsNavigator);
+    Car getCar();
+    
+}
