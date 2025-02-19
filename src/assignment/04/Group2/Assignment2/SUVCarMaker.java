978b1e163387132961ed7a7ad58f2b24b604ca56
Sikares <sikares.n@gmail.com>
2025-02-19 23:39:13 +0700

    feat: Add SUVCar

---
+++ src/assignment/04/Group2/Assignment2/SUVCarMaker.java
@@ -0,0 +1,51 @@
+public class SUVCarMaker implements CarMaker {
+    private Type type;
+    private int seats;
+    private Engine engine;
+    private Transmission transmission;
+    private TripComputer tripComputer;
+    private GPSNavigator gpsNavigator;
+
+    @Override
+    public CarMaker setCarType(Type type) {
+        this.type = type;
+        return this;
+    }
+
+    @Override
+    public CarMaker setSeats(int seats){
+        this.seats = seats;
+        return this;
+    }
+
+    @Override
+    public CarMaker setEngine(Engine engine){
+        this.engine = engine;
+        return this;
+    }
+
+    @Override
+    public CarMaker setTransmission(Transmission transmission){
+        this.transmission = transmission;
+        return this;
+    }
+
+    @Override
+    public CarMaker setTripComputer(TripComputer tripComputer){
+        this.tripComputer = tripComputer;
+        return this;
+    }
+
+    @Override
+    public CarMaker setGPSNavigator(GPSNavigator gpsNavigator){
+        this.gpsNavigator = gpsNavigator;
+        return this;
+    }
+
+    @Override
+    public Car getCar(){
+        return new Car(type, seats, engine, transmission, tripComputer, gpsNavigator);
+    }
+}
+
+//Sikares Nuntipatsakul 65070503439
