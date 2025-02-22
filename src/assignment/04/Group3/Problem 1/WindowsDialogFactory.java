cb5152970f998ace756a2e273464b963c2aca788
Pongpon Butsrimart <wavenakab2019@gmail.com>
2025-02-22 23:05:35 +0700

    ✨ feat: add WindowsDialogFactory implementation

---
+++ src/assignment/04/Group3/Problem 1/WindowsDialogFactory.java
@@ -0,0 +1,10 @@
+
+public class WindowsDialogFactory extends DialogFactory {
+    @Override
+    public Dialog BuildDialog() {
+        return new WindowsDialog();
+    }
+
+}
+
+// 65070501039 Pongpon Butseemart
