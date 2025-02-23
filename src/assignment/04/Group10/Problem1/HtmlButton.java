b30130acc61c2d4080c19f820eca5c75306d8636
kongkcypw <kongkcypw@gmail.com>
2025-02-24 00:02:09 +0700

    feat: HtmlButton class

---
+++ src/assignment/04/Group10/Problem1/HtmlButton.java
@@ -0,0 +1,14 @@
+// 65070501011 Chayapol Wongpuwarak
+public class HtmlButton implements Button {
+
+	@Override
+	public void render() {
+		System.out.println("<button>Test Button</button>");
+		onClick();
+	}
+
+	@Override
+	public void onClick() {
+		System.out.println("Click! Button says - 'Hello World!'");
+	}
+}
