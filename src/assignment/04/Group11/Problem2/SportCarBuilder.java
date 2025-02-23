7bebd84ac1355af1496fb51d484f680cb9682133
TGRZiminiar <82890761+TGRZiminiar@users.noreply.github.com>
2025-02-23 17:16:41 +0700

    fix: remove redundant code

--- src/assignment/04/Group11/Problem2/SportCarBuilder.java
+++ src/assignment/04/Group11/Problem2/SportCarBuilder.java
@@ -1,8 +1,6 @@
 public class SportCarBuilder extends ConcreteCarBuilder {
     public SportCarBuilder() {
         this.carType = Type.SPORTS_CAR;
-        this.tripComputer = new TripComputer();
-        this.gpsNavigator = new GPSNavigator();
     }
 }
 // 65070503412 Chitsanupong
