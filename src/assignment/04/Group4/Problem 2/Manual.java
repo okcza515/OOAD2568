f6eedf73b14b1074c3e90acb228fa92dc9123103
PANURUT SIRINAPAISAN <panurut.siri@kmutt.ac.th>
2025-02-23 15:57:48 +0700

    feat: Manual class for assignment4_2

---
+++ src/assignment/04/Group4/Problem 2/Manual.java
@@ -0,0 +1,37 @@
+
+public class Manual {
+    private final Type type;
+    private final int seats;
+    private final Engine engine;
+    private final Transmission transmission;
+    private final TripComputer tripComputer;
+    private final GPSNavigator gpsNavigator;
+
+    public Manual(Car car) {
+        this.type = car.getType();
+        this.seats = car.getSeats();
+        this.engine = car.getEngine();
+        this.transmission = car.getTransmission();
+        this.tripComputer = car.getTripComputer();
+        this.gpsNavigator = car.getGpsNavigator();
+    }
+
+    public String print() {
+        String info = "";
+        info += "Type of car: " + type + "\n";
+        info += "Count of seats: " + seats + "\n";
+        info += "Engine: volume - " + engine.getVolume() + "; mileage - " + engine.getMileage() + "\n";
+        info += "Transmission: " + transmission + "\n";
+        if (this.tripComputer != null) {
+            info += "Trip Computer: Functional" + "\n";
+        } else {
+            info += "Trip Computer: N/A" + "\n";
+        }
+        if (this.gpsNavigator != null) {
+            info += "GPS Navigator: Functional" + "\n";
+        } else {
+            info += "GPS Navigator: N/A";
+        }
+        return info;
+    }
+}
