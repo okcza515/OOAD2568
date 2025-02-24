4b5b0b3ed9e0c234a2c26d0be0406ea6e511b811
Puntharee Roongprasert <puntharee000@gmail.com>
2025-02-24 23:59:25 +0700

    feat : up again

--- src/assignment/04/Group12/Problem 2/AudioMixer.java
+++
@@ -1,9 +0,0 @@
-
-import java.io.File;
-
-public class AudioMixer {
-    public File fix(VideoFile result){
-        System.out.println("AudioMixer: fixing audio...");
-        return new File("tmp");
-    }
-}
---
+++ src/assignment/04/Group12/Problem 2/Transmission.java
@@ -0,0 +1,4 @@
+
+public enum Transmission {
+	SINGLE_SPEED, MANUAL, AUTOMATIC, SEMI_AUTOMATIC
+}
