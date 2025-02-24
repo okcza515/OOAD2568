27543e9bd6edc38ab7798989f1b699d02b4ecc68
Mickieee <jaatupoj.tuan@mail.kmutt.ac.th>
2025-02-24 20:16:27 +0700

    Update CarBuilder

--- src/assignment/04/Group7/assign2/CarBuilder.java
+++ src/assignment/04/Group7/assign2/CarBuilder.java
@@ -1,35 +1,41 @@
 public abstract class CarBuilder {
     Car car;
-  
+
     public void setCar(Car car) {
-          this.car = car;
-      }
-      
-      public Car getCar() {
-          return car;
-      }
-      
-      public abstract void createType();
+        this.car = car;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public abstract void createType();
+
     public abstract void createFuel();
+
     public abstract void createSeats();
+
     public abstract void createEngine();
-      public abstract void createTransmission();
+
+    public abstract void createTransmission();
+
     public abstract void createTripComputer();
+
     public abstract void createGpsNavigator();
-      
-      public final Car buildCar() {
-          Car c = new Car();
-          setCar(car);
-          createType();
-          createFuel();
-          createSeats();
-          createEngine();
-      createTransmission();
-          createTripComputer();
-      createGpsNavigator();
-  
-          return c;
-      }
-  }
-  
-  //Jaatupoj 65070501070
+
+    public final Car buildCar() {
+        Car c = new Car();
+        setCar(c);
+        createType();
+        createFuel();
+        createSeats();
+        createEngine();
+        createTransmission();
+        createTripComputer();
+        createGpsNavigator();
+
+        return c;
+    }
+}
+
+// Jaatupoj 65070501070
