79fdf48eb664a77e28957a35e378f27e60c9da36
“mmoladaa” <tangmo.n@icloud.com>
2025-02-24 21:35:39 +0700

    feat: add WindowDialog.java

---
+++ src/assignment/03/Group10/Assignment1/WindowsDialog.java
@@ -0,0 +1,8 @@
+//65070501019 Natlada Simasathien
+public class WindowsDialog implements DialogFactory {
+    @Override
+    public void renderWindow() {
+        WindowsButton okButton = createButton();
+        okButton.render();
+    }
+}
