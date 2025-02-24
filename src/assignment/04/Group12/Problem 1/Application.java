426d8e7436d0ca904747ecfcc4d6403b4072e1e2
Puntharee Roongprasert <puntharee000@gmail.com>
2025-02-25 00:34:13 +0700

    feat : add smt in pb1

--- src/assignment/04/Group12/Problem 1/Application.java
+++ src/assignment/04/Group12/Problem 1/Application.java
@@ -1,7 +1,8 @@
 
 public class Application {
 
-	private static HtmlDialog dialog;
+	private static IDialog dialog;
+	
 
 	public static void main(String[] args) {
 		configure();
@@ -10,12 +11,12 @@ public class Application {
 
 	static void configure() {
 		dialog = new HtmlDialog();
-		/* New code after implement Windows dialog
+		
 		if (System.getProperty("os.name").equals("Mac OS X")) {
 			dialog = new WindowsDialog();
 		} else {
 			dialog = new HtmlDialog();
-		}*/
+		}
 	}
 
 	static void runBusinessLogic() {
