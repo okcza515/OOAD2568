7f74cd4a73ae3a8738b41193d7a58cd7be56f1d3
Sinsorn <sinsorn.bn@gmail.com>
2025-03-03 03:06:25 +0700

    Group8: Implement Application

--- src/assignment/05/Group8/Problem 2/Application.java
+++ src/assignment/05/Group8/Problem 2/Application.java
@@ -1,3 +1,15 @@
 public class Application {
+    public static void main(String[] args) {
+        VideoConversionFacade converter = new VideoConversionFacade();
 
+        // Convert OGG to MP4
+        System.out.println("Application: Converting OGG to MP4...");
+        converter.convertVideo("video.ogg", "mp4");
+
+        // Convert MP4 to OGG
+        System.out.println("Application: Converting MP4 to OGG...");
+        converter.convertVideo("video.mp4", "ogg");
+
+        System.out.println("Application: Conversion process finished.");
+    }
 }
