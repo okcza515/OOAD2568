3c81b0834310265c4b60e7204b48ffdfce2bec65
okcza515 <68801197+okcza515@users.noreply.github.com>
2025-03-04 09:32:25 +0700

    add VideoFile and FileDataSource

---
+++ src/assignment/05/Group1/Problem 2/VideoFile.java
@@ -0,0 +1,22 @@
+
+public class VideoFile {
+    private String name;
+    private String codecType;
+
+    public VideoFile(String name) {
+        this.name = name;
+        this.codecType = name.substring(name.indexOf(".") + 1);
+    }
+
+    public String getCodecType() {
+        return codecType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNameWithOutCodec() {
+        return name.substring(0, name.indexOf("."));
+    }
+}
