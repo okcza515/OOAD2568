674b19d2f5e567ddb1fa6ca55ffe33119fd1a632
Patnakub <thanapatthaiking@gmail.com>
2025-03-04 12:44:04 +0700

    feat: เพิ่มไฟล์เหลือ group1 pb1

--- src/assignment/05/Group1/Problem 1/Application.java
+++ src/assignment/05/Group1/Problem 1/Application.java
@@ -9,7 +9,14 @@ public class Application {
        SquarePeg smallSqPeg = new SquarePeg(2);
        SquarePeg largeSqPeg = new SquarePeg(20);
 
-       // B put code here
+       SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
+       SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
 
+       if (hole.fits(smallSqPegAdapter)) {
+           System.out.println("Square peg w2 fits round hole r5.");
+       }
+       if (!hole.fits(largeSqPegAdapter)) {
+           System.out.println("Square peg w20 does not fit into round hole r5.");
+       }
 	}
 }
