6c768e6a7c7dbb63e88304af9d92f265dc19290d
TGRZiminiar <82890761+TGRZiminiar@users.noreply.github.com>
2025-03-08 22:47:55 +0700

    feat: implement problem 1

---
+++ src/assignment/06/Group11/Problem 1/PayByCreditCard.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PayByCreditCard implements PaymentStrategy {
+	private final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+	private CreditCard card;
+
+	public void collectPaymentDetails() {
+		try {
+			System.out.print("Enter the card number: ");
+			String number = READER.readLine();
+			System.out.print("Enter the card expiration date 'mm/yy': ");
+			String date = READER.readLine();
+			System.out.print("Enter the CVV code: ");
+			String cvv = READER.readLine();
+			card = new CreditCard(number, date, cvv);
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public boolean pay(int paymentAmount) {
+		if (cardIsPresent()) {
+			System.out.println("Paying " + paymentAmount + " using Credit Card.");
+			card.setAmount(card.getAmount() - paymentAmount);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private boolean cardIsPresent() {
+		return card != null;
+	}
+}
