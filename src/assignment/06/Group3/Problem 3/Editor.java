fe5705fcd1a007cfbd37575e1d4bd6bbc1e8d90b
Pongpon Butsrimart <wavenakab2019@gmail.com>
2025-03-09 23:06:40 +0700

    refactor: use EventManager to handle events

--- src/assignment/06/Group3/Problem 3/Editor.java
+++ src/assignment/06/Group3/Problem 3/Editor.java
@@ -3,25 +3,24 @@ import java.io.File;
 
 public class Editor {
 	private File file;
-	private LogOpenListener log;
-	private EmailNotificationListener email;
+	public EventManager events;
 	
 	public Editor(){
-		
+		this.events = new EventManager("open", "save");
 	}
 	
 	public void openFile(String filePath) {
 		this.file = new File(filePath);
-		log = new LogOpenListener(file.getName());
-		log.update("open", file);
+		events.notify("open", file);
 	}
 	
-	public void saveFile() throws Exception{
-		email = new EmailNotificationListener("admin@example.com");
+	public void saveFile() throws Exception {
 		if(this.file != null) {
-			email.update("save", file);
+			events.notify("save", file);
 		}else {
 			throw new Exception("Please open a file first.");
 		}
 	}
 }
+
+// 65070501039 Pongpon Butseemart
