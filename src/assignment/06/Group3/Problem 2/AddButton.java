086652a230752833f8bba13eb2ac4f784b0cdfca
Pongpon Butsrimart <wavenakab2019@gmail.com>
2025-03-09 22:04:11 +0700

    refactor: implement Component interface to Addbutton and DeleteButton classes

--- src/assignment/06/Group3/Problem 2/AddButton.java
+++ src/assignment/06/Group3/Problem 2/AddButton.java
@@ -2,10 +2,21 @@
 import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 
-public class AddButton extends JButton{
-	
+public class AddButton extends JButton implements Component {
+
+    private Mediator mediator
 	public AddButton() {
         super("Add");
+    }
+
+    @Override
+    public void setMediator(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    @Override
+    public String getName() {
+        return "AddButton";
     }
 	
 	@Override
@@ -14,3 +25,5 @@ public class AddButton extends JButton{
     }
 
 }
+
+// 65070501039 Pongpon Butseemart
