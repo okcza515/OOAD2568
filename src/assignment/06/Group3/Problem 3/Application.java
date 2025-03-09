3f1009206d7ac7aec2ca840c68b6ea760557bda7
Thanaphol <thanaphol.iqtha@gmail.com>
2025-03-09 22:57:54 +0700

    ✨ feat: Subscribe LogOpenListener and EmailNotificationListener to events

--- src/assignment/06/Group3/Problem 3/Application.java
+++ src/assignment/06/Group3/Problem 3/Application.java
@@ -3,6 +3,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		Editor editor = new Editor();
+        editor.events.subscribe("open", new LogOpenListener("just3rd.txt"));
+        editor.events.subscribe("save", new EmailNotificationListener("just3rd@gmail.com"));
 		try {
             editor.openFile("test.txt");
             editor.saveFile();
