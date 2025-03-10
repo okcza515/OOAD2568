91d628c8eb53c34d20017db7e794342efe56c7ad
Patnakub <thanapatthaiking@gmail.com>
2025-03-11 00:25:35 +0700

    feat: add Application class to demonstrate event handling with listeners

---
+++ src/assignment/06/Group1/Problem 3/Application.java
@@ -0,0 +1,16 @@
+public class Application {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+
+        editor.events.subscribe("open", new LogOpenListener("log.txt"));
+        editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
+        editor.events.subscribe("save", new EmailNotificationListener("manager@example.com"));
+
+        try {
+            editor.openFile("test.txt");
+            editor.saveFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
