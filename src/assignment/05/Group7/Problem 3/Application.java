18f2b803802ab1c39dbbbb069c95bcb7c1d9fb63
Boom Kanteng <boom.kanteng@hotmail.com>
2025-03-10 23:07:33 +0700

    feat: add_Application

--- src/assignment/05/Group7/Problem 3/Application.java
+++ src/assignment/05/Group7/Problem 3/Application.java
@@ -1,9 +1,26 @@
-
 public class Application {
-	public static void main(String[] args) {
-		String salaryRecords = "Name,Salary\nJohn Smith,100000\nSteven Jobs,912000";
-		FileDataSource plain = new FileDataSource("OutputDemo.txt");
-		plain.writeData(salaryRecords);
-		System.out.println(plain.readData());
-	}
+    public static void main(String[] args) {
+        String salaryRecords = "Name,Salary\nJohn Smith,100000\nSteven Jobs,912000";
+
+        // Plain File Storage
+        DataSource plain = new FileDataSource("OutputDemo.txt");
+        plain.writeData(salaryRecords);
+        System.out.println("Plain Read: \n" + plain.readData());
+
+        // Encryption Wrapper
+        DataSource encrypted = new Encryption(plain);
+        encrypted.writeData(salaryRecords);
+        System.out.println("\nEncrypted Read: \n" + encrypted.readData());
+
+        // Compression Wrapper
+        DataSource compressed = new Compression(plain);
+        compressed.writeData(salaryRecords);
+        System.out.println("\nCompressed Read: \n" + compressed.readData());
+
+        // Both Encryption and Compression
+        DataSource encryptedCompressed = new Encryption(new Compression(plain));
+        encryptedCompressed.writeData(salaryRecords);
+        System.out.println("\nEncrypted + Compressed Read: \n" + encryptedCompressed.readData());
+    }
 }
+//65070501042
