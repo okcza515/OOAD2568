67c2065dba490e246cd7de9743cf4ab6a6f893aa
PHAKAWAT RATTANASOPA <phakawat.ratt@kmutt.ac.th>
2025-03-10 23:10:46 +0700

    feat: add LogOpenListener class for logging events

---
+++ src/assignment/06/Group4/Problem 3/LogOpenListener.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class LogOpenListener implements EventListener{
+	private File log;
+	
+	public LogOpenListener(String fileName) {
+		this.log = new File(fileName);
+	}
+
+	@Override
+	public void update(String eventType, File file) {
+		System.out.println("Save to log " + log + ": Someone has performed " + eventType
+				+ " operation with the following file: " + file.getName());
+	}
+}
+
+//65070501081 Phakawat Rattanasopa
