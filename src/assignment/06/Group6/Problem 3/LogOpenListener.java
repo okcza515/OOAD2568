879226a838f857fe08509723a9d306b075bba17c
LuckyThanet <thanet.jomp@mail.kmutt.ac.th>
2025-03-10 21:30:50 +0700

    Add EmailNoti and LogOpen

--- src/assignment/06/Group6/Problem 3/LogOpenListener.java
+++ src/assignment/06/Group6/Problem 3/LogOpenListener.java
@@ -1,15 +1,15 @@
-
 import java.io.File;
 
-public class LogOpenListener {
-	private File log;
-	
-	public LogOpenListener(String fileName) {
-		this.log = new File(fileName);
-	}
-	
-	public void update(String eventType, File file) {
-		System.out.println("Save to log " + log + ": Someone has performed " + eventType
-				+ " operation with the following file: " + file.getName());
-	}
+public class LogOpenListener implements EventListener {
+    private String logFileName;
+
+    public LogOpenListener(String logFileName) {
+        this.logFileName = logFileName;
+    }
+
+    @Override
+    public void update(String eventType, File file) {
+        System.out.println("Save to log " + logFileName + ": Someone has performed " + eventType
+                + " operation with the following file: " + file.getName());
+    }
 }
