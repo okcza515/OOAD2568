dc709c7075c0f1f40afa82089b07ebd0fc0f045d
ZhiWang64070503461 <wang.zhi.0908@gmail.com>
2025-03-10 23:45:26 +0700

    Added Order to Assign 6

---
+++ src/assignment/06/Group1/Problem 1/Order.java
@@ -0,0 +1,24 @@
+public class Order {
+    private int totalCost = 0;
+    private boolean isClosed = false;
+    
+    public void processOrder(IPaymentStrategy paymentStrategy) {
+    paymentStrategy.collectPaymentDetails();
+    }
+    
+    public void setTotalCost(int cost) {
+    this.totalCost += cost;
+    }
+    
+    public int getTotalCost() {
+    return totalCost;
+    }
+    
+    public boolean isClosed() {
+    return isClosed;
+    }
+    
+    public void setClosed() {
+    isClosed = true;
+    }
+    }
