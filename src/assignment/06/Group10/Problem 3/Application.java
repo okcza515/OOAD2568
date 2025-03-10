f8bcbbcf46c756d4364d2111f9d6581add74f5e7
Pitchayuth Jampong <rofpitchayuth@gmail.com>
2025-03-10 23:57:49 +0700

    feat: "Add Eventmanager class"

--- src/assignment/06/Group10/Problem 3/Application.java
+++ src/assignment/06/Group10/Problem 3/Application.java
@@ -1,14 +1,20 @@
+//65070501079 Pitchayuth Jampong
 
 public class Application {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        EventListener logListener = new LogOpenListener("log.txt");
+        EventListener emailListener = new EmailNotificationListener("faanbodyslim@gmail.com");
 
-	public static void main(String[] args) {
-		Editor editor = new Editor();
-		try {
+        // Subscribe listeners to events
+        editor.events.subscribe("open", logListener);
+        editor.events.subscribe("save", emailListener);
+
+        try {
             editor.openFile("test.txt");
             editor.saveFile();
         } catch (Exception e) {
             e.printStackTrace();
         }
-	}
-
+    }
 }
