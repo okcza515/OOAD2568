9e571e829ada1f9d532e7a15e137cd764b742bf3
Puwadech Intong <noonemore.gg@gmail.com>
2025-03-11 00:37:24 +0700

    feat: Add EventListener and EventManager

--- src/assignment/06/Group7/Problem 3/Editor.java
+++ src/assignment/06/Group7/Problem 3/Editor.java
@@ -2,26 +2,25 @@
 import java.io.File;
 
 public class Editor {
-	private File file;
-	private LogOpenListener log;
-	private EmailNotificationListener email;
-	
-	public Editor(){
-		
-	}
-	
-	public void openFile(String filePath) {
-		this.file = new File(filePath);
-		log = new LogOpenListener(file.getName());
-		log.update("open", file);
-	}
-	
-	public void saveFile() throws Exception{
-		email = new EmailNotificationListener("admin@example.com");
-		if(this.file != null) {
-			email.update("save", file);
-		}else {
-			throw new Exception("Please open a file first.");
-		}
-	}
+	public EventManager events;
+    private File file;
+
+    public Editor() {
+        this.events = new EventManager("open", "save");
+    }
+
+    public void openFile(String filePath) {
+        this.file = new File(filePath);
+        events.notify("open", file);
+    }
+
+    public void saveFile() throws Exception {
+        if (this.file != null) {
+            events.notify("save", file);
+        } else {
+            throw new Exception("Open a file first.");
+        }
+    }
 }
+
+//65070501085
