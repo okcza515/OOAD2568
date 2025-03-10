7fd4db274b86f48b1eb9df66383775b060b69274
Mickieee <jaatupoj.tuan@mail.kmutt.ac.th>
2025-03-10 21:50:10 +0700

    Update Application Order and PayByPayPal

--- src/assignment/06/Group7/Problem 1/Order.java
+++ src/assignment/06/Group7/Problem 1/Order.java
@@ -1,25 +1,28 @@
-
 public class Order {
 	private int totalCost = 0;
 	private boolean isClosed = false;
 
-	public void processOrder(PayByPayPal payPal) {
-		payPal.collectPaymentDetails();
+	public void processOrder(PaymentStrategy strategy) {
+			strategy.collectPaymentDetails();
+	}
+
+	public boolean pay(PaymentStrategy strategy) {
+			return strategy.pay(totalCost);
 	}
 
 	public void setTotalCost(int cost) {
-		this.totalCost += cost;
+			this.totalCost += cost;
 	}
 
 	public int getTotalCost() {
-		return totalCost;
+			return totalCost;
 	}
 
 	public boolean isClosed() {
-		return isClosed;
+			return isClosed;
 	}
 
 	public void setClosed() {
-		isClosed = true;
+			isClosed = true;
 	}
 }
