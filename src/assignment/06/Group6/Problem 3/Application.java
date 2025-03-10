66bb756dc4b3e21fe668d533e060518fe0cf95de
Sirawit Arsanok <beforever369@gmail.com>
2025-03-10 21:28:07 +0700

    Add eventListener and update Editor

--- src/assignment/06/Group6/Problem 3/Application.java
+++ src/assignment/06/Group6/Problem 3/Application.java
@@ -1,14 +1,17 @@
-
 public class Application {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        
+        // Subscribe log and email listeners
+        editor.subscribe("open", new LogOpenListener("log.txt"));
+        editor.subscribe("save", new EmailNotificationListener("admin@example.com"));
+        editor.subscribe("save", new EmailNotificationListener("support@example.com")); // Additional recipient
 
-	public static void main(String[] args) {
-		Editor editor = new Editor();
-		try {
+        try {
             editor.openFile("test.txt");
             editor.saveFile();
         } catch (Exception e) {
             e.printStackTrace();
         }
-	}
-
+    }
 }
--- src/assignment/06/Group6/Problem 3/Editor.java
+++ src/assignment/06/Group6/Problem 3/Editor.java
@@ -1,27 +1,48 @@
-
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Editor {
-	private File file;
-	private LogOpenListener log;
-	private EmailNotificationListener email;
-	
-	public Editor(){
-		
-	}
-	
-	public void openFile(String filePath) {
-		this.file = new File(filePath);
-		log = new LogOpenListener(file.getName());
-		log.update("open", file);
-	}
-	
-	public void saveFile() throws Exception{
-		email = new EmailNotificationListener("admin@example.com");
-		if(this.file != null) {
-			email.update("save", file);
-		}else {
-			throw new Exception("Please open a file first.");
-		}
-	}
+    private File file;
+    private Map<String, List<EventListener>> listeners = new HashMap<>();
+
+    public Editor() {
+        listeners.put("open", new ArrayList<>());
+        listeners.put("save", new ArrayList<>());
+    }
+
+    public void subscribe(String eventType, EventListener listener) {
+        if (listeners.containsKey(eventType)) {
+            listeners.get(eventType).add(listener);
+        }
+    }
+
+    public void unsubscribe(String eventType, EventListener listener) {
+        if (listeners.containsKey(eventType)) {
+            listeners.get(eventType).remove(listener);
+        }
+    }
+
+    private void notifyListeners(String eventType) {
+        if (file != null && listeners.containsKey(eventType)) {
+            for (EventListener listener : listeners.get(eventType)) {
+                listener.update(eventType, file);
+            }
+        }
+    }
+
+    public void openFile(String filePath) {
+        this.file = new File(filePath);
+        notifyListeners("open");
+    }
+
+    public void saveFile() throws Exception {
+        if (this.file != null) {
+            notifyListeners("save");
+        } else {
+            throw new Exception("Please open a file first.");
+        }
+    }
 }
