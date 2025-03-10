ddb70e10510b0e0550203676de22f3489ac144fb
Boom Kanteng <boom.kanteng@hotmail.com>
2025-03-10 23:11:08 +0700

    feat: add_Compression

--- src/assignment/05/Group7/Problem 3/Compression.java
+++ src/assignment/05/Group7/Problem 3/Compression.java
@@ -1,50 +1,47 @@
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Base64;
 import java.util.zip.Deflater;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterInputStream;
 
-public class Compression {
-	private int compLevel = 6;
+public class CompressionDecorator extends DataSourceDecorator {
+    private int compLevel = 6;
 
-    public int getCompressionLevel() {
-        return compLevel;
+    public CompressionDecorator(DataSource source) {
+        super(source);
     }
 
-    public void setCompressionLevel(int value) {
-        compLevel = value;
+    @Override
+    public void writeData(String data) {
+        super.writeData(compress(data));
     }
 
-    private String compress(String stringData) {
-        byte[] data = stringData.getBytes();
-        try {
-            ByteArrayOutputStream bout = new ByteArrayOutputStream(512);
-            DeflaterOutputStream dos = new DeflaterOutputStream(bout, new Deflater(compLevel));
-            dos.write(data);
+    @Override
+    public String readData() {
+        return decompress(super.readData());
+    }
+
+    private String compress(String data) {
+        byte[] input = data.getBytes();
+        try (ByteArrayOutputStream bout = new ByteArrayOutputStream(512);
+             DeflaterOutputStream dos = new DeflaterOutputStream(bout, new Deflater(compLevel))) {
+            dos.write(input);
             dos.close();
-            bout.close();
             return Base64.getEncoder().encodeToString(bout.toByteArray());
         } catch (IOException ex) {
             return null;
         }
     }
 
-    private String decompress(String stringData) {
-        byte[] data = Base64.getDecoder().decode(stringData);
-        try {
-            InputStream in = new ByteArrayInputStream(data);
-            InflaterInputStream iin = new InflaterInputStream(in);
-            ByteArrayOutputStream bout = new ByteArrayOutputStream(512);
+    private String decompress(String data) {
+        byte[] input = Base64.getDecoder().decode(data);
+        try (ByteArrayInputStream in = new ByteArrayInputStream(input);
+             InflaterInputStream iin = new InflaterInputStream(in);
+             ByteArrayOutputStream bout = new ByteArrayOutputStream(512)) {
             int b;
             while ((b = iin.read()) != -1) {
                 bout.write(b);
             }
-            in.close();
-            iin.close();
-            bout.close();
             return new String(bout.toByteArray());
         } catch (IOException ex) {
             return null;
