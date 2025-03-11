21f754df2436903b66db7fa819ca85ebf5fde09d
wit03 <witjarukit@gmail.com>
2025-03-11 19:08:10 +0700

    refactor: update curriculum models

--- ModEd/curriculum/model/Curriculum.go
+++ ModEd/curriculum/model/Curriculum.go
@@ -3,9 +3,11 @@ package model
 import "ModEd/common/model"
 
 type Curriculum struct {
-	Name       string
-	StartYear  int
-	EndYear    int
-	Program    model.ProgramType
-	CourseList []Course
+	Name        string
+	StartYear   int
+	EndYear     int
+	Faculty     model.Faculty
+	Department  model.Department
+	ProgramType model.ProgramType
+	CourseList  []Course
 }
