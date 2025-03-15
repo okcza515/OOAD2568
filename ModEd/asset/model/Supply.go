aa5d489f5b5002e113a9352cb2e50abf68b27d80
Kanasorn Sudyodbunphot <masorn10110@gmail.com>
2025-03-16 02:32:56 +0700

    :bug: fix: update Supply model fields to ensure proper JSON and CSV serialization

--- ModEd/asset/model/Supply.go
+++ ModEd/asset/model/Supply.go
@@ -6,13 +6,12 @@ import (
 )
 
 type Supply struct {
-	gorm.Model
 	SupplyID    uuid.UUID      `gorm:"type:text;primaryKey" json:"supply_id" csv:"supply_id"`
-	SupplyLabel string         `gorm:"not null" json:"supply_label,omitempty" csv:"supply_label,omitempty"`
-	Description string         `gorm:"type:text" json:"description,omitempty" csv:"description,omitempty"`
-	RoomID      uuid.UUID      `gorm:"type:text;not null" json:"room_id,omitempty" csv:"room_id,omitempty"`
-	Location    string         `gorm:"type:text" json:"location,omitempty" csv:"location,omitempty"`
-	CategoryID  *uuid.UUID     `gorm:"type:text" json:"category_id,omitempty" csv:"category_id,omitempty"`
-	Quantity    int            `gorm:"not null" json:"quantity,omitempty" csv:"quantity,omitempty"`
-	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty" csv:"deleted_at,omitempty"`
+	SupplyLabel string         `gorm:"not null" json:"supply_label" csv:"supply_label"`
+	Description *string        `gorm:"type:text" json:"description" csv:"description"`
+	RoomID      uuid.UUID      `gorm:"type:text;not null" json:"room_id" csv:"room_id"`
+	Location    *string        `gorm:"type:text" json:"location" csv:"location"`
+	CategoryID  *uuid.UUID     `gorm:"type:text" json:"category_id" csv:"category_id"`
+	Quantity    int            `gorm:"not null" json:"quantity" csv:"quantity"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at" csv:"deleted_at"`
 }
