d940aa1d7652503b05dafb0c4efcb09960af7a7d
Thanaphol(WD MBP) <thanaphol.iqtha@gmail.com>
2025-03-16 00:09:02 +0700

    ✨ refactor: update SupplyLog model fields for consistency and nullability

--- ModEd/asset/model/SupplyLog.go
+++ ModEd/asset/model/SupplyLog.go
@@ -9,13 +9,12 @@ import (
 
 type SupplyLog struct {
 	gorm.Model
-	LogID       uuid.UUID       `gorm:"primaryKey;type:uuid;default:gen_random_uuid();not null;unique" json:"log_id,omitempty" csv:"log_id,omitempty"`
-	Timestamp   time.Time       `gorm:"autoCreateTime;not null" json:"timestamp,omitempty" csv:"timestamp,omitempty"`
-	RefUserID   uuid.UUID       `gorm:"type:uuid" json:"ref_user_id,omitempty" csv:"ref_user_id,omitempty"`
-	StaffUserID uuid.UUID       `gorm:"type:uuid;not null" json:"staff_user_id,omitempty" csv:"staff_user_id,omitempty"`
-	Action      SupplyLogAction `gorm:"not null" json:"action,omitempty" csv:"action,omitempty"`
-	SupplyID    uuid.UUID       `gorm:"type:uuid;not null" json:"supply_id,omitempty" csv:"supply_id,omitempty"`
-	Description string          `json:"description,omitempty" csv:"description,omitempty"`
-	Quantity    int             `json:"quantity,omitempty" csv:"quantity,omitempty"`
-	RefBorrowID uuid.UUID       `gorm:"type:uuid" json:"ref_borrow_id,omitempty" csv:"ref_borrow_id,omitempty"`
+	LogID       uuid.UUID       `gorm:"primaryKey; type:uuid; default:gen_random_uuid(); not null;unique" json:"log_id" csv:"log_id"`
+	Timestamp   time.Time       `gorm:"autoCreateTime;not null" json:"timestamp" csv:"timestamp"`
+	RefUserID   *uuid.UUID      `gorm:"type:uuid" json:"ref_user_id,omitempty" csv:"ref_user_id,omitempty"`
+	StaffUserID uuid.UUID       `gorm:"type:uuid;not null" json:"staff_user_id" csv:"staff_user_id"`
+	Action      SupplyLogAction `gorm:"not null" json:"action" csv:"action"`
+	SupplyID    uuid.UUID       `gorm:"type:uuid;not null" json:"supply_id" csv:"supply_id"`
+	Description *string         `json:"description,omitempty" csv:"description,omitempty"`
+	Quantity    int             `gorm:"not null;" json:"quantity" csv:"quantity"`
 }
