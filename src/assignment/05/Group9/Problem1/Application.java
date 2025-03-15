5044a3e087bbdc59ebeb692b2daa1d4403edf638
Mikipedia <Chayaphon.mick@gmail.com>
2025-03-15 17:16:02 +0700

    forgot to delete comment

--- src/assignment/05/Group9/Problem1/Application.java
+++ src/assignment/05/Group9/Problem1/Application.java
@@ -10,14 +10,14 @@ public class Application {
         
 // The following code is used after add SquarePeg
         
-//        SquarePeg smallSqPeg = new SquarePeg(2);
-//        SquarePeg largeSqPeg = new SquarePeg(20);
-//        
-//        if (hole.fits(smallSqPeg)) {
-//            System.out.println("Square peg w2 fits round hole r5.");
-//        }
-//        if (!hole.fits(largeSqPeg)) {
-//            System.out.println("Square peg w20 does not fit into round hole r5.");
-//        }
+        SquarePeg smallSqPeg = new SquarePeg(2);
+        SquarePeg largeSqPeg = new SquarePeg(20);
+       
+        if (hole.fits(smallSqPeg)) {
+            System.out.println("Square peg w2 fits round hole r5.");
+        }
+        if (!hole.fits(largeSqPeg)) {
+            System.out.println("Square peg w20 does not fit into round hole r5.");
+        }
 	}
 }
