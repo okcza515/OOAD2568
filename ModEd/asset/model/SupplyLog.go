c57bd4a4d73cf8caa31cc0bbb928633018ec19d7
Thanaphol(WD MBP) <thanaphol.iqtha@gmail.com>
2025-03-15 23:05:00 +0700

    ✨ feat: update SupplyLog model to use uuid.UUID for ID fields and add JSON/CSV tags

--- ModEd/asset/model/SupplyLog.go
+++ ModEd/asset/model/SupplyLog.go
@@ -3,18 +3,19 @@ package model
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type SupplyLog struct {
 	gorm.Model
-	LogID       string          `gorm:"primaryKey;type:uuid;default:gen_random_uuid();not null;unique"`
-	Timestamp   time.Time       `gorm:"autoCreateTime;not null"`
-	RefUserID   string          `gorm:"type:uuid"`
-	StaffUserID string          `gorm:"type:uuid;not null"`
-	Action      SupplyLogAction `gorm:"not null"`
-	SupplyID    string          `gorm:"type:uuid;not null"`
-	Description string
-	Quantity    int
-	RefBorrowID string `gorm:"type:uuid"`
+	LogID       uuid.UUID       `gorm:"primaryKey;type:uuid;default:gen_random_uuid();not null;unique" json:"log_id,omitempty" csv:"log_id,omitempty"`
+	Timestamp   time.Time       `gorm:"autoCreateTime;not null" json:"timestamp,omitempty" csv:"timestamp,omitempty"`
+	RefUserID   uuid.UUID       `gorm:"type:uuid" json:"ref_user_id,omitempty" csv:"ref_user_id,omitempty"`
+	StaffUserID uuid.UUID       `gorm:"type:uuid;not null" json:"staff_user_id,omitempty" csv:"staff_user_id,omitempty"`
+	Action      SupplyLogAction `gorm:"not null" json:"action,omitempty" csv:"action,omitempty"`
+	SupplyID    uuid.UUID       `gorm:"type:uuid;not null" json:"supply_id,omitempty" csv:"supply_id,omitempty"`
+	Description string          `json:"description,omitempty" csv:"description,omitempty"`
+	Quantity    int             `json:"quantity,omitempty" csv:"quantity,omitempty"`
+	RefBorrowID uuid.UUID       `gorm:"type:uuid" json:"ref_borrow_id,omitempty" csv:"ref_borrow_id,omitempty"`
 }
