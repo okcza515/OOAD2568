9b0b06e26877d69598dac9313a3599c535b379d8
Just3rd <kantapong.3rd@gmail.com>
2025-03-16 01:44:08 +0700

    :bug: fix: match sqlite datatype

--- ModEd/asset/model/InstrumentLog.go
+++ ModEd/asset/model/InstrumentLog.go
@@ -6,12 +6,12 @@ import (
 )
 
 type InstrumentLog struct {
-	LogID        uuid.UUID               `gorm:"type:uuid;default:gen_random_uuid();primaryKey;not null;unique" json:"log_id" csv:"log_id"`
+	LogID        uuid.UUID               `gorm:"type:text;primaryKey;not null;unique" json:"log_id" csv:"log_id"`
 	Timestamp    time.Time               `gorm:"type:timestamp;not null" json:"timestamp" csv:"timestamp"`
-	RefUserID    *uuid.UUID              `gorm:"type:uuid" json:"ref_user_id,omitempty" csv:"ref_user_id"`
-	StaffUserID  uuid.UUID               `gorm:"type:uuid;not null" json:"staff_user_id" csv:"staff_user_id"`
-	Action       InstrumentLogActionEnum `gorm:"type:varchar;not null" json:"action" csv:"action"`
-	InstrumentID uuid.UUID               `gorm:"type:uuid;not null" json:"instrument_id" csv:"instrument_id"`
-	Description  string                  `gorm:"type:varchar;not null" json:"description" csv:"description"`
-	RefBorrowID  *uuid.UUID              `gorm:"type:uuid" json:"ref_borrow_id,omitempty" csv:"ref_borrow_id"`
+	RefUserID    *uuid.UUID              `gorm:"type:text" json:"ref_user_id,omitempty" csv:"ref_user_id"`
+	StaffUserID  uuid.UUID               `gorm:"type:text;not null" json:"staff_user_id" csv:"staff_user_id"`
+	Action       InstrumentLogActionEnum `gorm:"type:text;not null" json:"action" csv:"action"`
+	InstrumentID uuid.UUID               `gorm:"type:text;not null" json:"instrument_id" csv:"instrument_id"`
+	Description  string                  `gorm:"type:text;not null" json:"description" csv:"description"`
+	RefBorrowID  *uuid.UUID              `gorm:"type:text" json:"ref_borrow_id,omitempty" csv:"ref_borrow_id"`
 }
--- ModEd/asset/model/InstrumentLogActionEnum.go
+++ ModEd/asset/model/InstrumentLogActionEnum.go
@@ -1,37 +1,18 @@
 package model
 
-type InstrumentLogActionEnum int
+type InstrumentLogActionEnum string
 
 const (
-	INS_LOG_ADDNEW InstrumentLogActionEnum = iota
-	INS_LOG_UPDATE
-	INS_LOG_BORROW
-	INS_LOG_RETURN
-	INS_LOG_MOVE
-	INS_LOG_BROKEN
-	INS_LOG_REPAIR
-	INS_LOG_LOST
-	INS_LOG_FOUND
-	INS_LOG_SALVAGING
-	INS_LOG_SALVAGE
-	INS_LOG_DONATE
+	INS_LOG_ADDNEW    InstrumentLogActionEnum = "AddNew"
+	INS_LOG_UPDATE    InstrumentLogActionEnum = "Update"
+	INS_LOG_BORROW    InstrumentLogActionEnum = "Borrow"
+	INS_LOG_RETURN    InstrumentLogActionEnum = "Return"
+	INS_LOG_MOVE      InstrumentLogActionEnum = "Move"
+	INS_LOG_BROKEN    InstrumentLogActionEnum = "Broken"
+	INS_LOG_REPAIR    InstrumentLogActionEnum = "Repair"
+	INS_LOG_LOST      InstrumentLogActionEnum = "Lost"
+	INS_LOG_FOUND     InstrumentLogActionEnum = "Found"
+	INS_LOG_SALVAGING InstrumentLogActionEnum = "Salvaging"
+	INS_LOG_SALVAGE   InstrumentLogActionEnum = "Salvage"
+	INS_LOG_DONATE    InstrumentLogActionEnum = "Donate"
 )
-
-var InstrumentLogActionLabel = map[InstrumentLogActionEnum]string{
-	INS_LOG_ADDNEW:    "AddNew",
-	INS_LOG_UPDATE:    "Update",
-	INS_LOG_BORROW:    "Borrow",
-	INS_LOG_RETURN:    "Return",
-	INS_LOG_MOVE:      "Move",
-	INS_LOG_BROKEN:    "Broken",
-	INS_LOG_REPAIR:    "Repair",
-	INS_LOG_LOST:      "Lost",
-	INS_LOG_FOUND:     "Found",
-	INS_LOG_SALVAGING: "Salvaging",
-	INS_LOG_SALVAGE:   "Salvage",
-	INS_LOG_DONATE:    "Donate",
-}
-
-func (program InstrumentLogActionEnum) ToString() string {
-	return InstrumentLogActionLabel[program]
-}
