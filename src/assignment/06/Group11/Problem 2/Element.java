4e00fccb066c65f927a5b855cdaa9f98f5be9e9d
wit03 <witjarukit@gmail.com>
2025-03-15 16:16:21 +0700

    feat: problem 2 lesson 6

--- src/assignment/06/Group11/Problem 2/Element.java
+++ src/assignment/06/Group11/Problem 2/Element.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public interface Element {
+    void setMediator(Mediator mediator);
+
+    String getName();
 }
--- src/assignment/06/Group11/Problem 2/Mediator.java
+++ src/assignment/06/Group11/Problem 2/Mediator.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 public interface Mediator {
+    void addNewNote(Note note);
+    void deleteNote();
+    void getInfoFromList(Note note);
+    void saveChanges();
+    void markNote();
+    void clear();
+    void sendToFilter(ListModel listModel);
+    void setElementsList(ListModel list);
+    void createElement(Element element);
+    void hideElements(boolean flag);
+    void createGUI();
 }
