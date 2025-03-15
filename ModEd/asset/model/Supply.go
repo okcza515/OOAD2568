e326c8868e693f577a1cec4b01d068ff08b7f150
Kanasorn Sudyodbunphot <masorn10110@gmail.com>
2025-03-16 02:10:15 +0700

    :bug: fix: change UUID fields to text type in Supply model

--- ModEd/asset/model/Supply.go
+++ ModEd/asset/model/Supply.go
@@ -7,12 +7,12 @@ import (
 
 type Supply struct {
 	gorm.Model
-	SupplyID    uuid.UUID      `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"supply_id,omitempty" csv:"supply_id,omitempty"`
+	SupplyID    uuid.UUID      `gorm:"type:text;primaryKey;default:gen_random_uuid()" json:"supply_id,omitempty" csv:"supply_id,omitempty"`
 	SupplyLabel string         `gorm:"not null" json:"supply_label,omitempty" csv:"supply_label,omitempty"`
 	Description string         `gorm:"type:text" json:"description,omitempty" csv:"description,omitempty"`
-	RoomID      uuid.UUID      `gorm:"type:uuid;not null" json:"room_id,omitempty" csv:"room_id,omitempty"`
+	RoomID      uuid.UUID      `gorm:"type:text;not null" json:"room_id,omitempty" csv:"room_id,omitempty"`
 	Location    string         `gorm:"type:text" json:"location,omitempty" csv:"location,omitempty"`
-	CategoryID  *uuid.UUID     `gorm:"type:uuid" json:"category_id,omitempty" csv:"category_id,omitempty"`
+	CategoryID  *uuid.UUID     `gorm:"type:text" json:"category_id,omitempty" csv:"category_id,omitempty"`
 	Quantity    int            `gorm:"not null" json:"quantity,omitempty" csv:"quantity,omitempty"`
 	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty" csv:"deleted_at,omitempty"`
 }
