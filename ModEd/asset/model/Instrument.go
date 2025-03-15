7bf9b1d3e68d4da48365222d729b470a58e40764
Pongpon Butsrimart <wavenakab2019@gmail.com>
2025-03-16 02:39:58 +0700

    :ambulance: fix: change data field in Instrument model corresponding to business logic + refactor InstrumentStatusEnum

--- ModEd/asset/model/Instrument.go
+++ ModEd/asset/model/Instrument.go
@@ -1,24 +1,24 @@
 package model
 
 import (
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Instrument struct {
-	InstrumentID       string               `gorm:"type:text;primaryKey;not null;unique" json:"instrument_id" csv:"instrument_id"`
+	InstrumentID       uuid.UUID            `gorm:"type:text;primaryKey" json:"instrument_id" csv:"instrument_id"`
 	InstrumentLabel    string               `gorm:"not null" json:"instrument_label" csv:"instrument_label"`
 	InstrumentCode     string               `gorm:"not null;unique" json:"instrument_code" csv:"instrument_code"`
-	InstrumentImageURL string               `gorm:"type:text" json:"instrument_image_url" csv:"instrument_image_url"`
-	Description        string               `gorm:"type:text" json:"description" csv:"description"`
+	Description        *string              `gorm:"type:text" json:"description" csv:"description"`
 	InstrumentStatus   InstrumentStatusEnum `gorm:"type:text;not null" json:"instrument_status" csv:"instrument_status"`
 	RoomID             string               `gorm:"type:text;not null" json:"room_id" csv:"room_id"`
-	Location           string               `gorm:"type:text" json:"location" csv:"location"`
-	CategoryID         *string              `gorm:"type:text" json:"category_id,omitempty" csv:"category_id"`
-	Cost               float64              `gorm:"type:real" json:"cost" csv:"cost"`
-	InstrumentSerialID string               `gorm:"type:text" json:"instrument_serial_id" csv:"instrument_serial_id"`
-	AddYear            int                  `gorm:"type:integer" json:"add_year" csv:"add_year"`
-	BudgetSource       string               `gorm:"type:text" json:"budget_source" csv:"budget_source"`
-	Brand              string               `gorm:"type:text" json:"brand" csv:"brand"`
-	Model              string               `gorm:"type:text" json:"model" csv:"model"`
-	DeletedAt          gorm.DeletedAt       `gorm:"index" json:"deleted_at,omitempty" csv:"deleted_at"`
+	Location           string               `gorm:"type:text;not null" json:"location" csv:"location"`
+	CategoryID         uuid.UUID            `gorm:"type:text;not null" json:"category_id" csv:"category_id"`
+	Cost               float64              `gorm:"type:real;not null" json:"cost" csv:"cost"`
+	InstrumentSerialID *string              `gorm:"type:text" json:"instrument_serial_id" csv:"instrument_serial_id"`
+	BudgetYear         int                  `gorm:"type:integer;not null" json:"add_year" csv:"add_year"`
+	BudgetSource       *string              `gorm:"type:text" json:"budget_source" csv:"budget_source"`
+	InstrumentBrand    *string              `gorm:"type:text" json:"brand" csv:"brand"`
+	InstrumentModel    *string              `gorm:"type:text" json:"model" csv:"model"`
+	DeletedAt          gorm.DeletedAt       `gorm:"index" json:"deleted_at" csv:"deleted_at"`
 }
--- ModEd/asset/model/InstrumentStatusEnum.go
+++ ModEd/asset/model/InstrumentStatusEnum.go
@@ -3,23 +3,23 @@ package model
 type InstrumentStatusEnum int
 
 const (
-	STATUS_AVAILABLE InstrumentStatusEnum = iota
-	STATUS_BORROWED
-	STATUS_BROKEN
-	STATUS_LOST
-	STATUS_SALVAGING
-	STATUS_SALVAGED
-	STATUS_DONATED
+	INS_AVAILABLE InstrumentStatusEnum = iota
+	INS_BORROWED
+	INS_BROKEN
+	INS_LOST
+	INS_SALVAGING
+	INS_SALVAGED
+	INS_DONATED
 )
 
 var InstrumentStatusEnumLabel = map[InstrumentStatusEnum]string{
-	STATUS_AVAILABLE: "Available",
-	STATUS_BORROWED:  "Borrowed",
-	STATUS_BROKEN:    "Broken",
-	STATUS_LOST:      "Lost",
-	STATUS_SALVAGING: "Salvaging",
-	STATUS_SALVAGED:  "Salvaged",
-	STATUS_DONATED:   "Donated",
+	INS_AVAILABLE: "Available",
+	INS_BORROWED:  "Borrowed",
+	INS_BROKEN:    "Broken",
+	INS_LOST:      "Lost",
+	INS_SALVAGING: "Salvaging",
+	INS_SALVAGED:  "Salvaged",
+	INS_DONATED:   "Donated",
 }
 
 func (status InstrumentStatusEnum) String() string {
