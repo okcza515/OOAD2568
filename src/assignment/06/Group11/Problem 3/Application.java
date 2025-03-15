aab451fc6963bfa414319b74d3e1964aecb7e086
KittoSanon <kittosanon@gmail.com>
2025-03-15 20:23:29 +0700

    mod file

--- src/assignment/06/Group11/Problem 3/Application.java
+++ src/assignment/06/Group11/Problem 3/Application.java
@@ -1,14 +1,31 @@
+import java.util.Arrays;
 
 public class Application {
-
-	public static void main(String[] args) {
-		Editor editor = new Editor();
-		try {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        
+        // Setting up listeners
+        LogOpenListener logListener = new LogOpenListener("file_ops.log");
+        EmailNotificationListener emailListener = new EmailNotificationListener("admin@example.com");
+        
+        // You can add more email recipients
+        emailListener.addEmail("manager@example.com");
+        
+        // Or create with multiple recipients at once
+        // EmailNotificationListener multiEmailListener = new EmailNotificationListener(
+        //     Arrays.asList("admin@example.com", "manager@example.com", "dev@example.com")
+        // );
+        
+        // Subscribe listeners to different events
+        editor.getEventManager().subscribe("open", logListener);
+        editor.getEventManager().subscribe("save", emailListener);
+        
+        // Using the editor
+        try {
             editor.openFile("test.txt");
             editor.saveFile();
         } catch (Exception e) {
             e.printStackTrace();
         }
-	}
-
+    }
 }
--- src/assignment/06/Group11/Problem 3/LogOpenListener.java
+++ src/assignment/06/Group11/Problem 3/LogOpenListener.java
@@ -1,15 +1,16 @@
-
+// LogOpenListener.java
 import java.io.File;
 
-public class LogOpenListener {
-	private File log;
-	
-	public LogOpenListener(String fileName) {
-		this.log = new File(fileName);
-	}
-	
-	public void update(String eventType, File file) {
-		System.out.println("Save to log " + log + ": Someone has performed " + eventType
-				+ " operation with the following file: " + file.getName());
-	}
+public class LogOpenListener implements EventListener {
+    private String filename;
+
+    public LogOpenListener(String filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public void update(String eventType, File file) {
+        System.out.println("Save to log " + filename + ": Someone has performed " + eventType
+                + " operation with the following file: " + file.getName());
+    }
 }
