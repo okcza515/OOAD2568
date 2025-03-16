76d7b4680e482c7bcef461bd3766c2074a3a39ec
Puwadech Intong <noonemore.gg@gmail.com>
2025-03-16 23:51:47 +0700

    feat: Create method in controller (For Department) and fix some model (Department, Instuctor)
    method for updating budget in department and filled in some info for department and instuctor model

--- ModEd/common/model/Department.go
+++ ModEd/common/model/Department.go
@@ -1,6 +1,14 @@
 package model
 
+import (
+	"github.com/google/uuid"
+)
+
 type Department struct {
-	Name   string
-	Parent Faculty
+	Name   		string
+	Parent 		Faculty
+	Students 	[]Student
+	Instructors []Instructor
+	CourseId 	[]uuid.UUID 		//seems like I have to use UUID as it would create circular dependency
+	Budget 		int
 }
--- ModEd/common/model/Instructor.go
+++ ModEd/common/model/Instructor.go
@@ -1,1 +1,18 @@
 package model
+
+import (
+	"time"
+	"github.com/google/uuid"
+)
+
+type Instructor struct {
+	Name 			string
+	InstructorID 	string
+	FirstName 		string
+	LastName  		string  
+	Email     		string  
+	StartDate 		time.Time
+	Faculty   		Faculty
+	Department 		Department
+	Courses   		[]uuid.UUID		//UUID to avoid circular dependency
+}
