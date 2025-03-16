8d36e0e5ea4d90083c6b515b6ec6d0fc6606c909
kmpoppp <kmpop8@gmail.com>
2025-03-16 22:04:50 +0700

    🐛 Fix BorrowInstrument and ReturnInstrument

--- ModEd/asset/model/BorrowInstrument.go
+++ ModEd/asset/model/BorrowInstrument.go
@@ -4,17 +4,15 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
 type BorrowInstrument struct {
-	gorm.Model
-	BorrowID           uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
-	StaffUserID        uuid.UUID `gorm:"type:uuid;not null"`
-	BorrowUserID       uuid.UUID `gorm:"type:uuid;not null"`
-	ExpectedReturnDate time.Time
-	BorrowDate         time.Time `gorm:"not null"`
-	ReturnID           uuid.UUID
-	Description        string
-	BorrowObjective    string `gorm:"not null"`
+	BorrowID           uuid.UUID  `gorm:"type:text;primaryKey;" json:"borrow_id" csv:"borrow_id"`
+	StaffUserID        uuid.UUID  `gorm:"type:text;not null" json:"staff_user_id" csv:"staff_user_id"`
+	BorrowUserID       uuid.UUID  `gorm:"type:text;not null" json:"borrow_user_id" csv:"borrow_user_id"`
+	ExpectedReturnDate *time.Time `gorm:"type:timestamp" json:"expected_return_date,omitempty" csv:"expected_return_date,omitempty"`
+	BorrowDate         time.Time  `gorm:"type:timestamp;not null" json:"borrow_date" csv:"borrow_date"`
+	ReturnID           *uuid.UUID `gorm:"type:text" json:"return_id,omitempty" csv:"return_id,omitempty"`
+	Description        *string    `gorm:"type:text" json:"description,omitempty" csv:"description,omitempty"`
+	BorrowObjective    string     `gorm:"type:text;not null" json:"borrow_objective" csv:"borrow_objective"`
 }
