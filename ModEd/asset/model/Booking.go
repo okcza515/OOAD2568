126fba04c9a95440763d3363503158efbbc54f41
Sikares <sikares.n@gmail.com>
2025-03-16 17:04:58 +0700

    feat: Add role & booking

---
+++ ModEd/asset/model/Booking.go
@@ -0,0 +1,27 @@
+package model
+
+import (
+	"time"
+)
+
+type Booking struct {
+	ID				string
+	ClassroomID		string
+	UserID          string
+	UserRole        Role
+	StartDate       time.Time
+	EndDate         time.Time
+	IsAvailable     bool
+	EventName       string
+	RoomStatus      bool
+	CapacityLimit   int
+}
+
+// func (b *Booking) IsRoomAvailable(startTime, endTime time.Time) bool {
+//     if (startTime.After(b.StartDate) && startTime.Before(b.EndDate)) ||
+// 	(endTime.After(b.StartDate) && endTime.Before(b.EndDate)) ||
+// 	(startTime.Equal(b.StartDate) || endTime.Equal(b.EndDate)) {
+//         return false
+//     }
+//     return true
+// }
---
+++ ModEd/asset/model/Role.go
@@ -0,0 +1,8 @@
+package model
+
+type Role string
+
+const (
+	Student Role = "Student"
+	Advisor Role = "Advisor"
+)
