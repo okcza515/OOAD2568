3496c41198160894df44a5eef2f0542e4ff9d33a
ffaann02 <rudfaanmaimahad@gmail.com>
2025-03-18 01:05:27 +0700

    feat(instructor-workload): update model type

--- ModEd/curriculum/model/instructor-workload/CourseUpdateRequest.go
+++ ModEd/curriculum/model/instructor-workload/CourseUpdateRequest.go
@@ -5,17 +5,17 @@ import (
 
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type CourseUpdateRequest struct {
 	gorm.Model
-	CourseId    string                      `gorm:"not null"`
-	RequestType CourseUpdateRequestTypeEnum `gorm:"not null"`
-	UpdatedBy   commonModel.Instructor      `gorm:"not null"`
-	UpdatedAt   time.Time                   `gorm:"not null"`
-
-	UpdateStrategy CourseUpdateStrategy
+	CourseId       uuid.UUID                   `gorm:"type:varchar(255);not null;index"`
+	RequestType    CourseUpdateRequestTypeEnum `gorm:"type:string;not null"`
+	UpdatedBy      commonModel.Instructor      `gorm:"foreignKey:UpdatedByID"`
+	UpdatedAt      time.Time                   `gorm:"not null;autoUpdateTime"`
+	UpdateStrategy CourseUpdateStrategy        `gorm:"-"`
 }
 
 type CourseNameUpdate struct {
