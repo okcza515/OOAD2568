927627d1bae79fac3edd1cf3b0e06a82e376505f
Puwadech Intong <noonemore.gg@gmail.com>
2025-03-17 20:54:27 +0700

    feat: Add faculty controller and some fix on department controller

--- ModEd/common/controller/DepartmentController.go
+++ ModEd/common/controller/DepartmentController.go
@@ -7,36 +7,36 @@ import (
 	"gorm.io/gorm"
 )
 
-type Department struct {
+type DepartmentController struct {
 	Connector *gorm.DB
 }
 
-func CreateDepartmentController(connector *gorm.DB) *Department {
-    department := Department{Connector: connector}
+func CreateDepartmentController(connector *gorm.DB) *DepartmentController {
+    department := DepartmentController{Connector: connector}
 	connector.AutoMigrate(&model.Department{})
 	return &department
 }
 
-func (department *Department) GetAll() ([]*model.Department, error) {
+func (department *DepartmentController) GetAllDepartments() ([]*model.Department, error) {
 	departments := []*model.Department{}
 	result := department.Connector.Find(&departments)
 	return departments, result.Error
 }
 
-func (department *Department) GetByDepartmentId(departmentId uuid.UUID) (*model.Department, error) {
+func (department *DepartmentController) GetByDepartmentId(departmentId uuid.UUID) (*model.Department, error) {
 	d := &model.Department{}
 	result := department.Connector.Where("department_id = ?", departmentId).First(d)
 	return d, result.Error
 }
 
-func (department *Department) SetBudget(departmentName string, newBudget int) error {
+func (department *DepartmentController) SetBudget(departmentName string, newBudget int) error {
     return department.Connector.Model(&model.Department{}).
         Where("name = ?", departmentName).
         Update("budget", newBudget).Error
 }
 
 //use for both decrement and increment
-func (department *Department) UpdateBudget(departmentName string, updateAmount int) error {
+func (department *DepartmentController) UpdateBudget(departmentName string, updateAmount int) error {
 	if (updateAmount >= 0) {
 		return department.Connector.Model(&model.Department{}).
         	Where("name = ?", departmentName).
