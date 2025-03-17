93a3c16644d634ec635f6384cb78e9705956492a
Patnakub <thanapatthaiking@gmail.com>
2025-03-17 18:04:37 +0700

    refactor(models): update Progress and ScoreAssignment models, remove unused SeniorProject files

--- ModEd/project/model/Progress.go
+++ ModEd/project/model/Progress.go
@@ -7,9 +7,9 @@ import (
 
 type Progress struct {
 	gorm.Model
-	ProgressId     uuid.UUID       `gorm:"type:text;primaryKey;default:gen_random_uuid()"`
-	AssignmentId uuid.UUID   `gorm:"not null;index"`
-	Name         string `gorm:"not null"`
-	IsCompleted  bool   `gorm:"not null"`
-	Assignment   *Assignment `gorm:"foreignKey:AssignmentId"`
+	ProgressId     	uuid.UUID   	`gorm:"type:text;primaryKey;default:gen_random_uuid()"`
+	AssignmentId 	uuid.UUID   	`gorm:"not null;index"`
+	Name         	string 			`gorm:"not null"`
+	IsCompleted  	bool   			`gorm:"not null"`
+	Assignment   	*Assignment 	`gorm:"foreignKey:AssignmentId"`
 }
