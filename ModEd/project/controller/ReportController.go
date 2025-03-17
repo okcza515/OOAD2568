2fcb5f7248425482bbac38bf5cea830719b40e8a
Nhell <pathza.2305@gmail.com>
2025-03-17 17:14:43 +0700

    style(controller): format ReportController code for improved readability

--- ModEd/project/controller/ReportController.go
+++ ModEd/project/controller/ReportController.go
@@ -1,49 +1,50 @@
 package controller
 
 import (
-    "ModEd/project/model"
-    "github.com/google/uuid"
-    "gorm.io/gorm"
+	"ModEd/project/model"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type IReportController interface {
-    ListAllReports() ([]model.Report, error)
-    RetrieveReport(id uuid.UUID) (*model.Report, error)
-    InsertReport(report *model.Report) error
-    UpdateReport(report *model.Report) error
-    DeleteReport(id uuid.UUID) error
+	ListAllReports() ([]model.Report, error)
+	RetrieveReport(id uuid.UUID) (*model.Report, error)
+	InsertReport(report *model.Report) error
+	UpdateReport(report *model.Report) error
+	DeleteReport(id uuid.UUID) error
 }
 
 type ReportController struct {
-    db *gorm.DB
+	db *gorm.DB
 }
 
 func NewReportController(db *gorm.DB) IReportController {
-    return &ReportController{db: db}
+	return &ReportController{db: db}
 }
 
 func (c *ReportController) ListAllReports() ([]model.Report, error) {
-    var reports []model.Report
-    err := c.db.Find(&reports).Error
-    return reports, err
+	var reports []model.Report
+	err := c.db.Find(&reports).Error
+	return reports, err
 }
 
 func (c *ReportController) RetrieveReport(id uuid.UUID) (*model.Report, error) {
-    var report model.Report
-    if err := c.db.Where("report_id = ?", id).First(&report).Error; err != nil {
-        return nil, err
-    }
-    return &report, nil
+	var report model.Report
+	if err := c.db.Where("report_id = ?", id).First(&report).Error; err != nil {
+		return nil, err
+	}
+	return &report, nil
 }
 
 func (c *ReportController) InsertReport(report *model.Report) error {
-    return c.db.Create(report).Error
+	return c.db.Create(report).Error
 }
 
 func (c *ReportController) UpdateReport(report *model.Report) error {
-    return c.db.Save(report).Error
+	return c.db.Save(report).Error
 }
 
 func (c *ReportController) DeleteReport(id uuid.UUID) error {
-    return c.db.Where("report_id = ?", id).Delete(&model.Report{}).Error
+	return c.db.Where("report_id = ?", id).Delete(&model.Report{}).Error
 }
