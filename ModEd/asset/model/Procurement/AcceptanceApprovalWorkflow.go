288ebd7234e24536c9ae2c20bb1dab2d08708d5b
ZTPG101 <110453102+ZTPG101@users.noreply.github.com>
2025-03-18 01:26:04 +0700

    more AAW

--- ModEd/asset/model/Procurement/AcceptanceApprovalWorkflow.go
+++ ModEd/asset/model/Procurement/AcceptanceApprovalWorkflow.go
@@ -5,8 +5,8 @@ import (
 )
 
 type AcceptanceApprovalWorkflow struct {
-	AcceptanceApprovalWorkflowID uuid.UUID      `gorm:"type:uuid;primaryKey"`
-	ProcurementID                Procurement    `gorm:"foreignKey:ProcurementID"`
-	ApproversID                  []uuid.UUID    `gorm:"foreignKey:InstructorId"` //TO-DO: Fix this data type
-	Status                       ApprovalStatus `gorm:"foreignKey:ApprovalStatusID"`
+	AcceptanceApprovalWorkflowID uuid.UUID                `gorm:"type:uuid;primaryKey"`
+	ProcurementID                Procurement              `gorm:"foreignKey:ProcurementID"`
+	ApproversID                  []uuid.UUID              `gorm:"foreignKey:InstructorId"` //TO-DO: Fix this data type
+	Status                       AcceptanceApprovalStatus `gorm:"foreignKey:AcceptanceApprovalStatusID"`
 }
