ad1cbc7b11cc45a32c6673cf7a6d127a51057591
kasinphatspam <kasinphat@icloud.com>
2025-03-17 16:41:39 +0700

    refractor: id

--- ModEd/project/model/ScorePresentationAdvisor.go
+++ ModEd/project/model/ScorePresentationAdvisor.go
@@ -7,7 +7,8 @@ import (
 
 type ScorePresentationAdvisor struct {
 	gorm.Model
-	PresentationId uuid.UUID `gorm:"type:text;not null;index"`
-	AdvisorID      uuid.UUID `gorm:"type:text;not null;index"`
-	Score          float64   `gorm:"not null"`
+	PresentationId uuid.UUID     `gorm:"type:text;not null;index"`
+	AdvisorId      uuid.UUID     `gorm:"type:text;not null;index"`
+	Score          float64       `gorm:"not null"`
+	Presentation   *Presentation `gorm:"foreignKey:PresentationId"`
 }
