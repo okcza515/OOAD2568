a37595f0e836a314cc3eb9e487401b2749787381
wavekanit <wavekanit@hotmail.com>
2025-03-17 21:30:50 +0700

    feat: add AcademicPostion and DepartmentPosition Model for instructor

---
+++ ModEd/hr/model/AcademicPosition.go
@@ -0,0 +1,10 @@
+package model
+
+type AcademicPosition int
+
+const (
+	ASSISTANT_PROF AcademicPosition = iota
+	ASSOCIATE_PROF
+	PROFESSOR
+	NONE
+)
--- ModEd/hr/model/InstructorInfo.go
+++ ModEd/hr/model/InstructorInfo.go
@@ -6,10 +6,10 @@ import (
 
 type InstructorInfo struct {
 	model.Instructor
-	Gender             string `csv:"Gender"`
-	CitizenID          string `csv:"CitizenID"`
-	PhoneNumber        string `csv:"PhoneNumber"`
-	Salary             int    `csv:"Salary"`
-	AcademicPosition   string `csv:"AcademicPosition"`
-	DepartmentPosition string `csv:"DepartmentPosition"`
+	Gender             string             `csv:"Gender"`
+	CitizenID          string             `csv:"CitizenID"`
+	PhoneNumber        string             `csv:"PhoneNumber"`
+	Salary             int                `csv:"Salary"`
+	AcademicPosition   AcademicPosition   `csv:"AcademicPosition"`
+	DepartmentPosition DepartmentPosition `csv:"DepartmentPosition"`
 }
