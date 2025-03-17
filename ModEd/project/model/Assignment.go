d26301421da1d42f010aeaac80d06c3d47f9bfe9
cinnamonjs <p.thunwattanakul@gmail.com>
2025-03-17 18:09:25 +0700

    chore: change format for model

--- ModEd/project/model/Assignment.go
+++ ModEd/project/model/Assignment.go
@@ -9,16 +9,15 @@ import (
 
 type Assignment struct {
 	gorm.Model
-	AssignmentId			uuid.UUID	`gorm:"type:text;primaryKey;default:gen_random_uuid()"`
-	SeniorProjectId 		uuid.UUID   `gorm:"type:text;not null;index"`
-	Name  					string    	`gorm:"not null"`
-	Description				string    	`gorm:"not null"`
-	SubmissionDate			*time.Time 	`gorm:"type:date"`
-	DueDate         		time.Time 	`gorm:"type:date;not null"`
+	AssignmentId    uuid.UUID  `gorm:"type:text;primaryKey;default:gen_random_uuid()"`
+	SeniorProjectId uuid.UUID  `gorm:"type:text;not null;index"`
+	Name            string     `gorm:"not null"`
+	Description     string     `gorm:"not null"`
+	SubmissionDate  *time.Time `gorm:"type:date"`
+	DueDate         time.Time  `gorm:"type:date;not null"`
 
-	ScoreAssignmentAdvisor		[]*ScoreAssessmentAdvisor 	`gorm:"foreignKey:AssignmentId"`
-	ScoreAssignmentCommittee	[]*ScoreAssessmentCommittee `gorm:"foreignKey:AssignmentId"`
-
-	SeniorProject	*SeniorProject	`gorm:"foreignKey:AssignmentId"`		
-	Progress		[]*Progress		`gorm:"foreignKey:AssignmentId"`
+	ScoreAssignmentAdvisor   []*ScoreAssessmentAdvisor   `gorm:"foreignKey:AssignmentId"`
+	ScoreAssignmentCommittee []*ScoreAssessmentCommittee `gorm:"foreignKey:AssignmentId"`
+	SeniorProject            *SeniorProject              `gorm:"foreignKey:SeniorProjectId"`
+	Progress                 []*Progress                 `gorm:"foreignKey:AssignmentId"`
 }
