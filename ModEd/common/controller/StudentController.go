2aaf355a346352b0d73b84029998f0271be93fe8
Supakorn Tungpatompramote <125183790+FlukeSupakorn@users.noreply.github.com>
2025-03-18 17:00:17 +0700

    refactor: replace Instructor and Student controllers with InstructorController and StudentController

--- ModEd/common/controller/StudentController.go
+++ ModEd/common/controller/StudentController.go
@@ -6,38 +6,38 @@ import (
 	"gorm.io/gorm"
 )
 
-type Student struct {
+type StudentController struct {
 	Connector *gorm.DB
 }
 
-func CreateStudentController(connector *gorm.DB) *Student {
-	student := Student{Connector: connector}
+func CreateStudentController(connector *gorm.DB) *StudentController {
+	student := StudentController{Connector: connector}
 	connector.AutoMigrate(&model.Student{})
 	return &student
 }
 
-func (student Student) GetAll() ([]*model.Student, error) {
+func (student StudentController) GetAll() ([]*model.Student, error) {
 	students := []*model.Student{}
 	result := student.Connector.Find(&students)
 	return students, result.Error
 }
 
-func (student Student) GetByStudentId(sid string) (*model.Student, error) {
+func (student StudentController) GetByStudentId(sid string) (*model.Student, error) {
 	s := &model.Student{}
 	result := student.Connector.Where("s_id = ?", sid).First(student)
 	return s, result.Error
 }
 
-func (student Student) Create(s *model.Student) error {
+func (student StudentController) Create(s *model.Student) error {
 	return student.Connector.Create(s).Error
 }
 
-func (student Student) Update(sid string, updatedData map[string]any) error {
+func (student StudentController) Update(sid string, updatedData map[string]any) error {
 	return student.Connector.Model(&model.Student{}).
 		Where("s_id = ?", sid).
 		Updates(updatedData).Error
 }
 
-func (student Student) DeleteByStudentId(sid string) error {
+func (student StudentController) DeleteByStudentId(sid string) error {
 	return student.Connector.Where("s_id = ?", sid).Delete(&model.Student{}).Error
 }
