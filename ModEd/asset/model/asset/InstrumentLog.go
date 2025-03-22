766f27e3f485dc83940917a262911c02ff316d29
Just3rd <kantapong.3rd@gmail.com>
2025-03-22 23:08:13 +0700

    :recycle: refactor: uuid -> uint + gorm.Model

--- ModEd/asset/model/asset/InstrumentLog.go
+++ ModEd/asset/model/asset/InstrumentLog.go
@@ -1,18 +1,16 @@
 package asset
 
 import (
-	"github.com/google/uuid"
-	"time"
+	"gorm.io/gorm"
 )
 
 type InstrumentLog struct {
-	LogID            uuid.UUID               `gorm:"type:text;primaryKey" json:"log_id" csv:"log_id"`
-	Timestamp        time.Time               `gorm:"type:timestamp;not null" json:"timestamp" csv:"timestamp"`
-	RefUserID        *uuid.UUID              `gorm:"type:text" json:"ref_user_id,omitempty" csv:"ref_user_id"`
-	StaffUserID      uuid.UUID               `gorm:"type:text;not null" json:"staff_user_id" csv:"staff_user_id"`
-	Action           InstrumentLogActionEnum `gorm:"type:text;not null" json:"action" csv:"action"`
-	InstrumentID     uuid.UUID               `gorm:"type:text;not null" json:"instrument_id" csv:"instrument_id"`
-	Description      string                  `gorm:"type:text;not null" json:"description" csv:"description"`
-	RefBorrowID      *uuid.UUID              `gorm:"type:text" json:"ref_borrow_id,omitempty" csv:"ref_borrow_id"`
+	gorm.Model
+	RefUserID        *uint
+	StaffUserID      uint                    `gorm:"not null"`
+	Action           InstrumentLogActionEnum `gorm:"not null"`
+	InstrumentID     uint                    `gorm:"not null"`
+	Description      string                  `gorm:"not null"`
+	RefBorrowID      *uint                   `gorm:"type:integer"`
 	BorrowInstrument BorrowInstrument        `gorm:"foreignKey:RefBorrowID;references:BorrowID;constraint:OnUpdate:CASCADE;"`
 }
