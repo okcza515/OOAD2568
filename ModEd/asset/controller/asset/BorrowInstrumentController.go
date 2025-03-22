d76e418880529116937af2adccdc4cb54b0d73b0
kmpoppp <kmpop8@gmail.com>
2025-03-22 23:14:42 +0700

    Fix BorrowInstrumentController.go

--- ModEd/asset/controller/asset/BorrowInstrumentController.go
+++ ModEd/asset/controller/asset/BorrowInstrumentController.go
@@ -2,7 +2,7 @@
 package asset
 
 import (
-	"ModEd/asset/model/asset"
+	model "ModEd/asset/model/asset"
 	"gorm.io/gorm"
 	"time"
 )
@@ -11,30 +11,30 @@ type BorrowInstrumentController struct {
 	Db *gorm.DB
 }
 
-func (c *BorrowInstrumentController) GetAll() (*[]asset.BorrowInstrument, error) {
-	borrowInstrument := new([]asset.BorrowInstrument)
+func (c *BorrowInstrumentController) GetAll() (*[]model.BorrowInstrument, error) {
+	borrowInstrument := new([]model.BorrowInstrument)
 	result := c.Db.Find(&borrowInstrument)
 	return borrowInstrument, result.Error
 }
 
-func (c *BorrowInstrumentController) GetByID(ID uint) (*asset.BorrowInstrument, error) {
-	borrowInstrument := new(asset.BorrowInstrument)
+func (c *BorrowInstrumentController) GetByID(ID uint) (*model.BorrowInstrument, error) {
+	borrowInstrument := new(model.BorrowInstrument)
 	result := c.Db.First(&borrowInstrument, "ID = ?", ID)
 	return borrowInstrument, result.Error
 }
 
-func (c *BorrowInstrumentController) Create(body *asset.BorrowInstrument) error {
+func (c *BorrowInstrumentController) Create(body *model.BorrowInstrument) error {
 	result := c.Db.Create(body)
 	return result.Error
 }
 
-func (c *BorrowInstrumentController) Update(ID uint, body *asset.BorrowInstrument) error {
+func (c *BorrowInstrumentController) Update(ID uint, body *model.BorrowInstrument) error {
 	body.ID = ID
 	result := c.Db.Updates(body)
 	return result.Error
 }
 
 func (c *BorrowInstrumentController) Delete(ID uint) error {
-	result := c.Db.Model(&asset.BorrowInstrument{}).Where("ID = ?", ID).Update("deleted_at", time.Now())
+	result := c.Db.Model(&model.BorrowInstrument{}).Where("ID = ?", ID).Update("deleted_at", time.Now())
 	return result.Error
 }
