410b7a6bc568af312ed4f76f3c65b5bf07f1cb1a
SK-Tonhom <sopidar25@gmail.com>
2025-03-22 22:28:05 +0700

    🐛 edit gorm.model catagory

--- ModEd/asset/model/asset/Category.go
+++ ModEd/asset/model/asset/Category.go
@@ -1,17 +1,15 @@
 package asset
 
 import (
-	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Category struct {
 	gorm.Model
-	CategoryID   uuid.UUID    `gorm:"type:text;primaryKey" json:"category_id" csv:"category_id"`
-	CategoryName string       `gorm:"type:varchar(255);not null" json:"category_name" csv:"category_name"`
-	Description  *string      `gorm:"type:varchar(255)" json:"description,omitempty" csv:"description,omitempty"`
-	UpdatedBy    *uuid.UUID   `gorm:"type:text" json:"updated_by" csv:"updated_by"`
-	DeletedBy    *uuid.UUID   `gorm:"type:text" json:"deleted_by,omitempty" csv:"deleted_by,omitempty"`
+	CategoryName string  `gorm:"type:varchar(255);not null"`
+	Description  *string `gorm:"type:varchar(255)"`
+	UpdatedBy    *uint
+	DeletedBy    *uint
 	Instrument   []Instrument `gorm:"foreignKey:CategoryID;references:CategoryID;constraint:OnUpdate:CASCADE;"`
 	Supply       []Supply     `gorm:"foreignKey:CategoryID;references:CategoryID;constraint:OnUpdate:CASCADE;"`
 }
