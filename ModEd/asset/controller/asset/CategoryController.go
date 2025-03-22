640b10f81e1676a8c3c2ee8152aae4ee1591ad35
SK-Tonhom <sopidar25@gmail.com>
2025-03-22 23:04:06 +0700

    🐛 edit: controller

--- ModEd/asset/controller/asset/CategoryController.go
+++ ModEd/asset/controller/asset/CategoryController.go
@@ -7,34 +7,34 @@ import (
 	"gorm.io/gorm"
 )
 
-type CategoryCotroller struct {
+type CategoryController struct {
 	Db *gorm.DB
 }
 
-func (c *CategoryCotroller) GetAll() (*[]asset.Category, error) {
+func (c *CategoryController) GetAll() (*[]asset.Category, error) {
 	categorys := new([]asset.Category)
 	result := c.Db.Find(&categorys)
 	return categorys, result.Error
 }
 
-func (c *CategoryCotroller) GetByID(supplyID uint) (*asset.Category, error) {
+func (c *CategoryController) GetByID(supplyID uint) (*asset.Category, error) {
 	categorys := new(asset.Category)
 	result := c.Db.First(&categorys, "ID = ?", supplyID)
 	return categorys, result.Error
 }
 
-func (c *CategoryCotroller) Create(body *asset.Category) error {
+func (c *CategoryController) Create(body *asset.Category) error {
 	result := c.Db.Create(body)
 	return result.Error
 }
 
-func (c *CategoryCotroller) Update(categoryID uint, body *asset.Category) error {
+func (c *CategoryController) Update(categoryID uint, body *asset.Category) error {
 	body.ID = categoryID
 	result := c.Db.Updates(body)
 	return result.Error
 }
 
-func (c *CategoryCotroller) Delete(categoryID uint) error {
+func (c *CategoryController) Delete(categoryID uint) error {
 	result := c.Db.Model(&asset.Category{}).Where("ID = ?", categoryID).Update("deleted_at", time.Now())
 	return result.Error
 }
