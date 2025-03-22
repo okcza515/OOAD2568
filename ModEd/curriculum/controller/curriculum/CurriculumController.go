861277ba5936d2619f4f8f6f533626e8179d1405
intouch-hymn <hymnjung01@gmail.com>
2025-03-22 23:06:10 +0700

    feat: curriculum

--- ModEd/curriculum/controller/curriculum/CurriculumController.go
+++ ModEd/curriculum/controller/curriculum/CurriculumController.go
@@ -1,17 +1,26 @@
 package controller
 
 import (
+<<<<<<< Updated upstream
 	modelCommon "ModEd/common/model"
 	model "ModEd/curriculum/model/Internship"
 	modelWorkload "ModEd/curriculum/model/instructor-workload"
 	"errors"
 
 	"gorm.io/gorm"
+=======
+	"gorm.io/gorm"
+
+	modelCurriculum "ModEd/curriculum/model"
+>>>>>>> Stashed changes
 )
 
 type ICurriculumController interface {
-	// Put methods here
-	// eg. CreateCurriculum(curriculum *modelCurriculum.Curriculum) error
+	CreateCurriculum(curriculum modelCurriculum.Curriculum) (curriculumId uint, err error)
+	GetCurriculum(curriculumId uint) (curriculum *modelCurriculum.Curriculum, err error)
+	GetCurriculums() (curriculums []*modelCurriculum.Curriculum, err error)
+	UpdateCurriculum(updatedCurriculum modelCurriculum.Curriculum) (curriculum *modelCurriculum.Curriculum, err error)
+	DeleteCurriculum(curriculumId uint) (curriculum *modelCurriculum.Curriculum, err error)
 }
 
 type CurriculumController struct {
@@ -38,4 +47,62 @@ func (c *CurriculumController) MigrateToDB() error {
 	}
 
 	return nil
+}
+
+// Create
+func (c *CurriculumController) CreateCurriculum(curriculum modelCurriculum.Curriculum) (curriculumId uint, err error) {
+	if err := c.db.Create(&curriculum).Error; err != nil {
+		return 0, err
+	}
+	return curriculum.ID, nil
+}
+
+// Read one
+func (c *CurriculumController) GetCurriculum(curriculumId uint) (curriculum *modelCurriculum.Curriculum, err error) {
+	curriculum = &modelCurriculum.Curriculum{}
+	if err := c.db.First(curriculum, curriculumId).Error; err != nil {
+		return nil, err
+	}
+	return curriculum, nil
+}
+
+// Read all
+func (c *CurriculumController) GetCurriculums() (curriculums []*modelCurriculum.Curriculum, err error) {
+	if err := c.db.Find(&curriculums).Error; err != nil {
+		return nil, err
+	}
+	return curriculums, nil
+}
+
+// Update
+func (c *CurriculumController) UpdateCurriculum(updated modelCurriculum.Curriculum) (curriculum *modelCurriculum.Curriculum, err error) {
+	curriculum = &modelCurriculum.Curriculum{}
+	if err := c.db.First(curriculum, updated.ID).Error; err != nil {
+		return nil, err
+	}
+
+	// update fields
+	curriculum.CurriculumId = updated.CurriculumId
+	curriculum.Name = updated.Name
+	curriculum.StartYear = updated.StartYear
+	curriculum.EndYear = updated.EndYear
+	curriculum.DepartmentName = updated.DepartmentName
+	curriculum.ProgramType = updated.ProgramType
+
+	if err := c.db.Save(curriculum).Error; err != nil {
+		return nil, err
+	}
+	return curriculum, nil
+}
+
+// Delete
+func (c *CurriculumController) DeleteCurriculum(curriculumId uint) (curriculum *modelCurriculum.Curriculum, err error) {
+	curriculum = &modelCurriculum.Curriculum{}
+	if err := c.db.First(curriculum, curriculumId).Error; err != nil {
+		return nil, err
+	}
+	if err := c.db.Delete(curriculum).Error; err != nil {
+		return nil, err
+	}
+	return curriculum, nil
 }
---
+++ ModEd/curriculum/testing/curriculum_interface_test.go
@@ -0,0 +1,170 @@
+package test
+
+import (
+	"testing"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	commonModel "ModEd/common/model"
+	controller "ModEd/curriculum/controller"
+	model "ModEd/curriculum/model"
+)
+
+func initCurriculumDB() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("../../data/curriculum.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	if err := db.AutoMigrate(&model.Curriculum{}); err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func TestCreateCurriculum(t *testing.T) {
+	db := initCurriculumDB()
+	curriculumController := controller.NewCurriculumController(db)
+
+	newCurriculum := model.Curriculum{
+		CurriculumId:   1001,
+		Name:           "Computer Science",
+		StartYear:      2023,
+		EndYear:        2027,
+		DepartmentName: "CS Dept",
+		ProgramType:    commonModel.ProgramType("International"),
+	}
+
+	curriculumId, err := curriculumController.CreateCurriculum(newCurriculum)
+	if err != nil {
+		t.Fatalf("Failed to create curriculum: %v", err)
+	}
+
+	retrieved, err := curriculumController.GetCurriculum(curriculumId)
+	if err != nil {
+		t.Fatalf("Failed to retrieve curriculum: %v", err)
+	}
+	if retrieved.Name != newCurriculum.Name {
+		t.Errorf("Expected name %s, got %s", newCurriculum.Name, retrieved.Name)
+	}
+}
+
+func TestGetCurriculum(t *testing.T) {
+	db := initCurriculumDB()
+	curriculumController := controller.NewCurriculumController(db)
+
+	curriculum := model.Curriculum{
+		CurriculumId:   2001,
+		Name:           "Software Engineering",
+		StartYear:      2022,
+		EndYear:        2026,
+		DepartmentName: "SE Dept",
+		ProgramType:    commonModel.ProgramType("International"),
+	}
+
+	id, err := curriculumController.CreateCurriculum(curriculum)
+	if err != nil {
+		t.Fatalf("Failed to create curriculum: %v", err)
+	}
+
+	result, err := curriculumController.GetCurriculum(id)
+	if err != nil {
+		t.Fatalf("Failed to get curriculum: %v", err)
+	}
+	if result.CurriculumId != curriculum.CurriculumId {
+		t.Errorf("Expected CurriculumId %d, got %d", curriculum.CurriculumId, result.CurriculumId)
+	}
+}
+
+func TestGetCurriculums(t *testing.T) {
+	db := initCurriculumDB()
+	curriculumController := controller.NewCurriculumController(db)
+
+	curriculum := model.Curriculum{
+		CurriculumId:   3001,
+		Name:           "Data Science",
+		StartYear:      2021,
+		EndYear:        2025,
+		DepartmentName: "DS Dept",
+		ProgramType:    commonModel.ProgramType("master"),
+	}
+
+	_, err := curriculumController.CreateCurriculum(curriculum)
+	if err != nil {
+		t.Fatalf("Failed to create curriculum: %v", err)
+	}
+
+	curriculums, err := curriculumController.GetCurriculums()
+	if err != nil {
+		t.Fatalf("Failed to get curriculums: %v", err)
+	}
+	if len(curriculums) == 0 {
+		t.Errorf("Expected at least 1 curriculum, got 0")
+	}
+}
+
+func TestUpdateCurriculum(t *testing.T) {
+	db := initCurriculumDB()
+	curriculumController := controller.NewCurriculumController(db)
+
+	curriculum := model.Curriculum{
+		CurriculumId:   4001,
+		Name:           "Cybersecurity",
+		StartYear:      2024,
+		EndYear:        2028,
+		DepartmentName: "Cyber Dept",
+		ProgramType:    commonModel.ProgramType("bachelor"),
+	}
+
+	id, err := curriculumController.CreateCurriculum(curriculum)
+	if err != nil {
+		t.Fatalf("Failed to create curriculum: %v", err)
+	}
+
+	retrieved, err := curriculumController.GetCurriculum(id)
+	if err != nil {
+		t.Fatalf("Failed to retrieve curriculum: %v", err)
+	}
+
+	retrieved.Name = "Cybersecurity & Digital Forensics"
+	updated, err := curriculumController.UpdateCurriculum(*retrieved)
+	if err != nil {
+		t.Fatalf("Failed to update curriculum: %v", err)
+	}
+	if updated.Name != "Cybersecurity & Digital Forensics" {
+		t.Errorf("Expected updated name, got %s", updated.Name)
+	}
+}
+
+func TestDeleteCurriculum(t *testing.T) {
+	db := initCurriculumDB()
+	curriculumController := controller.NewCurriculumController(db)
+
+	curriculum := model.Curriculum{
+		CurriculumId:   5001,
+		Name:           "AI and Robotics",
+		StartYear:      2023,
+		EndYear:        2027,
+		DepartmentName: "AI Dept",
+		ProgramType:    commonModel.ProgramType("phd"),
+	}
+
+	id, err := curriculumController.CreateCurriculum(curriculum)
+	if err != nil {
+		t.Fatalf("Failed to create curriculum: %v", err)
+	}
+
+	deleted, err := curriculumController.DeleteCurriculum(id)
+	if err != nil {
+		t.Fatalf("Failed to delete curriculum: %v", err)
+	}
+	if deleted.ID != id {
+		t.Errorf("Expected deleted ID %v, got %v", id, deleted.ID)
+	}
+
+	_, err = curriculumController.GetCurriculum(id)
+	if err == nil {
+		t.Fatalf("Expected error after deleting curriculum, got nil")
+	}
+}
