be132ed7a7356eeba438b237320f544e1439093e
Pongpon Butsrimart <wavenakab2019@gmail.com>
2025-03-23 00:10:39 +0700

    feat: init main cli

--- ModEd/asset/cli/asset/AssetCLI.go
+++ ModEd/asset/cli/asset/AssetCLI.go
@@ -1,19 +1,42 @@
 package main
 
 import (
-	controller "ModEd/asset/controller/asset"
+	//controller "ModEd/asset/controller/asset"
 	util "ModEd/asset/util"
 	"fmt"
 )
 
 func main() {
-	facade, err := controller.CreateAssetControllerFacade()
-	if err != nil {
-		panic("err: initialize controllers failed")
-	}
+	//facade, err := controller.CreateAssetControllerFacade()
+	//if err != nil {
+	//	panic("err: initialize controllers failed")
+	//}
 
 	util.PrintBanner()
 
-	supplies, err := facade.Supply.GetAll()
-	fmt.Println(supplies)
+	//supplies, err := facade.Supply.GetAll()
+	//fmt.Println(supplies)
+
+	for {
+		inputBuffer := ""
+		_, _ = fmt.Scanln(&inputBuffer)
+
+		switch inputBuffer {
+		case "1":
+			fmt.Println("Menu 1: ")
+			// your function goes here
+		case "2":
+			fmt.Println("Menu 2: ")
+			// your function goes here
+		case "3":
+			fmt.Println("Menu 3: ")
+			// your function goes here
+		case "4":
+			fmt.Println("Menu 4: ")
+			// your function goes here
+		case "exit":
+			break
+		}
+	}
+
 }
