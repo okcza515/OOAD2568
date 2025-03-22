a75f46c5feb7d5ebb1abe3d86d63b985a8a6abde
Just3rd <kantapong.3rd@gmail.com>
2025-03-22 23:08:33 +0700

    :sparkles: feat: asset controller facade

--- ModEd/asset/cli/asset/AssetCLI.go
+++ ModEd/asset/cli/asset/AssetCLI.go
@@ -1,71 +1,19 @@
 package main
 
 import (
-	asset2 "ModEd/asset/controller/asset"
-	"ModEd/asset/model/asset"
+	controller "ModEd/asset/controller/asset"
 	util "ModEd/asset/util"
-
-	"errors"
-	"flag"
 	"fmt"
-	"os"
-
-	"time"
-
-	"github.com/google/uuid"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
 )
 
 func main() {
-	fmt.Println("Hi im asset")
-
-	var (
-		database string
-		path     string
-	)
-	flag.StringVar(&database, "database", "data/ModEd.bin", "Path of SQLite Database.")
-	flag.StringVar(&path, "path", "", "Path to CSV or JSON for student registration.")
-	flag.Parse()
-
-	db, err := gorm.Open(sqlite.Open(database), &gorm.Config{})
+	facade, err := controller.CreateAssetControllerFacade()
 	if err != nil {
-		panic("failed to connect database")
-	}
-
-	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
-		panic("*** Error: " + path + " does not exist.\n")
-	}
-
-	instrumentLogController := asset2.InstrumentLogController{Db: db}
-	migrationController := asset2.MigrationController{Db: db}
-
-	err = migrationController.MigrateToDB()
-	if err != nil {
-		panic("err: migration failed")
+		panic("err: initialize controllers failed")
 	}
 
 	util.PrintBanner()
 
-	exampleLog := asset.InstrumentLog{
-		LogID:        uuid.New(),
-		Timestamp:    time.Now(),
-		RefUserID:    nil,
-		StaffUserID:  uuid.New(),
-		Action:       asset.INS_LOG_REPAIR,
-		InstrumentID: uuid.New(),
-		Description:  "Do something",
-		RefBorrowID:  nil,
-	}
-
-	db.Create(exampleLog)
-
-	logs, err := instrumentLogController.GetAll()
-	if err != nil {
-		panic("err: query failed")
-	}
-
-	for _, instrumentLog := range *logs {
-		util.PrintStruct(instrumentLog)
-	}
+	supplies, err := facade.Supply.GetAll()
+	fmt.Println(supplies)
 }
