974a3f53e26f7a47b9689882b5456e64e2ee0a8a
Just3rd <kantapong.3rd@gmail.com>
2025-03-23 04:16:51 +0700

    :sparkles: feat: asset cli + instrument handler

--- ModEd/asset/cli/asset/AssetCLI.go
+++ ModEd/asset/cli/asset/AssetCLI.go
@@ -1,42 +1,58 @@
 package main
 
 import (
-	//controller "ModEd/asset/controller/asset"
+	"ModEd/asset/cli/asset/handler"
+	controller "ModEd/asset/controller/asset"
 	util "ModEd/asset/util"
 	"fmt"
 )
 
 func main() {
-	//facade, err := controller.CreateAssetControllerFacade()
-	//if err != nil {
-	//	panic("err: initialize controllers failed")
-	//}
+	facade, err := controller.CreateAssetControllerFacade()
+	if err != nil {
+		panic("err: initialize controllers failed")
+	}
 
-	util.PrintBanner()
+	inputBuffer := ""
 
-	//supplies, err := facade.Supply.GetAll()
-	//fmt.Println(supplies)
+	for inputBuffer != "exit" {
+		util.ClearScreen()
+		util.PrintBanner()
 
-	for {
-		inputBuffer := ""
-		_, _ = fmt.Scanln(&inputBuffer)
+		printOption()
+
+		inputBuffer = util.GetCommandInput()
 
 		switch inputBuffer {
 		case "1":
-			fmt.Println("Menu 1: ")
-			// your function goes here
+			fmt.Println("Not implemented yet...")
 		case "2":
-			fmt.Println("Menu 2: ")
-			// your function goes here
+			handler.InstrumentHandler(facade)
 		case "3":
-			fmt.Println("Menu 3: ")
-			// your function goes here
+			fmt.Println("Not implemented yet...")
 		case "4":
-			fmt.Println("Menu 4: ")
-			// your function goes here
-		case "exit":
-			break
+			fmt.Println("Not implemented yet...")
+		case "5":
+			fmt.Println("Not implemented yet...")
+		case "6":
+			fmt.Println("Not implemented yet...")
 		}
 	}
 
+	util.PrintByeBye()
+}
+
+func printOption() {
+	fmt.Println(":/asset")
+	fmt.Println()
+	fmt.Println("Welcome to ModEd Asset Service CLI!")
+	fmt.Println("Here is the list of page you can use, choose wisely!")
+	fmt.Println("  1:\tCategory Page")
+	fmt.Println("  2:\tInstrument Page")
+	fmt.Println("  3:\tSupply Page")
+	fmt.Println("  4:\tBorrow Page")
+	fmt.Println("  5:\tInstrument Log Page")
+	fmt.Println("  6:\tSupply Log Page")
+	fmt.Println("  exit:\tExit the program (or Ctrl+C is fine ¯\\\\_(ツ)_/¯)")
+	fmt.Println()
 }
