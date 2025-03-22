8b1df6735d6c477afec8a1f9125127c99d9c0cae
wit03 <witjarukit@gmail.com>
2025-03-22 22:20:40 +0700

    feat: class controller

--- ModEd/curriculum/controller/class/ClassController.go
+++ ModEd/curriculum/controller/class/ClassController.go
@@ -1,10 +1,17 @@
 package controller
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+
+	modelCurriculum "ModEd/curriculum/model"
+)
 
 type IClassController interface {
-	// Put methods here
-	// eg. CreateClass(class *modelCurriculum.Class) error
+	CreateClass(class modelCurriculum.Class) (classId uint, err error)
+	GetClass(classId uint) (class *modelCurriculum.Class, err error)
+	GetClasses() (classes []*modelCurriculum.Class, err error)
+	UpdateClass(updatedClass modelCurriculum.Class) (class *modelCurriculum.Class, err error)
+	DeleteClass(classId uint) (class *modelCurriculum.Class, err error)
 }
 
 type ClassController struct {
@@ -13,4 +20,52 @@ type ClassController struct {
 
 func NewClassController(db *gorm.DB) IClassController {
 	return &ClassController{db: db}
+}
+
+func (c *ClassController) CreateClass(class modelCurriculum.Class) (classId uint, err error) {
+	if err := c.db.Create(&class).Error; err != nil {
+		return 0, err
+	}
+	return class.ID, nil
+}
+
+func (c *ClassController) GetClass(classId uint) (class *modelCurriculum.Class, err error) {
+	class = &modelCurriculum.Class{}
+	if err := c.db.First(class, classId).Error; err != nil {
+		return nil, err
+	}
+	return class, nil
+}
+
+func (c *ClassController) GetClasses() (classes []*modelCurriculum.Class, err error) {
+	if err := c.db.Find(&classes).Error; err != nil {
+		return nil, err
+	}
+	return classes, nil
+}
+
+func (c *ClassController) UpdateClass(updatedClass modelCurriculum.Class) (class *modelCurriculum.Class, err error) {
+	class = &modelCurriculum.Class{}
+	if err := c.db.First(class, updatedClass.ID).Error; err != nil {
+		return nil, err
+	}
+	class.CourseId = updatedClass.CourseId
+	class.ClassId = updatedClass.ClassId
+	class.Schedule = updatedClass.Schedule
+	class.Section = updatedClass.Section
+	if err := c.db.Save(class).Error; err != nil {
+		return nil, err
+	}
+	return class, nil
+}
+
+func (c *ClassController) DeleteClass(classId uint) (class *modelCurriculum.Class, err error) {
+	class = &modelCurriculum.Class{}
+	if err := c.db.First(class, classId).Error; err != nil {
+		return nil, err
+	}
+	if err := c.db.Delete(class).Error; err != nil {
+		return nil, err
+	}
+	return class, nil
 }
