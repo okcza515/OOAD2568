fdbdfbb029ec611026d0c02a6baf33ce0a833788
Tongzkungz <tongs2004@gmail.com>
2025-03-25 00:57:54 +0700

    fix instrumentmanagement model

--- ModEd/asset/model/SpaceManagement/InstrumentManagement.go
+++ ModEd/asset/model/SpaceManagement/InstrumentManagement.go
@@ -8,12 +8,12 @@ import (
 
 type InstrumentManagement struct {
 	gorm.Model
-	AssetManagementID  uint                   `gorm:"type:integer" json:"asset_management_id" csv:"asset_management_id"`
-	BorrowInstrument   asset.BorrowInstrument `gorm:"foreignKey:AssetManagementID;references:AssetManagementID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"borrow_instrument"`
-	BorrowUserID       uint       			  `gorm:"not null"`
-	RoomID             uint                   `gorm:"type:integer;not null;index" json:"room_id" csv:"room_id"`
-	Room               Room                   `gorm:"foreignKey:RoomID;references:RoomID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"room"`
-	InstrumentID       uint                   `gorm:"type:integer;not null;index" json:"instrument_id" csv:"instrument_id"`
-	Instrument         asset.Instrument       `gorm:"foreignKey:InstrumentID;references:InstrumentID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"instrument"`
-	InstrumentLabel    string                 `gorm:"type:text;not null" json:"instrument_label" csv:"instrument_label"`
+	AssetManagementID  	uint                   `gorm:"type:integer" json:"asset_management_id" csv:"asset_management_id"`
+	BorrowUserID	   	uint                   `gorm:"type:integer" json:"borrow_id" csv:"borrow_id"`
+	BorrowInstrument   	asset.BorrowInstrument `gorm:"foreignKey:BorrowUserID;references:BorrowUserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"borrow_instrument"`
+	RoomID             	uint                   `gorm:"type:integer;not null;index" json:"room_id" csv:"room_id"`
+	Room               	Room                   `gorm:"foreignKey:RoomID;references:RoomID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"room"`
+	InstrumentSerialID  uint                   `gorm:"type:integer;not null;index" json:"instrument_id" csv:"instrument_id"`
+	Instrument         	asset.Instrument       `gorm:"foreignKey:InstrumentSerialID;references:InstrumentSerialID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"instrument"`
+	InstrumentLabel    	string                 `gorm:"type:text;not null" json:"instrument_label" csv:"instrument_label"`
 }
