8bfe82f4744361880484c190f6762b72a7c028e1
Just3rd <kantapong.3rd@gmail.com>
2025-03-25 03:49:03 +0700

    :sparkles: feat: list all instrument

--- ModEd/asset/controller/asset/InstrumentController.go
+++ ModEd/asset/controller/asset/InstrumentController.go
@@ -1,20 +1,39 @@
 package asset
 
 import (
-	"ModEd/asset/model/asset"
+	model "ModEd/asset/model/asset"
 	"gorm.io/gorm"
+	"strings"
 )
 
 type InstrumentController struct {
 	db *gorm.DB
 }
 
-func (ins *InstrumentController) CreateNewInstrument(instrumentData *[]asset.Instrument) error {
-	result := ins.db.Create(instrumentData)
+func (c *InstrumentController) Create(instrumentData *[]model.Instrument) error {
+	result := c.db.Create(instrumentData)
 
 	if result.Error != nil {
 		return result.Error
 	}
 
 	return nil
+}
+
+func (c *InstrumentController) ListAll() ([]string, error) {
+	inst := new([]model.Instrument)
+	result := c.db.Find(&inst)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	var resultList []string
+
+	for _, i := range *inst {
+		list := strings.Join([]string{i.InstrumentCode, i.InstrumentLabel}, "\t")
+		resultList = append(resultList, list)
+	}
+
+	return resultList, result.Error
 }
