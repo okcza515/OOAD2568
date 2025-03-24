a1a3abf06a15fec8cf4e3fbfc483ee7ea77340ef
Billy19191 <bill.ratchanon@gmail.com>
2025-03-24 22:40:47 +0700

    ⚡️  feat: space management cli

--- ModEd/asset/cli/SpaceManagement/SpaceManagementCLI.go
+++ ModEd/asset/cli/SpaceManagement/SpaceManagementCLI.go
@@ -1,2 +1,48 @@
 // MEP-1013
 package main
+
+import (
+	"ModEd/asset/cli/spacemanagement/handler"
+	controller "ModEd/asset/controller/spacemanagement"
+	util "ModEd/asset/util"
+	"fmt"
+)
+
+func main() {
+	facade, err := controller.CreateSpaceManagementControllerFacade()
+	if err != nil {
+		panic("err: initialize controllers failed")
+	}
+
+	input := ""
+	for input != "exit" {
+		util.ClearScreen()
+		util.PrintBanner()
+		printOption()
+		input = util.GetCommandInput()
+
+		switch input {
+		case "1":
+			handler.AssetManagementHandler(facade)
+		case "2":
+			handler.BookingHandler(facade)
+		case "3":
+			handler.PermanentScheduleHandler(facade)
+		case "4":
+			handler.RoomHandler(facade)
+		}
+	}
+}
+
+func printOption() {
+	fmt.Println(":/spacemanagement")
+	fmt.Println()
+	fmt.Println("Welcome to ModEd Space Management Service CLI!")
+	fmt.Println("Here is the list of page you can use, choose wisely!")
+	fmt.Println("  1:\tAsset Management Page")
+	fmt.Println("  2:\tBooking Page")
+	fmt.Println("  3:\tPermanent Schedule Page")
+	fmt.Println("  4:\tRoom Page")
+	fmt.Println("  exit:\tExit the program (or Ctrl+C is fine ¯\\\\_(ツ)_/¯)")
+	fmt.Println()
+}
