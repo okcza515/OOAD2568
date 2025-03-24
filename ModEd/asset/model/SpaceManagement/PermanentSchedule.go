5e40e0f0c4cc1fb1c5da8aafd559fef58f3adb78
Ginger <ginger@gmail.com>
2025-03-24 23:27:34 +0700

    key fix

--- ModEd/asset/model/SpaceManagement/PermanentSchedule.go
+++ ModEd/asset/model/SpaceManagement/PermanentSchedule.go
@@ -11,13 +11,12 @@ import (
 
 type PermanentSchedule struct {
 	gorm.Model
-	ScheduleID uint      `gorm:"type:integer ;primaryKey" json:"schedule_id" csv:"schedule_id"`
-	StartDate  time.Time `gorm:"type:timestamp" json:"start_date" csv:"start_date"`
-	EndDate    time.Time `gorm:"type:timestamp" json:"end_date" csv:"end_date"`
-	//RecurringDays []string           `gorm:"type:text[]" json:"recurring_days" csv:"recurring_days"`
+	ScheduleID  uint               `gorm:"type:integer ;primaryKey" json:"schedule_id" csv:"schedule_id"`
+	StartDate   time.Time          `gorm:"type:timestamp" json:"start_date" csv:"start_date"`
+	EndDate     time.Time          `gorm:"type:timestamp" json:"end_date" csv:"end_date"`
 	IsAvailable bool               `gorm:"type:boolean" json:"is_available" csv:"is_available"`
-	Faculty     master.Faculty     `gorm:"foreignKey:Name;references:Name" json:"faculty" csv:"faculty"`
-	Department  master.Department  `gorm:"foreignKey:Name;references:Name" json:"department" csv:"department"`
+	Faculty     master.Faculty     `gorm:"foreignKey:Faculty;references:Name" json:"faculty" csv:"faculty"`
+	Department  master.Department  `gorm:"foreignKey:Department;references:Name" json:"department" csv:"department"`
 	ProgramType master.ProgramType `gorm:"foreignKey:ProgramType;references:ProgramType" json:"program_type" csv:"program_type"`
 	Classroom   Room               `gorm:"foreignKey:RoomID;references:RoomID" json:"classroom" csv:"classroom"`
 	Course      curriculum.Course  `gorm:"foreignKey:CourseId;references:CourseId" json:"course" csv:"course"`
