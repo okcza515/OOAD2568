950042f378ce88cd93120eec5ccd6a38d5ccfdb5
Ginger <ginger@gmail.com>
2025-03-24 19:37:38 +0700

    fix: PermanentSchedule/Model

--- ModEd/asset/controller/SpaceManagement/PermanentBookingController.go
+++ ModEd/asset/controller/SpaceManagement/PermanentBookingController.go
@@ -2,13 +2,40 @@
 package spacemanagement
 
 import (
-	model "ModEd/asset/model/SpaeManagement"
+	model "ModEd/asset/model/SpaceManagement"
+	"errors"
+
 	"gorm.io/gorm"
-	"time"
 )
 
 type PermanentScheduleController struct {
 	DB *gorm.DB
 }
 
-func 
+func (c *PermanentScheduleController) CheckRoomInService(roomID uint) (*bool, error) {
+	var room model.Room
+
+	err := c.DB.First(&room, roomID).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, errors.New("Room not found")
+	} else if err != nil {
+		return nil, err
+	}
+
+	if room.IsRoomOutOfService {
+		return nil, errors.New("Room is out of service")
+	}
+
+	isInService := !room.IsRoomOutOfService
+	return &isInService, nil
+}
+
+func (c *PermanentScheduleController) CreateSubjectSchedule(body *model.PermanentSchedule) error {
+	if body.StartDate.IsZero() || body.EndDate.IsZero() {
+		return errors.New("Start date and end date are required")
+	}
+	result := c.DB.Create(body)
+	return result.Error
+}
+
+func (c *PermanentScheduleController) UpdateSubjectSchedule()
--- ModEd/asset/model/SpaceManagement/PermanentSchedule.go
+++ ModEd/asset/model/SpaceManagement/PermanentSchedule.go
@@ -1,4 +1,4 @@
-//MEP-1013
+// MEP-1013
 package spacemanagement
 
 import (
@@ -11,14 +11,15 @@ import (
 
 type PermanentSchedule struct {
 	gorm.Model
-	ScheduleID  uint               `gorm:"type:integer ;primaryKey" json:"schedule_id" csv:"schedule_id"`
-	StartDate   time.Time          `gorm:"type:timestamp" json:"start_date" csv:"start_date"`
-	EndDate     time.Time          `gorm:"type:timestamp" json:"end_date" csv:"end_date"`
-	IsAvailable bool               `gorm:"type:boolean" json:"is_available" csv:"is_available"`
-	Faculty     master.Faculty     `gorm:"foreignKey:FacultyID;references:FacultyID" json:"faculty" csv:"faculty"`
-	Department  master.Department  `gorm:"foreignKey:DepartmentID;references:DepartmentID" json:"department" csv:"department"`
-	ProgramType master.ProgramType `gorm:"foreignKey:ProgramType;references:ProgramType" json:"program_type" csv:"program_type"`
-	Classroom   Room               `gorm:"foreignKey:RoomID;references:RoomID" json:"classroom" csv:"classroom"`
-	Course      curriculum.Course  `gorm:"foreignKey:CourseID;references:CourseID" json:"course" csv:"course"`
-	Class       curriculum.Class   `gorm:"foreignKey:ClassID;references:ClassID" json:"class" csv:"class"`
+	ScheduleID    uint               `gorm:"type:integer ;primaryKey" json:"schedule_id" csv:"schedule_id"`
+	StartTime     time.Time          `gorm:"type:timestamp" json:"start_date" csv:"start_date"`
+	EndTime       time.Time          `gorm:"type:timestamp" json:"end_date" csv:"end_date"`
+	RecurringDays []string           `gorm:"type:text[]" json:"recurring_days" csv:"recurring_days"`
+	IsAvailable   bool               `gorm:"type:boolean" json:"is_available" csv:"is_available"`
+	Faculty       master.Faculty     `gorm:"foreignKey:FacultyID;references:FacultyID" json:"faculty" csv:"faculty"`
+	Department    master.Department  `gorm:"foreignKey:DepartmentID;references:DepartmentID" json:"department" csv:"department"`
+	ProgramType   master.ProgramType `gorm:"foreignKey:ProgramType;references:ProgramType" json:"program_type" csv:"program_type"`
+	Classroom     Room               `gorm:"foreignKey:RoomID;references:RoomID" json:"classroom" csv:"classroom"`
+	Course        curriculum.Course  `gorm:"foreignKey:CourseID;references:CourseID" json:"course" csv:"course"`
+	Class         curriculum.Class   `gorm:"foreignKey:ClassID;references:ClassID" json:"class" csv:"class"`
 }
