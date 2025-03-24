a5b1ffa388d6129ad6faa565f19145754af0b1af
Sikares <sikares.n@gmail.com>
2025-03-24 23:15:32 +0700

    fix: booking model

--- ModEd/asset/model/SpaceManagement/Booking.go
+++ ModEd/asset/model/SpaceManagement/Booking.go
@@ -9,12 +9,12 @@ import (
 
 type Booking struct {
 	gorm.Model
-	BookingID     uint      `gorm:"type:integer" json:"booking_id" csv:"booking_id"`
-	Room          Room      `gorm:"foreignKey:RoomID;references:RoomID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"room"`
-	UserID        uint      `gorm:"type:integer;not null" json:"user_id" csv:"user_id"`
-	UserRole      Role      `gorm:"type:text;not null" json:"user_role" csv:"user_role"`
-	StartDate     time.Time `gorm:"type:timestamp;not null" json:"start_date" csv:"start_date"`
-	EndDate       time.Time `gorm:"type:timestamp;not null" json:"end_date" csv:"end_date"`
-	IsAvailable   bool      `gorm:"type:boolean;not null" json:"is_available" csv:"is_available"`
-	EventName     string    `gorm:"type:text;not null" json:"event_name" csv:"event_name"`
+	BookingID     uint      	`gorm:"type:integer" json:"booking_id" csv:"booking_id"`
+	Room          []Room        `gorm:"foreignKey:RoomID;references:RoomID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"room"`
+	UserID        uint      	`gorm:"type:integer;not null" json:"user_id" csv:"user_id"`
+	UserRole      Role      	`gorm:"type:text;not null" json:"user_role" csv:"user_role"`
+	StartDate     time.Time 	`gorm:"type:timestamp;not null" json:"start_date" csv:"start_date"`
+	EndDate       time.Time 	`gorm:"type:timestamp;not null" json:"end_date" csv:"end_date"`
+	IsAvailable   bool      	`gorm:"type:boolean;not null" json:"is_available" csv:"is_available"`
+	EventName     string    	`gorm:"type:text;not null" json:"event_name" csv:"event_name"`
 }
