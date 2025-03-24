4811dd419f8a3e4e20ccf76cf24075a5b73697a3
Tongzkungz <tongs2004@gmail.com>
2025-03-24 15:28:27 +0700

    fix assetmanagement model

--- ModEd/asset/model/SpaceManagement/AssetManagement.go
+++ ModEd/asset/model/SpaceManagement/AssetManagement.go
@@ -2,27 +2,17 @@ package space
 
 import (
 	"ModEd/asset/model/asset"
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type AssetManagement struct {
 	gorm.Model
-	AssetManagementID  uint                   `gorm:"type:text;primaryKey"`
-	RoomID             uint                   `gorm:"type:text;not null;index" json:"room_id" csv:"room_id"`
+	AssetManagementID  uint                   `gorm:"type:int;primaryKey"`
+	RoomID             uint                   `gorm:"type:int;not null;index" json:"room_id" csv:"room_id"`
 	Room               Room                   `gorm:"foreignKey:RoomID;references:RoomID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"room"`
 	InstrumentID       uint                   `gorm:"type:text;not null;index" json:"instrument_id" csv:"instrument_id"`
 	Instrument         asset.Instrument       `gorm:"foreignKey:InstrumentID;references:InstrumentID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"instrument"`
 	InstrumentLabel    string                 `gorm:"type:text;not null" json:"instrument_label" csv:"instrument_label"`
-	SupplyID           uint                   `gorm:"type:text;not null;index" json:"supply_id" csv:"supply_id"`
 	Supply             asset.Supply           `gorm:"foreignKey:SupplyID;references:SupplyID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"supply"`
 	Quantity           int                    `gorm:"not null" json:"quantity" csv:"quantity"`
-	BorrowStatus       int                    `gorm:"type:integer;not null" json:"borrow_status" csv:"borrow_status"`
-	BorrowID           uint                   `gorm:"type:text;not null" json:"borrow_id" csv:"borrow_id"`
-	BorrowInstrument   asset.BorrowInstrument `gorm:"foreignKey:BorrowID;references:BorrowID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"borrow_instrument"`
-	BorrowDate         time.Time              `gorm:"type:timestamp" json:"borrow_date"`
-	ReturnDate         time.Time              `gorm:"type:timestamp" json:"return_date"`
-	ExpectedReturnDate time.Time              `gorm:"type:timestamp" json:"expected_return_date"`
-	IsLate             bool                   `json:"is_late"`
 }
