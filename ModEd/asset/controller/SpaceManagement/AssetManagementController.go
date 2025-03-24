698fff66c1cf652e88d30f768982087a17d0a6a4
Tongzkungz <tongs2004@gmail.com>
2025-03-24 20:45:39 +0700

    update assetmanagement controller

--- ModEd/asset/controller/SpaceManagement/AssetManagementController.go
+++ ModEd/asset/controller/SpaceManagement/AssetManagementController.go
@@ -26,10 +26,43 @@ func (c* AssetManagementController) getById(Id uint) (*model.AssetManagement, er
 	return assetInfo, result.Error
 }
 
-func (c* AssetManagementController) Create(payload *model.AssetManagement) error {
+func (c *AssetManagementController) GetByRoomId(roomID uint) (*[]model.AssetManagement, error) {
+    if roomID == 0 {
+        return nil, errors.New("No RoomID provided")
+    }
+    
+    assetList := new([]model.AssetManagement)
+    result := c.db.Where("room_id = ?", roomID).Find(&assetList)
+    
+    return assetList, result.Error
+}
+
+
+func (c* AssetManagementController) CreateAssetManagement(payload *model.AssetManagement) error {
 	if payload == nil {
 		return errors.New("Invalid asset data")
 	}
 	result := c.db.Create(payload)
+	return result.Error
+}
+
+func (c* AssetManagementController) UpdateAssetManagement(Id uint, payload *model.AssetManagement) error {
+	if payload == nil || Id == 0 {
+		return errors.New("Invalid info")
+	}
+	existingAsset := new(model.AssetManagement)
+	if err := c.db.First(existingAsset, Id).Error; err != nil {
+		return err
+	}
+	result := c.db.Model(existingAsset).Updates(payload)
+	return result.Error
+}
+
+func (c* AssetManagementController) DeleteAssetManagement(Id uint) error {
+	if Id == 0 {
+		return errors.New("No Id provide")
+	}
+	assetInfo := new(model.AssetManagement)
+	result := c.db.Delete(&assetInfo, Id)
 	return result.Error
 }
