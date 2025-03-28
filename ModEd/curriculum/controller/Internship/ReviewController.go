7ecf4a4822b3d2bd31f8d8dccb051d7b77a8e326
BaBoon <chitsanucha.onri@gmail.com>
2025-03-28 22:58:21 +0700

    feat:Report and Review

--- ModEd/curriculum/controller/Internship/ReviewController.go
+++ ModEd/curriculum/controller/Internship/ReviewController.go
@@ -7,19 +7,25 @@ import (
 	"gorm.io/gorm"
 )
 
-type ReportController struct {
+type ReviewController struct {
 	DB *gorm.DB
 }
 
-func (rc *ReportController) UpdateReportScore(studentID string, Score int) error {
+func (rc *ReviewController) UpdateReviewScore(studentID string, SupervisorScore int, MentorScore int) error {
 	var application model.InternshipApplication
 	if err := rc.DB.Where("student_code = ?", studentID).First(&application).Error; err != nil {
 		return fmt.Errorf("failed to find application for student_code '%s': %w", studentID, err)
 	}
-	var report model.InternshipReport
+	var report model.SupervisorReview
 	if err := rc.DB.Where("id = ?", application.InternshipReportId).First(&report).Error; err != nil {
 		return fmt.Errorf("failed to find report with id '%d': %w", application.InternshipReportId, err)
 	}
-	rc.DB.Model(&model.InternshipReport{}).Where("id = ?", report.ID).Update("ReportScore", Score)
+	if err := rc.DB.Model(&model.SupervisorReview{}).Where("id = ?", report.ID).Updates(map[string]interface{}{
+		"InstructorScore": SupervisorScore,
+		"MentorScore":     MentorScore,
+	}).Error; err != nil {
+		return fmt.Errorf("failed to update review scores for report id '%d': %w", report.ID, err)
+	}
+
 	return nil
 }
