19ddce0c4e0e7c6b06445582029d56be4ad8ef72
Emma Paris <Bewantza33@gmail.com>
2025-04-12 15:28:15 +0700

    Update Assignment.go

--- ModEd/eval/model/Assignment.go
+++ ModEd/eval/model/Assignment.go
@@ -1,5 +1,3 @@
-// Assignment
-// MEP-1006
 package model
 
 import (
@@ -11,18 +9,18 @@ import (
 )
 
 type Assignment struct {
-	// Instructor_Name  model.Instructor
 	gorm.Model
-	Title       string               
-	Description string                
-	StartDate   time.Time             
-	DueDate     time.Time              
-	Submission  []AssignmentSubmission 
+	InstructorId model.Instructor
+	Title        string
+	Description  string
+	StartDate    time.Time
+	DueDate      time.Time
+	Submission   []AssignmentSubmission
 }
 
-type AssignmentSubmission struct { 
+type AssignmentSubmission struct {
 	gorm.Model
-	StudentCode model.Student 
-	Content     string       
-	SubmittedAt string       
+	StudentCode model.Student
+	Content     string
+	SubmittedAt string
 }
