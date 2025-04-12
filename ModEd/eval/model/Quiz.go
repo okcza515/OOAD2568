92aa3e5cd9704cc9998205a9c09734de81a13da9
Mikipedia <Chayaphon.mick@gmail.com>
2025-04-12 16:19:55 +0700

    Update Quiz.go

--- ModEd/eval/model/Quiz.go
+++ ModEd/eval/model/Quiz.go
@@ -1,39 +1,36 @@
-// 65070503409 Chayaphon Chaisangkha
-// MEP-1006
 package model
 
 import (
 	"time"
 
-	"gorm.io/gorm"
-
 	"ModEd/common/model"
+
+	"ModEd/curriculum/model"
+
+	"gorm.io/gorm"
 )
 
-// Model to store quiz detail
 type Quiz struct {
 	gorm.Model
-	ID           uint             //Quiz ID
-	Title        string           //Quiz Title
-	Description  string           //Quiz Description
-	CourseId     model.Instructor //Course ID of quiz
-	InstructorId model.Instructor //Instructor ID of quiz
-	Released     bool             //to check if quiz is released
-	QuizStart    time.Time        //Time that quiz start
-	QuizEnd      time.Time        //Time that quiz finish
-	Status       string           //Quiz status
-	Submission   []QuizSubmission //Submission detail
-	TotalSubmit  uint             //Toltal number of submited quiz
+	ID           uint
+	Title        string
+	Description  string
+	CourseId     model.Course
+	InstructorId model.Instructor
+	Released     bool
+	QuizStart    time.Time
+	QuizEnd      time.Time
+	Status       string
+	Submission   []QuizSubmission
 }
 
-// Model to store quiz submission detail
 type QuizSubmission struct {
 	gorm.Model
-	SID        model.Student //Student ID
-	FirstName  model.Student //Student FirstName
-	LastName   model.Student //Student LastName
-	Email      model.Student //Student Email
-	Answers    string        //Student answer
-	Submitted  bool          //to check if student is submited
-	SubmitTime time.Time     //Student submit time
+	SID        model.Student
+	FirstName  model.Student
+	LastName   model.Student
+	Email      model.Student
+	Answers    string
+	Submitted  bool
+	SubmitTime time.Time
 }
