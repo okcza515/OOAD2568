9861e44638d5d82e26dee604e40110f1dc97a7ee
Billy19191 <bill.ratchanon@gmail.com>
2025-04-16 00:00:34 +0700

    ✨  feat: update room route

--- ModEd/asset/cli/SpaceManagement/handler/RoomHandler.go
+++ ModEd/asset/cli/SpaceManagement/handler/RoomHandler.go
@@ -125,6 +125,65 @@ func RoomHandler(facade *controller.SpaceManagementControllerFacade) {
 			util.PressEnterToContinue()
 		case "4":
 			fmt.Println("Update a Room")
+			fmt.Println("Please enter the ID of the Room:")
+			roomIdStr := util.GetCommandInput()
+			roomId, err := strconv.Atoi(roomIdStr)
+			if err != nil {
+				fmt.Println("Invalid ID")
+				util.PressEnterToContinue()
+				break
+			}
+			fmt.Println("Please enter the new name of the Room:")
+			roomName := util.GetCommandInput()
+			fmt.Println("Please enter the new room type: (Lecture / Laboratory / Office)")
+			roomTypeStr := util.GetCommandInput()
+			var roomType model.RoomTypeEnum
+			switch roomTypeStr {
+			case "Lecture":
+				roomType = "ROOM_LECTURE_ROOM"
+			case "Laboratory":
+				roomType = "ROOM_LAB_ROOM"
+			case "Office":
+				roomType = "ROOM_MEETING_ROOM"
+			default:
+				fmt.Println("Invalid room type. Using default type.")
+				roomType = "ROOM_LECTURE_ROOM"
+			}
+			fmt.Println("Please enter the new description:")
+			description := util.GetCommandInput()
+			fmt.Println("Please enter the new floor number:")
+			floor, errFloor := strconv.Atoi(util.GetCommandInput())
+			fmt.Println("Please enter the new building name:")
+			building := util.GetCommandInput()
+			fmt.Println("Please enter the new location:")
+			location := util.GetCommandInput()
+			fmt.Println("Please enter the new capacity:")
+			capacity, errCapacity := strconv.Atoi(util.GetCommandInput())
+			fmt.Println("Is the room out of service? (true/false):")
+			isOutOfServiceStr := util.GetCommandInput()
+
+			isOutOfService := isOutOfServiceStr == "true"
+			room := &model.Room{
+				RoomName:           roomName,
+				RoomType:           roomType,
+				Description:        description,
+				Floor:              floor,
+				Building:           building,
+				Location:           location,
+				Capacity:           capacity,
+				IsRoomOutOfService: isOutOfService,
+				Instrument:         nil,
+				Supply:             nil,
+			}
+			err = facade.Room.UpdateRoom(uint(roomId), room)
+			if err != nil || errFloor != nil || errCapacity != nil {
+				fmt.Println("Failed to update Room", err, errFloor, errCapacity)
+				util.PressEnterToContinue()
+				break
+
+			}
+			fmt.Println("Room updated successfully")
+			util.PressEnterToContinue()
 		case "5":
 			fmt.Println("Delete a Room")
 		}
