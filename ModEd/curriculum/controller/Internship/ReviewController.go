bd647947ff8bf2ca6e446a9c872bfa002c7fa4c8
BaBoon <chitsanucha.onri@gmail.com>
2025-04-19 22:31:36 +0700

    feat: fix report review

--- ModEd/curriculum/controller/Internship/ReviewController.go
+++ ModEd/curriculum/controller/Internship/ReviewController.go
@@ -13,11 +13,11 @@ type ReviewController struct {
 
 func (rc *ReviewController) UpdateReviewScore(studentID string, SupervisorScore int, MentorScore int) error {
 	var application model.InternshipApplication
-	if err := rc.DB.Where("student_code = ?", studentID).First(&application).Error; err != nil {
+	if err := rc.DB.Where("student_code = ?", studentID).Last(&application).Error; err != nil {
 		return fmt.Errorf("failed to find application for student_code '%s': %w", studentID, err)
 	}
 	var report model.SupervisorReview
-	if err := rc.DB.Where("id = ?", application.InternshipReportId).First(&report).Error; err != nil {
+	if err := rc.DB.Where("id = ?", application.InternshipReportId).Last(&report).Error; err != nil {
 		return fmt.Errorf("failed to find report with id '%d': %w", application.InternshipReportId, err)
 	}
 	if err := rc.DB.Model(&model.SupervisorReview{}).Where("id = ?", report.ID).Updates(map[string]interface{}{
