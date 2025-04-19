911a45acde3c5ac557e4cfcf058ea07814872798
wavekanit <wavekanit@hotmail.com>
2025-04-19 22:37:46 +0700

    feat: add ifNotEmpty for un-updated field

--- ModEd/hr/cli/commands/UpdateStudentCommand.go
+++ ModEd/hr/cli/commands/UpdateStudentCommand.go
@@ -1,63 +1,60 @@
 package commands
 
 import (
-    "flag"
-    "fmt"
-    "os"
-    "ModEd/hr/controller"
-    hrUtil "ModEd/hr/util"
-    
-
+	"ModEd/hr/controller"
+	hrModel "ModEd/hr/model"
+	hrUtil "ModEd/hr/util"
+	"flag"
+	"fmt"
+	"os"
 )
+
 func (c *UpdateStudentCommand) Run(args []string) {
-    fs := flag.NewFlagSet("update", flag.ExitOnError)
-    studentID := fs.String("id", "", "Student ID to update")
-    firstName := fs.String("fname", "", "New First Name value")
-    lastName := fs.String("lname", "", "New Last Name value")
-    gender := fs.String("gender", "", "New Gender value")
-    citizenID := fs.String("citizenID", "", "New Citizen ID value")
-    phoneNumber := fs.String("phone", "", "New Phone Number value")
-    emailStudent := fs.String("email", "", "New Email value")
-    fs.Parse(args)
+	fs := flag.NewFlagSet("update", flag.ExitOnError)
+	studentID := fs.String("id", "", "Student ID to update")
+	firstName := fs.String("fname", "", "New First Name value")
+	lastName := fs.String("lname", "", "New Last Name value")
+	gender := fs.String("gender", "", "New Gender value")
+	citizenID := fs.String("citizenID", "", "New Citizen ID value")
+	phoneNumber := fs.String("phone", "", "New Phone Number value")
+	emailStudent := fs.String("email", "", "New Email value")
+	fs.Parse(args)
 
-    if err := hrUtil.ValidateRequiredFlags(fs, []string{"id"}); err != nil {
-        fmt.Printf("Validation error: %v\n", err)
-        fs.Usage()
-        os.Exit(1)
-    }
+	if err := hrUtil.ValidateRequiredFlags(fs, []string{"id"}); err != nil {
+		fmt.Printf("Validation error: %v\n", err)
+		fs.Usage()
+		os.Exit(1)
+	}
 
-    db := hrUtil.OpenDatabase(*hrUtil.DatabasePath)
+	db := hrUtil.OpenDatabase(*hrUtil.DatabasePath)
 
 	hrFacade := controller.NewHRFacade(db)
 	studentInfo, err := hrFacade.GetStudentById(*studentID)
-    if err != nil {
-        fmt.Printf("Error retrieving student with ID %s: %v\n", *studentID, err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Printf("Error retrieving student with ID %s: %v\n", *studentID, err)
+		os.Exit(1)
+	}
 
-    if *firstName != "" {
-        studentInfo.FirstName = *firstName
-    }
-    if *lastName != "" {
-        studentInfo.LastName = *lastName
-    }
-    if *gender != "" {
-        studentInfo.Gender = *gender
-    }
-    if *citizenID != "" {
-        studentInfo.CitizenID = *citizenID
-    }
-    if *phoneNumber != "" {
-        studentInfo.PhoneNumber = *phoneNumber
-    }
-    if *emailStudent != "" {
-        studentInfo.Email = *emailStudent
-    }
+	studentInfoBuilder := hrModel.NewStudentInfoBuilder().
+		WithFirstName(ifNotEmpty(*firstName, studentInfo.FirstName)).
+		WithLastName(ifNotEmpty(*lastName, studentInfo.LastName)).
+		WithGender(ifNotEmpty(*gender, studentInfo.Gender)).
+		WithCitizenID(ifNotEmpty(*citizenID, studentInfo.CitizenID)).
+		WithPhoneNumber(ifNotEmpty(*phoneNumber, studentInfo.PhoneNumber)).
+		WithEmail(ifNotEmpty(*emailStudent, studentInfo.Email)).
+		Build()
 
-	if err := hrFacade.UpdateStudent(studentInfo); err != nil {
-        fmt.Printf("Failed to update student info: %v\n", err)
-        os.Exit(1)
-    }
+	if err := hrFacade.UpdateStudent(studentInfoBuilder); err != nil {
+		fmt.Printf("Failed to update student info: %v\n", err)
+		os.Exit(1)
+	}
 
-    fmt.Println("Student updated successfully!")
+	fmt.Println("Student updated successfully!")
+}
+
+func ifNotEmpty(newValue, fallbackValue string) string {
+	if newValue != "" {
+		return newValue
+	}
+	return fallbackValue
 }
