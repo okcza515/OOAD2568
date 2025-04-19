546d48ac2854ffb0745bba147669f1f059f74200
Trollface8D <kenpontakorn@gmail.com>
2025-04-19 22:30:15 +0700

    MEP-1014: adjust itemrequest model

--- ModEd/asset/model/Procurement/ItemRequest.go
+++ ModEd/asset/model/Procurement/ItemRequest.go
@@ -2,17 +2,19 @@
 package model
 
 import (
+	master "ModEd/common/model"
 	"time"
 
 	"gorm.io/gorm"
 )
 
 type ItemRequest struct {
-	ItemRequestID uint              `gorm:"primaryKey"`                 // Auto-increment ID
-	DepartmentID  uint              `gorm:"type:varchar(255);not null"` // Foreign key (uint from gorm.Model)
+	ItemRequestID uint              `gorm:"primaryKey"` // Auto-increment ID
 	Status        ItemRequestStatus `gorm:"type:varchar(50);default:'draft'"`
 	Items         []ItemDetail      `gorm:"foreignKey:ItemRequestID"`
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
-	DeleteAt      gorm.DeletedAt `gorm:"index"` // Soft delete
+	DeleteAt      gorm.DeletedAt `gorm:"index"`                      // Soft delete
+	DepartmentID  uint           `gorm:"type:varchar(255);not null"` // Foreign key (uint from gorm.Model)
+	Departments   master.Department
 }
