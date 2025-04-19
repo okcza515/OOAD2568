917627dc4fdbaad204982890447d102524974427
Manassanan Nilruang <manatsanan.ninrueang@gmail.com>
2025-04-19 21:12:05 +0700

    split updatestudent and Deletestudent func in cli

--- ModEd/hr/cli/commands/CommandsInterface.go
+++ ModEd/hr/cli/commands/CommandsInterface.go
@@ -4,3 +4,5 @@ type ListStudentsCommand struct{}
 type AddStudentCommand struct{}
 type UpdateStudentStatus struct{}
 type ImportStudents struct{}
+type DeleteStudentCommand struct{}
+type UpdateStudentCommand struct{}
