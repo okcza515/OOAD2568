19a84c16a4773c1a7b0d2c3169d987d18101ee96
ZTPG101 <110453102+ZTPG101@users.noreply.github.com>
2025-04-19 22:05:14 +0700

    update model

--- ModEd/asset/model/Procurement/Quotation.go
+++ ModEd/asset/model/Procurement/Quotation.go
@@ -5,11 +5,12 @@ import (
 	"gorm.io/gorm"
 )
 
-type Supplier struct {
-	SupplierID  uint           `gorm:"primaryKey"`
+type Quotation struct {
+	QuotationID uint           `gorm:"primaryKey"`
 	Name        string         `gorm:"type:varchar(255);not null"`
 	ContactInfo string         `gorm:"type:text"`
 	Performance float64        `gorm:"not null"`
 	DeletedAt   gorm.DeletedAt `gorm:"index"`
-	Quotations  []Quotation
+	SupplierID  uint           `gorm:"index"`
+	Supplier    Supplier
 }
