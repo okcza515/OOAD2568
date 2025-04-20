d308d5b55971568b5d4e072243f328435d761b12
Just3rd <kantapong.3rd@gmail.com>
2025-04-21 00:27:11 +0700

    :recycle: refactor: add interface + BaseController

--- ModEd/asset/controller/InstrumentLogController.go
+++ ModEd/asset/controller/InstrumentLogController.go
@@ -2,19 +2,28 @@ package controller
 
 import (
 	"ModEd/asset/model"
+	"ModEd/core"
 	"gorm.io/gorm"
 	"strconv"
 )
 
 type InstrumentLogController struct {
 	db *gorm.DB
+	*core.BaseController
 }
 
-func (c *InstrumentLogController) getAll() (*[]model.InstrumentLog, error) {
+type InstrumentLogControllerInterface interface {
+	getAll() ([]model.InstrumentLog, error)
+	ListAll() ([]string, error)
+	Insert(data core.RecordInterface) error
+	RetrieveByID(id uint, preloads ...string) (*core.RecordInterface, error)
+}
+
+func (c *InstrumentLogController) getAll() ([]model.InstrumentLog, error) {
 	logs := new([]model.InstrumentLog)
 	result := c.db.Find(&logs)
 
-	return logs, result.Error
+	return *logs, result.Error
 }
 
 func (c *InstrumentLogController) ListAll() ([]string, error) {
--- ModEd/asset/controller/MigrationController.go
+++ ModEd/asset/controller/MigrationController.go
@@ -1,7 +1,7 @@
 package controller
 
 import (
-	model2 "ModEd/asset/model"
+	model "ModEd/asset/model"
 	"errors"
 
 	"gorm.io/gorm"
@@ -11,16 +11,22 @@ type MigrationController struct {
 	db *gorm.DB
 }
 
+type MigrationControllerInterface interface {
+	migrateToDB() error
+	dropDB() error
+	resetDB() error
+}
+
 func (c *MigrationController) migrateToDB() error {
 	err := c.db.AutoMigrate(
-		&model2.InstrumentLog{},
-		&model2.Instrument{},
-		&model2.BorrowInstrument{},
-		&model2.Category{},
-		&model2.Instrument{},
-		&model2.InstrumentLog{},
-		&model2.Supply{},
-		&model2.SupplyLog{},
+		&model.InstrumentLog{},
+		&model.Instrument{},
+		&model.BorrowInstrument{},
+		&model.Category{},
+		&model.Instrument{},
+		&model.InstrumentLog{},
+		&model.Supply{},
+		&model.SupplyLog{},
 	)
 	if err != nil {
 		return errors.New("err: migration failed")
@@ -31,14 +37,14 @@ func (c *MigrationController) migrateToDB() error {
 
 func (c *MigrationController) dropDB() error {
 	err := c.db.Migrator().DropTable(
-		&model2.InstrumentLog{},
-		&model2.Instrument{},
-		&model2.BorrowInstrument{},
-		&model2.Category{},
-		&model2.Instrument{},
-		&model2.InstrumentLog{},
-		&model2.Supply{},
-		&model2.SupplyLog{},
+		&model.InstrumentLog{},
+		&model.Instrument{},
+		&model.BorrowInstrument{},
+		&model.Category{},
+		&model.Instrument{},
+		&model.InstrumentLog{},
+		&model.Supply{},
+		&model.SupplyLog{},
 	)
 	if err != nil {
 		return err
