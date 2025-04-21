ed9d3219c1df3387e3211fbb2f3cd6dc7d8da88b
PANURUT SIRINAPAISAN <panurut.siri@kmutt.ac.th>
2025-04-21 16:13:04 +0700

    chore(wil-project): fix wil-project application

--- ModEd/curriculum/controller/WILProjectApplicationController.go
+++ ModEd/curriculum/controller/WILProjectApplicationController.go
@@ -1,43 +1,55 @@
 package controller
 
 import (
-	commonModel "ModEd/common/model"
+	"ModEd/core"
 	model "ModEd/curriculum/model"
 
 	"gorm.io/gorm"
 )
 
 type WILProjectApplicationController struct {
-	Connector *gorm.DB
+	connector *gorm.DB
+	*core.BaseController
+}
+
+type WILProjectApplicationControllerInterface interface {
+	RegisterWILProjectsApplication(projects []core.RecordInterface)
+	Insert(data core.RecordInterface) error
+	UpdateByID(data core.RecordInterface) error
+	RetrieveByID(id uint, preloads ...string) (*core.RecordInterface, error)
+	DeleteByID(id uint) error
+	ListPagination(condition map[string]interface{}, page int, pageSize int)
 }
 
 func CreateWILProjectApplicationController(connector *gorm.DB) *WILProjectApplicationController {
-	wil := WILProjectApplicationController{Connector: connector}
-	connector.AutoMigrate(&commonModel.Student{}, &model.WILProjectApplication{}, &model.WILProjectMember{})
-
+	wil := WILProjectApplicationController{connector: connector, BaseController: core.NewBaseController("WILProjectApplication", connector)}
 	return &wil
+}
+
+func (repo WILProjectApplicationController) RegisterWILProjectApplication(application *model.WILProjectApplication) {
+	repo.connector.Create(application)
 }
 
 func (repo WILProjectApplicationController) RegisterWILProjectApplications(applications []*model.WILProjectApplication) {
 	for _, application := range applications {
-		repo.Connector.Create(application)
+		repo.connector.Create(application)
 	}
 }
 
 func (repo WILProjectApplicationController) GetAllWILProjectApplications() ([]*model.WILProjectApplication, error) {
 	applications := []*model.WILProjectApplication{}
-	result := repo.Connector.Find(&applications)
+	result := repo.connector.Find(&applications)
 	return applications, result.Error
 }
 
 func (repo WILProjectApplicationController) GetWILProjectApplicationByID(id uint) (*model.WILProjectApplication, error) {
 	application := &model.WILProjectApplication{}
-	result := repo.Connector.Where("WILProjectApplicationId = ?", id).First(application)
+	result := repo.connector.Where("WILProjectApplicationId = ?", id).First(application)
 	return application, result.Error
 }
 
 func (repo WILProjectApplicationController) UpdateWILProjectApplication(application *model.WILProjectApplication) error {
-	result := repo.Connector.Save(application)
+	result := repo.connector.Save(application)
 	if result.Error != nil {
 		return result.Error
 	}
