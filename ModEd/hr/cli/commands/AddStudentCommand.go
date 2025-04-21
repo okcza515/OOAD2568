8f895df3445ee5b632214a45c59c892d8fc855af
encall <apichataimimpat@gmail.com>
2025-04-21 23:46:16 +0700

    Revert "refactor: move command files to main package"
    This reverts commit 0fc821455b2dd509a70657541e5640ff8bb7722d.

--- ModEd/hr/cli/commands/AddStudentCommand.go
+++ ModEd/hr/cli/commands/AddStudentCommand.go
@@ -1,4 +1,4 @@
-package main
+package commands
 
 import (
 	commonController "ModEd/common/controller"
---
+++ ModEd/hr/cli/commands/RequestCommand.go
@@ -0,0 +1,42 @@
+package commands
+
+import (
+	"fmt"
+	"strings"
+
+	"gorm.io/gorm"
+)
+
+func (c *RequestCommand) Execute(args []string, tx *gorm.DB) error {
+    if len(args) < 2 {
+        return fmt.Errorf("usage: request {student|instructor} {resign|leave} [options]")
+    }
+
+    target := strings.ToLower(args[0])
+    action := strings.ToLower(args[1])
+
+    switch target {
+    case "student":
+        switch action {
+        case "resign":
+            return requestResignationStudent(args[2:], tx)
+        case "leave":
+            return requestLeaveStudent(args[2:], tx)
+        default:
+            return fmt.Errorf("unknown action for student: %s", action)
+        }
+    case "instructor":
+        switch action {
+        case "resign":
+            return requestResignationInstructor(args[2:], tx)
+		case "leave":
+            return requestLeaveInstructor(args[2:], tx)
+		case "raise":
+			return requestRaiseInstructor(args[2:], tx)
+        default:
+            return fmt.Errorf("unknown action for instructor: %s", action)
+        }
+    default:
+        return fmt.Errorf("unknown target: %s", target)
+    }
+}
