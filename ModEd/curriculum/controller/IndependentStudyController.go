93d44934ae0a5d2901c69439212722c834a2beed
Sorn.Udom <sorrawit.udom@kmutt.ac.th>
2025-04-21 17:04:17 +0700

    [Add] IndependentControllerInterface

--- ModEd/curriculum/controller/IndependentStudyController.go
+++ ModEd/curriculum/controller/IndependentStudyController.go
@@ -1,45 +1,55 @@
 package controller
 
 import (
+	"ModEd/core"
 	model "ModEd/curriculum/model"
 
-	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type IndependentStudyController struct {
+	*core.BaseController
 	Connector *gorm.DB
+}
+
+type IndependentStudyControllerInterface interface {
+	Insert(data core.RecordInterface) error
+	UpdateByID(data core.RecordInterface) error
+	RetrieveByID(id uint, preloads ...string) (*core.RecordInterface, error)
+	List(condition map[string]interface{}) ([]core.RecordInterface, error)
+	ListPagination(condition map[string]interface{}, page int, pageSize int) (*core.PaginationResult, error)
 }
 
 func CreateIndependentStudyController(connector *gorm.DB) *IndependentStudyController {
 	connector.AutoMigrate(&model.IndependentStudy{})
 	return &IndependentStudyController{
-		Connector: connector,
+		Connector:      connector,
+		BaseController: core.NewBaseController("IndependentStudy", connector),
 	}
 }
 
 func (repo IndependentStudyController) CreateIndependentStudy(independentStudies *[]model.IndependentStudy) {
 	for _, independentStudy := range *independentStudies {
-		repo.Connector.Create(independentStudy)
+		repo.BaseController.Insert(&independentStudy)
 	}
 }
 
-func (repo IndependentStudyController) GetIndenpendentStudyByID(id uuid.UUID) (*model.IndependentStudy, error) {
-	independentStudy := new(model.IndependentStudy)
-	result := repo.Connector.First(&independentStudy, "independent_study_id = ?", id)
-	return independentStudy, result.Error
-}
+// func (repo IndependentStudyController) GetIndenpendentStudyByID(id uint) (*model.IndependentStudy, error) {
+// 	independentStudy := new(model.IndependentStudy)
+// 	result := repo.Connector.First(&independentStudy, "id = ?", id)
+// 	return independentStudy, result.Error
+// }
 
-func (repo IndependentStudyController) GetAllIndenpendentStudy() ([]*model.IndependentStudy, error) {
-	independentStudy := []*model.IndependentStudy{}
-	result := repo.Connector.Find(&independentStudy, "DeletedAt IS NULL")
-	return independentStudy, result.Error
-}
+// func (repo IndependentStudyController) GetAllIndenpendentStudy() ([]*model.IndependentStudy, error) {
+// 	independentStudy := []*model.IndependentStudy{}
+// 	result := repo.Connector.Find(&independentStudy, "DeletedAt IS NULL")
+// 	return independentStudy, result.Error
+// }
 
-func (repo IndependentStudyController) UpdateIndenpendentStudy(updatedStudy *model.IndependentStudy) error {
-	result := repo.Connector.Save(updatedStudy)
-	if result.Error != nil {
-		return result.Error
-	}
-	return nil
-}
+// func (repo IndependentStudyController) UpdateIndenpendentStudy(updatedStudy *model.IndependentStudy) error {
+// 	result := repo.Connector.Save(updatedStudy)
+// 	if result.Error != nil {
+// 		return result.Error
+// 	}
+// 	return nil
+// }
