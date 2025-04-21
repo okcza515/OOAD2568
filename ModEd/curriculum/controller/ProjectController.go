03a837207e31b14fa30992175d5d522dba1fe13e
ffaann02 <rudfaanmaimahad@gmail.com>
2025-04-21 21:50:40 +0700

    refactor: change controller name convention

--- ModEd/curriculum/controller/ProjectController.go
+++ ModEd/curriculum/controller/ProjectController.go
@@ -21,7 +21,7 @@ type ProjectController struct {
 	Connector *gorm.DB
 }
 
-func NewProjectController(db *gorm.DB) ProjectControllerService {
+func CreateProjectController(db *gorm.DB) ProjectControllerService {
 	return &ProjectController{
 		BaseController: core.NewBaseController("Project", db),
 		Connector:      db,
--- ModEd/curriculum/controller/StudentAdvisingController.go
+++ ModEd/curriculum/controller/StudentAdvisingController.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"ModEd/core"
 	model "ModEd/curriculum/model"
 	"errors"
 
@@ -10,27 +11,30 @@ import (
 type StudentWorkloadService interface {
 	CreateStudentAdvisor(studentAdvisor model.StudentAdvisor) error
 	GetStudentUnderSupervisionByInstructorId(instructorId uint) ([]model.StudentAdvisor, error)
-
 	CreateStudentRequest(studentRequest model.StudentRequest) error
 	GetStudentRequestsByInstructorId(instructorId uint) ([]model.StudentRequest, error)
 	ReviewStudentRequest(id uint, review string, comment string) error
 }
 
 type StudentWorkloadController struct {
-	DB *gorm.DB
+	*core.BaseController
+	Connector *gorm.DB
 }
 
-func NewStudentWorkloadController(db *gorm.DB) StudentWorkloadService {
-	return &StudentWorkloadController{DB: db}
+func CreateStudentWorkloadController(db *gorm.DB) StudentWorkloadService {
+	return &StudentWorkloadController{
+		BaseController: core.NewBaseController("StudentWorkload", db),
+		Connector:      db,
+	}
 }
 
 func (swc *StudentWorkloadController) CreateStudentAdvisor(studentAdvisor model.StudentAdvisor) error {
-	return swc.DB.Create(&studentAdvisor).Error
+	return swc.Connector.Create(&studentAdvisor).Error
 }
 
 func (swc *StudentWorkloadController) GetStudentUnderSupervisionByInstructorId(instructorId uint) ([]model.StudentAdvisor, error) {
 	var studentAdvisors []model.StudentAdvisor
-	err := swc.DB.Where("instructor_id = ?", instructorId).Preload("Students").Find(&studentAdvisors).Error
+	err := swc.Connector.Where("instructor_id = ?", instructorId).Preload("Students").Find(&studentAdvisors).Error
 	if err != nil {
 		return nil, err
 	}
@@ -38,12 +42,12 @@ func (swc *StudentWorkloadController) GetStudentUnderSupervisionByInstructorId(i
 }
 
 func (swc *StudentWorkloadController) CreateStudentRequest(studentRequest model.StudentRequest) error {
-	return swc.DB.Create(&studentRequest).Error
+	return swc.Connector.Create(&studentRequest).Error
 }
 
 func (swc *StudentWorkloadController) GetStudentRequestsByInstructorId(instructorId uint) ([]model.StudentRequest, error) {
 	var studentRequests []model.StudentRequest
-	err := swc.DB.Where("instructor_id = ?", instructorId).Find(&studentRequests).Error
+	err := swc.Connector.Where("instructor_id = ?", instructorId).Find(&studentRequests).Error
 	if err != nil {
 		return nil, err
 	}
@@ -52,7 +56,7 @@ func (swc *StudentWorkloadController) GetStudentRequestsByInstructorId(instructo
 
 func (swc *StudentWorkloadController) ReviewStudentRequest(id uint, review string, comment string) error {
 	var studentRequest model.StudentRequest
-	err := swc.DB.First(&studentRequest, id).Error
+	err := swc.Connector.First(&studentRequest, id).Error
 	if err != nil {
 		return err
 	}
@@ -61,5 +65,5 @@ func (swc *StudentWorkloadController) ReviewStudentRequest(id uint, review strin
 	}
 	studentRequest.Review = review
 	studentRequest.Comment = comment
-	return swc.DB.Save(&studentRequest).Error
+	return swc.Connector.Save(&studentRequest).Error
 }
