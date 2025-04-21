85cfcd9bafe38bcdb5064d5dd0351b77c0a11b75
encall <apichataimimpat@gmail.com>
2025-04-21 23:29:57 +0700

    fix: fix public private method

--- ModEd/hr/controller/RaiseHRController.go
+++ ModEd/hr/controller/RaiseHRController.go
@@ -9,26 +9,26 @@ type RaiseHRController struct {
 	db *gorm.DB
 }
 
-func CreateRaiseHRController(db *gorm.DB) *RaiseHRController {
+func createRaiseHRController(db *gorm.DB) *RaiseHRController {
 	db.AutoMigrate(&model.RequestRaise{})
 	return &RaiseHRController{db: db}
 }
 
-func (c *RaiseHRController) Insert(req *model.RequestRaise) error {
+func (c *RaiseHRController) insert(req *model.RequestRaise) error {
 	return c.db.Create(req).Error
 }
 
-func (c *RaiseHRController) GetByID(id uint) (*model.RequestRaise, error) {
+func (c *RaiseHRController) getByID(id uint) (*model.RequestRaise, error) {
 	var raise model.RequestRaise
 	err := c.db.First(&raise, id).Error
 	return &raise, err
 }
 
-func (c *RaiseHRController) UpdateStatus(id uint, status string) error {
+func (c *RaiseHRController) updateStatus(id uint, status string) error {
 	return c.db.Model(&model.RequestRaise{}).Where("id = ?", id).Update("status", status).Error
 }
 
-func (c *RaiseHRController) GetAll() ([]model.RequestRaise, error) {
+func (c *RaiseHRController) getAll() ([]model.RequestRaise, error) {
 	var requests []model.RequestRaise
 	err := c.db.Find(&requests).Error
 	return requests, err
