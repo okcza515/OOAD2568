3ca1428907ceb4db7d4f40680e801b62c7c9ae66
chaiyapatoam <chaiyapatoam12@gmail.com>
2025-04-21 09:24:45 +0700

    feat: WIL project application implement advisor(Instructor) common model

--- ModEd/curriculum/model/wil-project/WILProjectApplication.go
+++ ModEd/curriculum/model/wil-project/WILProjectApplication.go
@@ -1,15 +1,19 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	commonModel "ModEd/common/model"
+
+	"github.com/google/uuid"
+)
 
 type WILProjectApplication struct {
-	WILProjectApplicationId uuid.UUID          `gorm:"primaryKey;unique"`
-	ProjectName             string             `gorm:"not null"`
-	ProjectDetail           string             `gorm:"not null"`
-	Company                 string             `gorm:"not null"`
-	Mentor                  string             `gorm:"not null"`
-	Students                []WILProjectMember `gorm:"foreignKey:WILProjectApplicationId"`
-	// Advisor                 Instructor      `json:"Advisor"` //TODO: Change this Instructor model to modelCommon.Instructor when its already implemented
-	ApplicationStatus string `gorm:"not null"`
-	TurninDate        string `gorm:"not null"`
+	WILProjectApplicationId uuid.UUID              `gorm:"primaryKey;unique"`
+	ProjectName             string                 `gorm:"not null"`
+	ProjectDetail           string                 `gorm:"not null"`
+	Company                 string                 `gorm:"not null"`
+	Mentor                  string                 `gorm:"not null"`
+	Students                []WILProjectMember     `gorm:"foreignKey:WILProjectApplicationId"`
+	Advisor                 commonModel.Instructor `json:"Advisor"`
+	ApplicationStatus       string                 `gorm:"not null"`
+	TurninDate              string                 `gorm:"not null"`
 }
--- ModEd/curriculum/model/wil-project/WILProjectMember.go
+++ ModEd/curriculum/model/wil-project/WILProjectMember.go
@@ -1,13 +1,13 @@
 package model
 
 import (
-	"ModEd/common/model"
+	commonModel "ModEd/common/model"
 
 	"github.com/google/uuid"
 )
 
 type WILProjectMember struct {
-	WILProjectApplicationId uuid.UUID     `json:"-" gorm:"primaryKey"`
-	StudentId               string        `json:"StudentId" gorm:"primaryKey"`
-	Student                 model.Student `json:"Student" gorm:"foreignKey:StudentId;references:SID"`
+	WILProjectApplicationId uuid.UUID           `json:"-" gorm:"primaryKey"`
+	StudentId               string              `json:"StudentId" gorm:"primaryKey"`
+	Student                 commonModel.Student `json:"Student" gorm:"foreignKey:StudentId;references:SID"`
 }
