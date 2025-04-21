6353e9e0a919c9bbaa166ad3f4ee0dab352bfdac
PANURUT SIRINAPAISAN <panurut.siri@kmutt.ac.th>
2025-04-21 18:21:30 +0700

    chore(wil-project): move migration db to migrationController

--- ModEd/curriculum/cli/cli.go
+++ ModEd/curriculum/cli/cli.go
@@ -6,9 +6,7 @@ import (
 	wilproject "ModEd/curriculum/cli/wil-project"
 	controller "ModEd/curriculum/controller/curriculum"
 	"ModEd/curriculum/controller/migration"
-	"ModEd/curriculum/model"
 	"ModEd/curriculum/utils"
-	"errors"
 	"fmt"
 
 	"gorm.io/gorm"
@@ -29,12 +27,6 @@ func main() {
 	})
 	if err != nil {
 		panic(err)
-	}
-
-	err = migrateDB(db)
-
-	if err != nil {
-		fmt.Println(err.Error())
 	}
 
 	curriculumController := controller.NewCurriculumController(db)
@@ -81,23 +73,4 @@ func getUserChoice() string {
 	fmt.Print("Enter your choice: ")
 	fmt.Scanln(&choice)
 	return choice
-}
-
-func migrateDB(db *gorm.DB) error {
-	err := db.AutoMigrate(
-		&model.Curriculum{},
-		&model.Course{},
-		&model.Class{},
-		&model.WILProjectCourse{},
-		&model.WILProjectClass{},
-		&model.WILProjectMember{},
-		&model.WILProjectApplication{},
-		&model.WILProject{},
-		&model.IndependentStudy{},
-	)
-	if err != nil {
-		return errors.New("err: migration failed")
-	}
-
-	return nil
 }
