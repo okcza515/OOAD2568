b2d4d4173b892a8fb1d32f6e6129a7ff3f878c4b
Sikares <sikares.n@gmail.com>
2025-04-22 15:23:33 +0700

    fix: CLI

--- ModEd/asset/model/SpaceManagement/InstrumentManagement.go
+++ ModEd/asset/model/SpaceManagement/InstrumentManagement.go
@@ -12,7 +12,7 @@ type InstrumentManagement struct {
 	BorrowUserID       uint                   `gorm:"type:integer" json:"borrow_id" csv:"borrow_id"`
 	BorrowInstrument   model.BorrowInstrument `gorm:"foreignKey:ID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"borrow_instrument"`
 	RoomID             uint                   `gorm:"type:integer;not null;index" json:"room_id" csv:"room_id"`
-	Room               Room                   `gorm:"foreignKey:RoomID;references:RoomID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"room"`
+	Room               Room                   `gorm:"foreignKey:ID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"room"`
 	InstrumentSerialID uint                   `gorm:"type:integer;not null;index" json:"instrument_id" csv:"instrument_id"`
 	Instrument         model.Instrument       `gorm:"foreignKey:ID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"instrument"`
 	InstrumentLabel    string                 `gorm:"type:text;not null" json:"instrument_label" csv:"instrument_label"`
