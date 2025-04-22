87d45ca63bfc0c42916ebc9572e0182c7c3652c5
Chanawat3445 <veerawatlwat@gmail.com>
2025-04-22 23:04:29 +0700

    .

---
+++ ModEd/eval/cli/evaluation/EvaluationTest.go
@@ -0,0 +1,15 @@
+// MEP-1006
+package main
+
+import (
+	"ModEd/eval/cli"
+	"fmt"
+	// "gorm.io/gorm"
+)
+
+func main() {
+	fmt.Println("Hello from Main")
+
+	cli.PrintAssignment()
+	cli.PrintEvaluation()
+}
