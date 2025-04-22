9d1a51b50f93db9712c8d68feba4c8f5a12fc87e
kasinphatspam <kasinphat@icloud.com>
2025-04-22 13:44:47 +0700

    fix: migrate 2 times

--- ModEd/project/cli/ProjectManagerCLI.go
+++ ModEd/project/cli/ProjectManagerCLI.go
@@ -14,32 +14,6 @@ import (
 func main() {
 	db := utils.OpenDatabase("project.db")
 	db.Exec("PRAGMA foreign_keys = ON;")
-	migration := true
-
-	if migration {
-		if err := db.AutoMigrate(
-			&model.SeniorProject{},
-			&model.GroupMember{},
-			&model.Advisor{},
-			&model.Assessment{},
-			&model.AssessmentCriteria{},
-			&model.Assignment{},
-			&model.Committee{},
-			&model.Presentation{},
-			&model.Progress{},
-			&model.Report{},
-			&model.ScoreAssessmentAdvisor{},
-			&model.ScoreAssessmentCommittee{},
-			&model.ScoreAssignmentAdvisor{},
-			&model.ScoreAssignmentCommittee{},
-			&model.ScorePresentationAdvisor{},
-			&model.ScorePresentationCommittee{},
-			&model.ScoreReportAdvisor{},
-			&model.ScoreReportCommittee{},
-		); err != nil {
-			panic(err)
-		}
-	}
 
 	seniorProjectController := controller.NewSeniorProjectController(db)
 	advisorController := controller.NewAdvisorController(db)
