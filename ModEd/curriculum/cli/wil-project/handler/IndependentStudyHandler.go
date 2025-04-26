7b871e1244dea1918246e43eabc645ad624a2238
Phakawat Rattanasopa <dewmagic2013@gmail.com>
2025-04-27 00:05:59 +0700

    refactor: restructure Independent Study and WIL Project handlers to use state management

--- ModEd/curriculum/cli/wil-project/handler/IndependentStudyHandler.go
+++ ModEd/curriculum/cli/wil-project/handler/IndependentStudyHandler.go
@@ -2,48 +2,67 @@
 package handler
 
 import (
+	"ModEd/asset/util"
+	"ModEd/core/cli"
 	"ModEd/curriculum/controller"
-	"ModEd/curriculum/model"
-	"ModEd/curriculum/utils"
-	"ModEd/utils/deserializer"
+	// "ModEd/curriculum/model"
+	// "ModEd/utils/deserializer"
 	"fmt"
 )
 
-func RunIndependentStudyHandler(controller *controller.WILModuleFacade) {
-	for {
-		printIndependentMenu()
-		choice := utils.GetUserChoice()
-		switch choice {
-		case "1":
-			fmt.Println("Migrate Independent Study")
-			path := ""
-			fmt.Println("Please enter the path of the independent study(s) file (csv or json): ")
-			_, _ = fmt.Scanln(&path)
-			fd, err := deserializer.NewFileDeserializer(path)
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-			isModel := new([]model.IndependentStudy)
-			if err := fd.Deserialize(isModel); err != nil {
-				fmt.Println(err)
-				continue
-			}
-			for _, is := range *isModel {
-				controller.IndependentStudyController.Insert(&is)
-			}
-			fmt.Println("Migration Success!")
-		case "0":
-			fmt.Println("Exiting...")
-			return
-		default:
-			fmt.Println("Invalid option")
-		}
+type IndependentStudyMenuStateHandler struct {
+	manager *cli.CLIMenuStateManager
+	proxy   *controller.WILModuleProxy
+
+	wilModuleMenuStateHandler *WILModuleMenuStateHandler
+}
+
+func NewIndependentStudyMenuStateHandler(
+	manager *cli.CLIMenuStateManager, proxy *controller.WILModuleProxy, wilModuleMenuStateHandler *WILModuleMenuStateHandler,
+) *IndependentStudyMenuStateHandler {
+	return &IndependentStudyMenuStateHandler{
+		manager:                   manager,
+		proxy:                     proxy,
+		wilModuleMenuStateHandler: wilModuleMenuStateHandler,
 	}
 }
 
-func printIndependentMenu() {
+func (menu *IndependentStudyMenuStateHandler) Render() {
 	fmt.Println("\nIndependent Study Menu:")
 	fmt.Println("1.Migrate independent study")
 	fmt.Println("0.Exit Independent Study module")
+}
+
+func (menu *IndependentStudyMenuStateHandler) HandleUserInput(input string) error {
+		switch input {
+		case "1":
+			fmt.Println("Migrate Independent Study")
+			// path := ""
+			// fmt.Println("Please enter the path of the independent study(s) file (csv or json): ")
+			// _, _ = fmt.Scanln(&path)
+			// fd, err := deserializer.NewFileDeserializer(path)
+			// if err != nil {
+			// 	fmt.Println(err)
+			// 	return nil
+			// }
+			// isModel := new([]model.IndependentStudy)
+			// if err := fd.Deserialize(isModel); err != nil {
+			// 	fmt.Println(err)
+			// 	return nil
+			// }
+			// for _, is := range *isModel {
+			// 	controller.IndependentStudyController.Insert(&is)
+			// }
+			// fmt.Println("Migration Success!")
+		case "0":
+			menu.manager.SetState(menu.wilModuleMenuStateHandler)
+			return nil
+		default:
+			fmt.Println("Invalid Command")
+		}
+
+		util.PressEnterToContinue()
+
+		return nil
+
 }
--- ModEd/curriculum/cli/wil-project/handler/WILProjectHandler.go
+++ ModEd/curriculum/cli/wil-project/handler/WILProjectHandler.go
@@ -2,31 +2,30 @@
 package handler
 
 import (
+	"ModEd/asset/util"
+	"ModEd/core/cli"
 	"ModEd/curriculum/controller"
-	"ModEd/curriculum/utils"
 	"fmt"
 )
 
-func RunWILProjectHandler(controller *controller.WILModuleFacade) {
-	for {
-		printWILProjectModuleMenu()
-		choice := utils.GetUserChoice()
+type WILProjectMenuStateHandler struct {
+	manager *cli.CLIMenuStateManager
+	proxy   *controller.WILModuleProxy
 
-		switch choice {
-		case "1":
-			fmt.Println("1 Not implemented yet...")
-		case "2":
-			fmt.Println("2 Not implemented yet...")
-		case "0":
-			fmt.Println("Exiting...")
-			return
-		default:
-			fmt.Println("Invalid option")
-		}
+	wilModuleMenuStateHandler *WILModuleMenuStateHandler
+}
+
+func NewWILProjectMenuStateHandler(
+	manager *cli.CLIMenuStateManager, proxy *controller.WILModuleProxy, wilModuleMenuStateHandler *WILModuleMenuStateHandler,
+) *WILProjectMenuStateHandler {
+	return &WILProjectMenuStateHandler{
+		manager:                   manager,
+		proxy:                     proxy,
+		wilModuleMenuStateHandler: wilModuleMenuStateHandler,
 	}
 }
 
-func printWILProjectModuleMenu() {
+func (menu *WILProjectMenuStateHandler) Render() {
 	fmt.Println("\nWIL Project Menu:")
 	fmt.Println("1. Create WIL Project")
 	fmt.Println("2. Edit WIL Project")
@@ -35,4 +34,30 @@ func printWILProjectModuleMenu() {
 	fmt.Println("5. Get WIL Project Detail By ID")
 	fmt.Println("6. Delete WIL Project By ID")
 	fmt.Println("0. Exit WIL Module")
+}
+
+func (menu *WILProjectMenuStateHandler) HandleUserInput(input string) error {
+	switch input {
+	case "1":
+		fmt.Println("1 Not implemented yet...")
+	case "2":
+		fmt.Println("2 Not implemented yet...")
+	case "3":
+		fmt.Println("3 Not implemented yet...")
+	case "4":
+		fmt.Println("4 Not implemented yet...")
+	case "5":
+		fmt.Println("5 Not implemented yet...")
+	case "6":
+		fmt.Println("6 Not implemented yet...")
+	case "0":
+		menu.manager.SetState(menu.wilModuleMenuStateHandler)
+		return nil
+	default:
+		fmt.Println("Invalid Command")
+	}
+
+	util.PressEnterToContinue()
+
+	return nil
 }
