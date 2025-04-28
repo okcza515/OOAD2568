f3229f74ec1e84eb2241eb5e5ce5804bc9647aac
Just3rd <kantapong.3rd@gmail.com>
2025-04-29 02:42:34 +0700

    merge: bro someone deleted my entire code :skull:

--- ModEd/asset/controller/InstrumentLogController.go
+++ ModEd/asset/controller/InstrumentLogController.go
@@ -5,6 +5,7 @@ package controller
 import (
 	"ModEd/asset/model"
 	"ModEd/core"
+	"ModEd/core/migration"
 	"gorm.io/gorm"
 	"strconv"
 )
@@ -19,7 +20,59 @@ type InstrumentLogControllerInterface interface {
 	Insert(data model.InstrumentLog) error
 	InsertMany(data []model.InstrumentLog) error
 	RetrieveByID(id uint, preloads ...string) (model.InstrumentLog, error)
-	List(condition map[string]interface{}) ([]model.InstrumentLog, error)
+	List(condition map[string]interface{}, preloads ...string) ([]model.InstrumentLog, error)
+
+	GetObserverID() string
+	HandleEvent(eventType string, dataContext model.BorrowInstrument)
+}
+
+func NewInstrumentLogController() *InstrumentLogController {
+	db := migration.GetInstance().DB
+	return &InstrumentLogController{
+		db:             db,
+		BaseController: core.NewBaseController[model.InstrumentLog](db),
+	}
+}
+
+func (c *InstrumentLogController) GetObserverID() string {
+	return "InstrumentLogController"
+}
+
+func (c *InstrumentLogController) HandleEvent(eventType string, dataContext model.BorrowInstrument) {
+	switch model.InstrumentLogActionEnum(eventType) {
+	case model.INS_LOG_ADDNEW:
+		log := model.InstrumentLog{
+			InstrumentID: dataContext.Instrument.ID,
+			Action:       model.INS_LOG_ADDNEW,
+		}
+
+		err := c.Insert(log)
+		if err != nil {
+			panic(err)
+		}
+	case model.INS_LOG_UPDATE:
+		panic("not implemented")
+	case model.INS_LOG_BORROW:
+		panic("not implemented")
+	case model.INS_LOG_RETURN:
+		panic("not implemented")
+	case model.INS_LOG_MOVE:
+		panic("not implemented")
+	case model.INS_LOG_BROKEN:
+		panic("not implemented")
+	case model.INS_LOG_REPAIR:
+		panic("not implemented")
+	case model.INS_LOG_LOST:
+		panic("not implemented")
+	case model.INS_LOG_FOUND:
+		panic("not implemented")
+	case model.INS_LOG_SALVAGING:
+		panic("not implemented")
+	case model.INS_LOG_SALVAGE:
+		panic("not implemented")
+	case model.INS_LOG_DONATE:
+		panic("not implemented")
+	}
 }
 
 func (c *InstrumentLogController) getAll() ([]model.InstrumentLog, error) {
