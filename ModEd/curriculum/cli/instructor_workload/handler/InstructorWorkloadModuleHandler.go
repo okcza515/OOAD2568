54cf54fe12d551105d5cb6875646f7c63d3308bd
kongkcypw <kongkcypw@gmail.com>
2025-04-28 23:15:54 +0700

    instructor-workload cli implement core function

--- ModEd/curriculum/cli/instructor_workload/handler/InstructorWorkloadModuleHandler.go
+++ ModEd/curriculum/cli/instructor_workload/handler/InstructorWorkloadModuleHandler.go
@@ -3,11 +3,14 @@ package handler
 import (
 	"ModEd/core/cli"
 	"ModEd/curriculum/controller"
+	"fmt"
 )
 
 type InstructorWorkloadModuleMenuStateHandler struct {
 	menuManger *cli.CLIMenuStateManager
 	wrapper    *controller.InstructorWorkloadModuleWrapper
+
+	SeniorProjectWorkloadMenuStateHandler *SeniorProjectWorkloadMenuStateHandler
 }
 
 func NewInstructorWorkloadModuleMenuStateHandler(manager *cli.CLIMenuStateManager, wrapper *controller.InstructorWorkloadModuleWrapper) *InstructorWorkloadModuleMenuStateHandler {
@@ -15,5 +18,29 @@ func NewInstructorWorkloadModuleMenuStateHandler(manager *cli.CLIMenuStateManage
 		menuManger: manager,
 		wrapper:    wrapper,
 	}
+
+	instructorWorkloadModuleHandler.SeniorProjectWorkloadMenuStateHandler = NewSeniorProjectModuleStateHandler(manager, wrapper, instructorWorkloadModuleHandler)
+
 	return instructorWorkloadModuleHandler
+}
+
+func (handler *InstructorWorkloadModuleMenuStateHandler) Render() {
+	fmt.Println("\nInstructor Workload Module Menu:")
+	fmt.Println("1. Load CSV Seed Data")
+	fmt.Println("2. Academic")
+	fmt.Println("3. Student Advisor Workload")
+	fmt.Println("4. Administrative Task")
+	fmt.Println("5. Senior Project")
+	fmt.Println("Type 'exit' to quit")
+}
+
+func (handler *InstructorWorkloadModuleMenuStateHandler) HandleUserInput(input string) error {
+	switch input {
+	case "5":
+		handler.menuManger.SetState(handler.SeniorProjectWorkloadMenuStateHandler)
+	default:
+		fmt.Println("Invalid input")
+	}
+
+	return nil
 }
