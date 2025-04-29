a9853b9083cc2266983562a9203ff1a3a57ee49a
Mickieee <jaatupoj.tuan@mail.kmutt.ac.th>
2025-04-29 14:06:20 +0700

    Update CLI

--- ModEd/curriculum/cli/Internship/MainInternShip.go
+++ ModEd/curriculum/cli/Internship/MainInternShip.go
@@ -1,61 +1,34 @@
-//MEP-1009 Student Internship
-package Internship
+// MEP-1009 Student Internship
+package internship
 
 import (
-	handler "ModEd/curriculum/cli/Internship/handler"
-	controller "ModEd/curriculum/controller"
-	"ModEd/curriculum/utils"
-	"fmt"
+    "ModEd/core/cli"
+    handler "ModEd/curriculum/cli/Internship/handler"
+    controller "ModEd/curriculum/controller"
+    "ModEd/curriculum/utils"
 
-	"gorm.io/gorm"
+    "gorm.io/gorm"
 )
 
 func RunInterShipCLI(
-	db *gorm.DB,
+    db *gorm.DB,
+    curriculumController controller.CurriculumControllerInterface,
 ) {
 
-	const (
-		defaultStudentPath = "../../data/StudentList.csv"
-	)
+    menuManager := cli.NewCLIMenuManager()
+    wrapper := controller.NewInternshipModuleWrapper(
+        db,
+        curriculumController,
+    )
+    instructorWorkloadModuleState := handler.NewInternShipModuleMenuStateHandler(menuManager, wrapper)
+    menuManager.SetState(instructorWorkloadModuleState)
 
-	const (
-		defaultCompaniesPath = "../../data/Internship/Company.csv"
-	)
-
-	CreateInternshipApplicationController := controller.CreateInternshipApplicationController(db)
-	CreateReviewController := controller.CreateReviewController(db)
-	CreateReportController := controller.CreateReportController(db)
-	CreateAprovedController := controller.CreateApprovedController(db)
-	CreateInternStudentController := controller.CreateInternStudentController(db)
-	CreateGenericImportController := controller.CreateGenericImportController(db)
-
-	CreateGenericImportController.ImportCompaniesFromCSV(defaultCompaniesPath)
-	CreateInternStudentController.RegisterInternStudentsFromFile(defaultStudentPath)
-
-	for {
-		fmt.Println("\n==== Internship Application System ====")
-		fmt.Println("1. Create Internship Application")
-		fmt.Println("2. Evaluation Student Performance")
-		fmt.Println("3. Evaluation Student Report")
-		fmt.Println("4. Update Approval Status")
-		fmt.Println("5. Exit")
-		fmt.Print("Enter your choice: ")
-
-		choice := utils.GetUserChoice()
-		switch choice {
-		case "1":
-			handler.InternshipApplication(CreateInternshipApplicationController, db)
-		case "2":
-			handler.InternshipReview(CreateReviewController)
-		case "3":
-			handler.InternshipReport(CreateReportController)
-		case "4":
-			handler.InternShipApproved(CreateAprovedController)
-		case "5":
-			fmt.Println("Exiting the system. Goodbye!")
-			return
-		default:
-			fmt.Println("Invalid choice. Please try again.")
-		}
-	}
+    for {
+        menuManager.Render()
+        menuManager.UserInput = utils.GetUserChoice()
+        err := menuManager.HandleUserInput()
+        if err != nil {
+            panic(err)
+        }
+    }
 }
