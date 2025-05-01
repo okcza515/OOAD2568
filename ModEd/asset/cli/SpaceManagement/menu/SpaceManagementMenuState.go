8a2e31296a2e0f79a08034423ddc36dec16a0599
Billy19191 <bill.ratchanon@gmail.com>
2025-05-01 23:31:56 +0700

    🔥  feat & fix: migrate to use state menu and strategy

---
+++ ModEd/asset/cli/SpaceManagement/menu/SpaceManagementMenuState.go
@@ -0,0 +1,61 @@
+package menu
+
+import (
+	"ModEd/asset/util"
+	"ModEd/core/cli"
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+type SpaceManagementState struct {
+	manager *cli.CLIMenuStateManager
+
+	//Add more menu here
+	roomMenu *RoomMenuState
+}
+
+func NewSpaceManagementState(db *gorm.DB, manager *cli.CLIMenuStateManager) *SpaceManagementState {
+	spaceManagementMenu := &SpaceManagementState{
+		manager: manager,
+	}
+
+	spaceManagementMenu.roomMenu = NewRoomMenuState(db, manager, spaceManagementMenu)
+
+	return spaceManagementMenu
+}
+
+func (menu *SpaceManagementState) Render() {
+	util.PrintSpaceManagementBanner()
+	fmt.Println("\n===============================")
+	fmt.Println()
+	fmt.Println("Welcome to ModEd Space Management Service CLI!")
+	fmt.Println("Here is the list of page you can use, choose wisely!")
+	fmt.Println("  1:\tInstrument Management Page")
+	fmt.Println("  2:\tSupply Management Page")
+	fmt.Println("  3:\tBooking Page")
+	fmt.Println("  4:\tPermanent Schedule Page")
+	fmt.Println("  5:\tRoom Page")
+	fmt.Println("  exit:\tExit the program (or Ctrl+C is fine ¯\\\\_(ツ)_/¯)")
+	fmt.Println()
+}
+
+func (menu *SpaceManagementState) HandleUserInput(input string) error {
+	switch input {
+	case "1":
+		fmt.Println("Not implemented yet...")
+	case "2":
+		fmt.Println("Not implemented yet...")
+	case "3":
+		fmt.Println("Not implemented yet...")
+	case "4":
+		fmt.Println("Not implemented yet...")
+	case "5":
+		menu.manager.SetState(menu.roomMenu)
+	case "exit":
+		return nil
+	default:
+		fmt.Println("Invalid input, please try again.")
+	}
+	return nil
+}
