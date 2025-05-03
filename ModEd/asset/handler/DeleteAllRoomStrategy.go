962850dca3f81ee5ccb1503f198e22292db1298f
Billy19191 <bill.ratchanon@gmail.com>
2025-05-04 04:11:35 +0700

    ✨  feat: delete all room handler

---
+++ ModEd/asset/handler/DeleteAllRoomStrategy.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"ModEd/asset/util"
+	"fmt"
+)
+
+type DeleteAllRoomStrategy struct {
+	controller interface {
+		DeleteAll() error
+	}
+}
+
+func NewDeleteAllRoomStrategy(controller interface {
+	DeleteAll() error
+}) *DeleteAllRoomStrategy {
+	return &DeleteAllRoomStrategy{controller: controller}
+}
+
+func (handler DeleteAllRoomStrategy) Execute() error {
+	err := handler.controller.DeleteAll()
+	if err != nil {
+		return err
+	}
+	fmt.Println("All rooms deleted successfully!")
+	util.PressEnterToContinue()
+	return nil
+}
