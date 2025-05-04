20d8dc3a7a623e3305a903ba034dc50b17fb150a
Encall <apichataimimpat@gmail.com>
2025-05-04 23:49:30 +0700

    feat: add abstract factory

--- ModEd/hr/model/RequestRaise.go
+++ ModEd/hr/model/RequestRaise.go
@@ -1,22 +1,7 @@
 package model
 
-import (
-	"gorm.io/gorm"
-)
-
 type RequestRaise struct {
-	gorm.Model
-	InstructorCode string `gorm:"not null"`        // อ้างถึง StudentInfo.StudentCode
-	Reason         string `gorm:"type:text"`       // optional เหตุผลลาออก
-	Status         string `gorm:"default:Pending"` // Pending / Approved / Rejected
-	TargetSalary   int    `gorm:"not null"`        // เป้าหมายเงินเดือนที่ต้องการ
-}
-
-func NewRequestRaise(InstructorCode string, Reason string, TargetSalary int) *RequestRaise {
-	return &RequestRaise{
-		InstructorCode: InstructorCode,
-		Reason:         Reason,
-		Status:         "Pending",
-		TargetSalary:   TargetSalary,
-	}
+	BaseStandardRequest
+	InstructorCode string `gorm:"not null"`
+	TargetSalary   int    `gorm:"not null"`
 }
