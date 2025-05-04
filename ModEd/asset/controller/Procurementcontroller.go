73e796ba4301ce36cac37832eaacfcc14e44c67f
ThanooThanu <thanoozaa@gmail.com>
2025-05-04 22:14:37 +0700

    minimize code

--- ModEd/asset/controller/Procurementcontroller.go
+++ ModEd/asset/controller/Procurementcontroller.go
@@ -22,7 +22,6 @@ func (c *ProcurementController) ListAll() ([]model.Procurement, error) {
 	var procurements []model.Procurement
 	err := c.db.
 		Preload("TOR.InstrumentRequest.Instruments.Category").
-		Preload("TOR.InstrumentRequest").
 		Preload("Approver").
 		Find(&procurements).Error
 	return procurements, err
@@ -33,7 +32,6 @@ func (c *ProcurementController) GetByID(id uint) (*model.Procurement, error) {
 	var procurement model.Procurement
 	err := c.db.
 		Preload("TOR.InstrumentRequest.Instruments.Category").
-		Preload("TOR").
 		Preload("Approver").
 		First(&procurement, "procurement_id = ?", id).Error
 	return &procurement, err
@@ -44,8 +42,8 @@ func (c *ProcurementController) Create(p *model.Procurement) error {
 	return c.db.Create(p).Error
 }
 
-// Update procurement fields (generic)
-func (c *ProcurementController) Update(id uint, updates map[string]any) error {
+// Update procurement fields
+func (c *ProcurementController) Update(id uint, updates map[string]interface{}) error {
 	return c.db.Model(&model.Procurement{}).
 		Where("procurement_id = ?", id).
 		Updates(updates).Error
@@ -65,27 +63,17 @@ func (c *ProcurementController) Approve(id uint, approverID uint) error {
 
 // Reject procurement
 func (c *ProcurementController) Reject(id uint, approverID uint) error {
+	now := time.Now()
 	return c.db.Model(&model.Procurement{}).
 		Where("procurement_id = ?", id).
 		Updates(map[string]interface{}{
 			"status":      model.ProcurementStatusRejected,
 			"approver_id": approverID,
+			"updated_at":  now,
 		}).Error
 }
 
-// Delete soft
+// Soft delete procurement
 func (c *ProcurementController) Delete(id uint) error {
 	return c.db.Delete(&model.Procurement{}, id).Error
-}
-
-func (c *ProcurementController) OnApproved(id uint) error {
-	return c.db.Model(&model.Procurement{}).
-		Where("procurement_id = ?", id).
-		Update("status", model.ProcurementStatusApproved).Error
-}
-
-func (c *ProcurementController) OnRejected(id uint) error {
-	return c.db.Model(&model.Procurement{}).
-		Where("procurement_id = ?", id).
-		Update("status", model.ProcurementStatusRejected).Error
 }
