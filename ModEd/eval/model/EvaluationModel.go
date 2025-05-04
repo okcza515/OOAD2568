c334fea90d566e77136e1195eedb2483abe06a1c
Chanawat3445 <veerawatlwat@gmail.com>
2025-05-04 23:55:22 +0700

    delete comment

--- ModEd/eval/model/EvaluationModel.go
+++ ModEd/eval/model/EvaluationModel.go
@@ -104,19 +104,3 @@ func SaveEvaluationsToCSV(filePath string, evaluations []*Evaluation) error {
 
 	return nil
 }
-
-// package model
-
-// import (
-// 	"time"
-// )
-
-// type Evaluation struct {
-// 	StudentCode    string    `csv:"student_code"`
-// 	InstructorCode string    `csv:"instructor_code"`
-// 	AssignmentId   *uint     `csv:"assignment_id"`
-// 	QuizId         *uint     `csv:"quiz_id"`
-// 	Score          uint      `csv:"score"`
-// 	Comment        string    `csv:"comment"`
-// 	EvaluatedAt    time.Time `csv:"evaluated_at"`
-// }
