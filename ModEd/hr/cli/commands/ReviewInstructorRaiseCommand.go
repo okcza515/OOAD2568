8c638303d718b2bdc6dd7d79d99fa60e438309b0
Puntharee Roongprasert <puntharee000@gmail.com>
2025-05-05 22:11:01 +0700

    feat: Add instructor raise request and review functionality

---
+++ ModEd/hr/cli/commands/ReviewInstructorRaiseCommand.go
@@ -0,0 +1,24 @@
+package commands
+
+import (
+	"ModEd/hr/controller"
+
+	"gorm.io/gorm"
+)
+
+// ReviewInstructorLeaveCommand processes the review for an instructor leave request.
+// Usage: go run hr/cli/HumanResourceCLI.go review-instructor-leave -id=<requestID> -action=approve|reject [-reason="if reject"]
+type ReviewInstructorRaiseCommand struct{}
+
+func (cmd *ReviewInstructorRaiseCommand) Execute(args []string, tx *gorm.DB) error {
+	controller := controller.CreateRaiseInstructorHRController(tx)
+	return handleReviewCommand(
+		args,
+		tx,
+		"review-instructor-raise",
+		"Instructor raise request ID",
+		controller.ReviewInstructorRaiseRequest,
+		"instructor raise",
+	)
+}				
+// handleReviewCommand is a helper function to handle the review of requests.
