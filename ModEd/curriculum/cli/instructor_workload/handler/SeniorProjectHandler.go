98218081b9e0c9e99df532d369405b6add112d1e
ffaann02 <rudfaanmaimahad@gmail.com>
2025-05-05 18:12:42 +0700

    feat: insert project evaluation based on task type

--- ModEd/curriculum/cli/instructor_workload/handler/SeniorProjectHandler.go
+++ ModEd/curriculum/cli/instructor_workload/handler/SeniorProjectHandler.go
@@ -1,14 +1,92 @@
 // MEP-1008
 package handler
 
-type SeniorProjectWorkload struct{}
+import (
+	instructorWorkloadController "ModEd/curriculum/controller"
+	"ModEd/curriculum/model"
+	projectModel "ModEd/project/model"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 
-func (s SeniorProjectWorkload) Execute() {
+	"gorm.io/gorm"
+)
+
+type SeniorProjectWorkloadHandler struct {
+	db *gorm.DB
+}
+
+func NewSeniorProjectWorkload(db *gorm.DB) SeniorProjectWorkloadHandler {
+	return SeniorProjectWorkloadHandler{db: db}
+}
+
+func (s SeniorProjectWorkloadHandler) Execute() {
 	seniorProjectMenu := NewMenuHandler("Senior Project Workload Menu", true)
-	seniorProjectMenu.Add("View Advising Project", nil)
+	seniorProjectMenu.Add("View Advising Project", ViewAdvisingProject{db: s.db})
 	seniorProjectMenu.Add("View Committee Project", nil)
-	seniorProjectMenu.Add("Evaluate Project", nil)
+	seniorProjectMenu.Add("Evaluate Project", EvaluateProject{db: s.db})
 	seniorProjectMenu.SetBackHandler(Back{})
 	seniorProjectMenu.SetDefaultHandler(UnknownCommand{})
 	seniorProjectMenu.Execute()
+}
+
+type ViewAdvisingProject struct {
+	db *gorm.DB
+}
+
+func (v ViewAdvisingProject) Execute() {
+	// controller := projectController.NewAdvisorController(v.db)
+	// controller.ListAdvisorsByInstructor(1)
+}
+
+type ViewCommitteeProject struct {
+	db *gorm.DB
+}
+
+func (v ViewCommitteeProject) Execute() {
+	// controller := projectController.NewCommitteeController(v.db)
+	// controller.ListCommitteesByInstructor(1)
+}
+
+type EvaluateProject struct {
+	db *gorm.DB
+}
+
+func (e EvaluateProject) Execute() {
+	controller := instructorWorkloadController.NewProjectController(e.db)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("\nEnter evaluation type (Assignment, Proposal, Report, Presentation):")
+	fmt.Print("Enter: ")
+
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+
+	if !model.IsValidAssignmentType(input) {
+		fmt.Println("Invalid type. Defaulting to 'Assignment'.")
+		input = string(model.ASSIGNMENT)
+	}
+
+	taskType := model.ProjectEvaluationTypeEnum(input)
+	mockEvaluation := &model.ProjectEvaluation{
+		GroupId:        1,
+		AssignmentId:   1,
+		AssignmentType: string(taskType),
+		Score:          0.0,
+		Comment:        "",
+	}
+	mockCriteria := []projectModel.AssessmentCriteria{
+		{
+			CriteriaName: "Criteria A",
+		},
+		{
+			CriteriaName: "Criteria B",
+		},
+		{
+			CriteriaName: "Criteria C",
+		},
+	}
+	controller.CreateEvaluation(mockEvaluation, string(taskType), mockCriteria)
 }
