23033546f24d757308b6c95b7072de1fd1f09459
ZTPG101 <110453102+ZTPG101@users.noreply.github.com>
2025-05-05 22:54:58 +0700

    add handler

--- ModEd/asset/cli/Procurement/ProcurementCLI.go
+++ ModEd/asset/cli/Procurement/ProcurementCLI.go
@@ -23,13 +23,13 @@ func main() {
 		case "1":
 			handler.InstrumentRequestHandler(facade)
 		case "2":
-			handler.ProcurementHandler(facade)
-		case "3":
 			handler.ApprovalHandler(facade)
+		case "3":
+			handler.ProcurementHandler(facade)
 		case "4":
-			fmt.Println("Not implemented yet...")
+			handler.TORHandler(facade)
 		case "5":
-			fmt.Println("Not implemented yet...")
+			handler.QuotationSupplierHandler(facade)
 		case "6":
 			fmt.Println("Not implemented yet...")
 		}
@@ -54,10 +54,10 @@ func printOption() {
 	fmt.Println("Welcome to ModEd Procurement Service CLI!")
 	fmt.Println("Here is the list of page you can use, choose wisely!")
 	fmt.Println("  1:\tRequest Instrument Page")
-	fmt.Println("  2:\tProcurement Page")
-	fmt.Println("  3\tApproval Page")
-	fmt.Println("  4:\tNot implemented yet... Page")
-	fmt.Println("  5:\tNot implemented yet... Page")
+	fmt.Println("  2:\tApproval Page")
+	fmt.Println("  3:\tProcurement Page")
+	fmt.Println("  4:\tTOR Page")
+	fmt.Println("  5:\tQuotation & Supplier Page")
 	fmt.Println("  6:\tNot implemented yet... Page")
 	fmt.Println("  exit:\tExit the program (or Ctrl+C is fine ¯\\\\_(ツ)_/¯)")
 	fmt.Println()
