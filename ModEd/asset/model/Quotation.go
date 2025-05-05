36cd4fa8eb9732fda5dbd3bc90b0d8620296f399
NattyHam <pnbookclub@gmail.com>
2025-05-06 00:05:16 +0700

    edit model and create mockup data

--- ModEd/asset/model/Quotation.go
+++ ModEd/asset/model/Quotation.go
@@ -10,7 +10,9 @@ type Quotation struct {
 	TORID       uint            `gorm:"index"`
 	TOR         TOR             `gorm:"foreignKey:TORID"`
 	SupplierID  uint            `gorm:"index"`
+	Supplier    Supplier        `gorm:"foreignKey:SupplierID"`
+	TotalOfferedPrice float64 `gorm:"type:decimal(12,2);default:0"`
 	Status      QuotationStatus `gorm:"type:varchar(50);default:'pending'"`
+	Details     []QuotationDetail `gorm:"foreignKey:QuotationID"`
 	DeletedAt   gorm.DeletedAt  `gorm:"index"`
-	Supplier    Supplier
 }
