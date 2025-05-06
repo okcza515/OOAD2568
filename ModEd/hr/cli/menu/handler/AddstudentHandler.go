40d51c5fa22c2d25b6a02a9200284503ca0b266c
wavekanit <wavekanit@hotmail.com>
2025-05-06 23:49:21 +0700

    feat: add field input for add student

--- ModEd/hr/cli/menu/handler/AddstudentHandler.go
+++ ModEd/hr/cli/menu/handler/AddstudentHandler.go
@@ -2,7 +2,10 @@ package handler
 
 import (
 	"ModEd/core"
+	"ModEd/hr/controller"
 	"fmt"
+
+	"gorm.io/gorm"
 )
 
 type AddStudentStrategy[T core.RecordInterface] struct {
@@ -13,14 +16,43 @@ func NewAddStudentStrategy[T core.RecordInterface](controller interface{ InsertO
 	return &AddStudentStrategy[T]{controller: controller}
 }
 
-func (handler AddStudentStrategy[T]) Execute() error {
-	id := ""
-	fmt.Printf("Please enter the student ID: ")
-	fmt.Scanln(&id)
+func (handler AddStudentStrategy[T]) Execute(db *gorm.DB) error {
+	studentCode := core.GetUserInput("Enter student ID: ")
+	firstName := core.GetUserInput("Enter student first name: ")
+	lastName := core.GetUserInput("Enter student last name: ")
+	email := core.GetUserInput("Enter student email: ")
+	startDate := core.GetUserInput("Enter student start date: ")
+	birthDate := core.GetUserInput("Enter student birth date: ")
+	program := core.GetUserInput("Enter student program: ")
+	department := core.GetUserInput("Enter student department: ")
+	status := core.GetUserInput("Enter student status: ")
+	gender := core.GetUserInput("Enter student gender: ")
+	citizenID := core.GetUserInput("Enter student citizen ID: ")
+	phoneNumber := core.GetUserInput("Enter student phone number: ")
+	advisorCode := core.GetUserInput("Enter student advisor code: ")
 
-	name := ""
-	fmt.Printf("Please enter the student name: ")
-	fmt.Scanln(&name)
+	studentController := controller.NewStudentHRController(db)
+	err := studentController.AddStudent(
+		studentCode,
+		firstName,
+		lastName,
+		email,
+		startDate,
+		birthDate,
+		program,
+		department,
+		status,
+		gender,
+		citizenID,
+		phoneNumber,
+		advisorCode,
+	)
+
+	if err != nil {
+		return fmt.Errorf("failed to add student: %w", err)
+	}
+
+	fmt.Println("Student added and HR info updated successfully!")
 
 	return nil
 }
