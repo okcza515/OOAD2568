a32ef6ca89b344a2756050e742cb9be92b60e11f
Mikipedia <Chayaphon.mick@gmail.com>
2025-05-06 10:18:16 +0700

    Update migration controller

--- ModEd/eval/controller/MigrationController.go
+++ ModEd/eval/controller/MigrationController.go
@@ -22,8 +22,9 @@ func (c *MigrationController) MigrateToDB() error {
 		&model.Question{},
 		&model.Answer{},
 		&model.Result{},
+		&model.Assessment{},
+		&model.AssessmentSubmission{},
 	)
-
 	if err != nil {
 		return errors.New("fail to Migrate to DB")
 	}
