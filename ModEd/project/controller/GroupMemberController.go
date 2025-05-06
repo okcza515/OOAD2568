715a1da229dd593ee655f0a7be634c0bf3085231
ZhiWang64070503461 <wang.zhi.0908@gmail.com>
2025-05-06 12:46:43 +0700

    Updated GroupMemberController

--- ModEd/project/controller/GroupMemberController.go
+++ ModEd/project/controller/GroupMemberController.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"ModEd/core"
 	"ModEd/project/model"
+	"fmt"
 
 	"gorm.io/gorm"
 )
@@ -19,6 +20,33 @@ func NewGroupMemberController(db *gorm.DB) *GroupMemberController {
 	}
 }
 
+func (c *GroupMemberController) ListAllGroupMembers() ([]*model.GroupMember, error) {
+	groupMembers, err := c.List(map[string]interface{}{})
+	if err != nil {
+		return nil, err
+	}
+	return groupMembers, nil
+}
+
+func (c *GroupMemberController) RetrieveGroupMember(id uint) (*model.GroupMember, error) {
+	return c.RetrieveByID(id)
+}
+
 func (c *GroupMemberController) RetrieveGroupMembersBySeniorProjectId(projectId uint) ([]*model.GroupMember, error) {
 	return c.List(map[string]interface{}{"senior_project_id": projectId})
+}
+
+func (c *GroupMemberController) InsertGroupMember(member *model.GroupMember) error {
+	if member.SeniorProjectId == 0 {
+		return fmt.Errorf("senior project ID is required")
+	}
+	return c.Insert(member)
+}
+
+func (c *GroupMemberController) UpdateGroupMember(member *model.GroupMember) error {
+	return c.UpdateByID(member)
+}
+
+func (c *GroupMemberController) DeleteGroupMember(id uint) error {
+	return c.DeleteByID(id)
 }
