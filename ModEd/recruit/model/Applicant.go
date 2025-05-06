95ff4eca9f0cb2af602df032042dee69eef07db9
Rattipong Sakunjeen <rattipong.mark@gmail.com>
2025-05-06 16:54:58 +0700

    update : applicant model

--- ModEd/recruit/model/Applicant.go
+++ ModEd/recruit/model/Applicant.go
@@ -3,26 +3,26 @@ package model
 
 type Applicant struct {
 	ApplicantID        uint    `gorm:"primaryKey" json:"applicant_id" csv:"applicant_id"`
-	FirstName          string  `json:"first_name" csv:"first_name"`
-	LastName           string  `json:"last_name" csv:"last_name"`
-	Email              string  `json:"email" csv:"email"`
-	Address            string  `json:"address" csv:"address"`
-	Phonenumber        string  `json:"phone_number" csv:"phone_number"`
-	GPAX               float32 `json:"gpax" csv:"gpax"`
-	HighSchool_Program string  `json:"high_school_program" csv:"high_school_program"`
+	FirstName          string  `gorm:"not null" json:"first_name" csv:"first_name"`
+	LastName           string  `gorm:"not null" json:"last_name" csv:"last_name"`
+	Email              string  `gorm:"not null" json:"email" csv:"email"`
+	Address            string  `gorm:"not null" json:"address" csv:"address"`
+	Phonenumber        string  `gorm:"not null" json:"phone_number" csv:"phone_number"`
+	GPAX               float32 `gorm:"not null" json:"gpax" csv:"gpax"`
+	HighSchool_Program string  `gorm:"not null" json:"high_school_program" csv:"high_school_program"`
 
-	TGAT1 float32 `json:"tgat1" csv:"tgat1"`
-	TGAT2 float32 `json:"tgat2" csv:"tgat2"`
-	TGAT3 float32 `json:"tgat3" csv:"tgat3"`
+	TGAT1 float32 `gorm:"not null" json:"tgat1" csv:"tgat1"`
+	TGAT2 float32 `gorm:"not null" json:"tgat2" csv:"tgat2"`
+	TGAT3 float32 `gorm:"not null" json:"tgat3" csv:"tgat3"`
 
-	TPAT1 float32 `json:"tpat1" csv:"tpat1"`
-	TPAT2 float32 `json:"tpat2" csv:"tpat2"`
-	TPAT3 float32 `json:"tpat3" csv:"tpat3"`
-	TPAT4 float32 `json:"tpat4" csv:"tpat4"`
-	TPAT5 float32 `json:"tpat5" csv:"tpat5"`
+	TPAT1 float32 `gorm:"not null" json:"tpat1" csv:"tpat1"`
+	TPAT2 float32 `gorm:"not null" json:"tpat2" csv:"tpat2"`
+	TPAT3 float32 `gorm:"not null" json:"tpat3" csv:"tpat3"`
+	TPAT4 float32 `gorm:"not null" json:"tpat4" csv:"tpat4"`
+	TPAT5 float32 `gorm:"not null" json:"tpat5" csv:"tpat5"`
 
-	PortfolioURL string  `json:"portfolio_url" csv:"portfolio_url"`
-	FamilyIncome float32 `json:"family_income" csv:"family_income"`
+	PortfolioURL string  `gorm:"not null" json:"portfolio_url" csv:"portfolio_url"`
+	FamilyIncome float32 `gorm:"not null" json:"family_income" csv:"family_income"`
 }
 
 func (i *Applicant) GetID() uint {
