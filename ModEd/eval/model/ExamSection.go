8ef103a59984e75c8968fc95cb90858ef1c13a76
Pooripat Apirukchotima <pooripat.apirukchotima@gmail.com>
2025-05-08 22:31:46 +0700

    examination model

---
+++ ModEd/eval/model/ExamSection.go
@@ -0,0 +1,17 @@
+// MEP-1007
+package model
+
+import (
+	"ModEd/core"
+)
+
+type ExamSection struct {
+	core.BaseModel
+	ID          		uint			`gorm:"primaryKey" csv:"id" json:"id"` 				
+	ExamID   		    string 			`gorm:"not null" csv:"exam_id" json:"exam_id"`
+	Exam				Examination 	`gorm:"foreignKey:ExamID;references:ID" csv:"-" json:"-"`
+	SectionNo           uint			`gorm:"not null" csv:"section_no" json:"section_no"`
+	Description			string			`gorm:"not null" csv:"description" json:"description"`
+	NumQuestions		int				`gorm:"not null" csv:"num_question" json:"num_question"`
+	Score				float64			`gorm:"not null" csv:"score" json:"score"`
+}
--- ModEd/eval/model/Examination.go
+++ ModEd/eval/model/Examination.go
@@ -1,28 +1,24 @@
+// MEP-1007
 package model
 
 import (
 	instructorModel "ModEd/common/model"
 	curriculumModel "ModEd/curriculum/model"
 	"time"
-
-	"gorm.io/gorm"
+	"ModEd/core"
 )
 
 type Examination struct {
-	gorm.Model
+	core.BaseModel
 	ID          		uint 							`gorm:"primaryKey" csv:"id" json:"id"`
-	Exam_name   		string 							`gorm:"not null" csv:"exam_name" json:"exam_name"`
+	ExamName   		    string 							`gorm:"not null" csv:"exam_name" json:"exam_name"`
 	InstructorID 		uint 							`gorm:"not null" csv:"instructor_id" json:"instructor_id"`
 	Instructor  		instructorModel.Instructor 		`gorm:"foreignKey:Instructor_id;references:ID" csv:"-" json:"-"`
 	CourseID    		uint            				`gorm:"not null" csv:"course_id" json:"course_id"`
 	Course      		curriculumModel.Course 			`gorm:"foreignKey:CourseId;references:CourseId" csv:"-" json:"-"`
-	CurriculumID    	uint            				`gorm:"not null" csv:"curriculum_id" json:"curriculum_id"`
-	Curriculum  		curriculumModel.Curriculum 		`gorm:"foreignKey:CurriculumId;references:CurriculumId" csv:"-" json:"-"`
-	Criteria    		ExaminationCriteria 			`gorm:"not null" csv:"criteria" json:"criteria"`
 	Description 		string 							`gorm:"not null" csv:"description" json:"description"`
-	ExamStatus          string    						`gorm:"not null" csv:"exam_status" json:"exam_status"` // draft, published, closed
-	Attempt  			int     						`gorm:"not null" csv:"attempt" json:"attempt"`
-	Start_date   		time.Time 						`gorm:"not null" csv:"start_date" json:"start_date"`
-	End_date   			time.Time 						`gorm:"not null" csv:"end_date" json:"end_date"`
-	Create_at   		time.Time 						`gorm:"autoCreateTime" csv:"created_at" json:"created_at"`
+	ExamStatus          ExamStatus    					`gorm:"not null" csv:"exam_status" json:"exam_status"` // draft, published, closed
+	Attempt  			uint     						`gorm:"not null" csv:"attempt" json:"attempt"`
+	StartDate   		time.Time 						`gorm:"not null" csv:"start_date" json:"start_date"`
+	EndDate   			time.Time 						`gorm:"not null" csv:"end_date" json:"end_date"`
 }
