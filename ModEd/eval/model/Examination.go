87917258659e76f8869633b8d93ed8871152bfda
Sirawit Arsanok <beforever369@gmail.com>
2025-05-09 04:19:35 +0700

    ีfix exam model CourseId foreignKey

--- ModEd/eval/model/Examination.go
+++ ModEd/eval/model/Examination.go
@@ -14,7 +14,7 @@ type Examination struct {
 	InstructorID 		uint 							`gorm:"type:integer;not null" json:"instructor_id" csv:"instructor_id"`
 	Instructor  		instructorModel.Instructor 		`gorm:"foreignKey:InstructorID;references:ID" csv:"instructor" json:"instructor"`
 	CourseID    		uint            				`gorm:"type:integer;not null" json:"course_id" csv:"course_id"`
-	Course      		curriculumModel.Course 			`gorm:"foreignKey:CourseID;references:ID" csv:"course" json:"course"`
+	Course      		curriculumModel.Course 			`gorm:"foreignKey:CourseId;references:ID" csv:"course" json:"course"`
 	Description 		string 							`gorm:"type:text;not null" json:"description" csv:"description"`
 	ExamStatus          ExamStatus    					`gorm:"type:text;not null" json:"exam_status" csv:"exam_status"` // draft, published, closed
 	Attempt  			uint     						`gorm:"type:integer;not null" json:"attempt" csv:"attempt"`
