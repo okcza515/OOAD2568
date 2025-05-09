a85285323ace217ed343d7214a05c0cdb52037a2
Turbulentguy <sawittngam2@gmail.com>
2025-05-09 13:54:09 +0700

    Create EvalModuleWrapper.go

---
+++ ModEd/eval/controller/EvalModuleWrapper.go
@@ -0,0 +1,24 @@
+package controller
+
+import (
+	"ModEd/eval/model"
+
+	"gorm.io/gorm"
+)
+
+type EvalModuleWrapper struct {
+	ProgressController   *ProgressController
+	AssessmentController *AssessmentController
+	EvaluationController *EvaluationController
+}
+
+func NewEvalModuleWrapper(db *gorm.DB) *EvalModuleWrapper {
+	evaluations := make([]*model.Evaluation, 0)
+	csvPath := ""
+
+	return &EvalModuleWrapper{
+		ProgressController:   NewProgressController(db),
+		AssessmentController: NewAssessmentController(),
+		EvaluationController: NewEvaluationController(evaluations, csvPath),
+	}
+}
