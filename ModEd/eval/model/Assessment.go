b131056e9cf978c043fef2a59b54a8e4f059fc8d
Emma Paris <Bewantza33@gmail.com>
2025-05-09 21:52:25 +0700

    Update Assessment.go

--- ModEd/eval/model/Assessment.go
+++ ModEd/eval/model/Assessment.go
@@ -40,12 +40,12 @@ type AssessmentState interface {
 type AssessmentSubmission struct {
 	gorm.Model
 	StudentCode string `gorm:"foreignKey:StudentCode;references:StudentCode"`
-	FirstName   string
-	LastName    string
-	Email       string
+	FirstName   string `gorm:"foreignKey:StudentCode;references:FirstName"`
+	LastName    string `gorm:"foreignKey:StudentCode;references:LastName"`
+	Email       string `gorm:"foreignKey:StudentCode;references:Email"`
 	Answers     PathFile
-	Submitted   bool
+	Submitted   bool      `gorm:"default:false"`
 	UpdatedAt   time.Time `gorm:"autoUpdateTime" csv:"updated_at" json:"updated_at" validate:"-"`
-	Score       float64
-	Feedback    string
+	Score       float64   `gorm:"default:0.0"`
+	Feedback    string    `gorm:"type:varchar(255);"`
 }
