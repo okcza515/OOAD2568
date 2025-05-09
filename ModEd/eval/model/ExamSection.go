88d58b9bb691b8c08e58e0863ae4d02db8bbfa44
LuckyThanet <thanet.jomp@mail.kmutt.ac.th>
2025-05-09 23:29:08 +0700

    fix

--- ModEd/eval/model/ExamSection.go
+++ ModEd/eval/model/ExamSection.go
@@ -8,7 +8,7 @@ import (
 type ExamSection struct {
 	core.BaseModel	
 	ExamID				uint			`gorm:"type:integer;not null" json:"exam_id" csv:"exam_id"`
-	Exam  				Examination 	`gorm:"foreignKey:ExamID;references:ID" json:"exam" csv:"exam"`
+	Exam  				Exam 	`gorm:"foreignKey:ExamID;references:ID" json:"exam" csv:"exam"`
 	SectionNo           uint			`gorm:"type:integer;not null" json:"section_no" csv:"section_no"`
 	Description			string			`gorm:"type:text;not null" json:"description" csv:"description"`
 	NumQuestions		int				`gorm:"type:integer;not null" json:"num_questions" csv:"num_questions"`
