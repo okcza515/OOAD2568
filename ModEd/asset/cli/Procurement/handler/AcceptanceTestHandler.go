77e0c93145da7fa10928824902aa0a98ceaf0000
ZTPG101 <110453102+ZTPG101@users.noreply.github.com>
2025-05-09 23:28:33 +0700

    AcceptanceTestHandler

--- ModEd/asset/cli/Procurement/handler/AcceptanceTestHandler.go
+++ ModEd/asset/cli/Procurement/handler/AcceptanceTestHandler.go
@@ -17,10 +17,13 @@ func AcceptanceTestHandler(facade *controller.ProcurementControllerFacade) {
 		switch inputBuffer {
 		case "1":
 			ListAllTORs(facade)
-			torid := util.GetUintInput("Enter TOR ID: ")
-			PrintCategoriesByTOR(torid, facade)
+			// torID := util.GetUintInput("Enter TOR ID: ")
+			// PrintCategoriesByTOR(torID, facade)
 			WaitForEnter()
 		case "2":
+			// caid := util.GetUintInput("Enter category ID: ")
+			// facade.AcceptanceTest.PrintCategoriesByIDs(caid)
+
 			WaitForEnter()
 		}
 	}
@@ -35,71 +38,37 @@ func printAcceptanceTestOptions() {
 	fmt.Println("  back:\tBack to main menu (or Ctrl+C to exit ¯\\\\_(ツ)_/¯)")
 	fmt.Println()
 }
-func PrintCategor7tiesByTOR(torID uint, facade *controller.ProcurementControllerFacade) error {
-	details, err := facade.AcceptanceTest.GetQuotationDetailsByTOR(torID)
-	if err != nil {
-		return err
-	}
 
-	categorySet := make(map[uint]bool)
-	for _, d := range details {
-		categorySet[d.CategoryID] = true
-	}
+// func PrintCategoriesByTOR(torID uint, facade *controller.ProcurementControllerFacade) error {
+// 	details, err := facade.AcceptanceTest.GetQuotationDetailsByTOR(torID)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	var categoryIDs []uint
-	for id := range categorySet {
-		categoryIDs = append(categoryIDs, id)
-	}
+// 	categorySet := make(map[uint]bool)
+// 	for _, d := range details {
+// 		categorySet[d.CategoryID] = true
+// 	}
 
-	condition := map[string]interface{}{"id": categoryIDs}
-	categories, err := controller.NewCategoryController().List(condition)
-	if err != nil {
-		return err
-	}
+// 	var categoryIDs []uint
+// 	for id := range categorySet {
+// 		categoryIDs = append(categoryIDs, id)
+// 	}
 
-	for _, category := range categories {
-		fmt.Println("Category Name:", category.CategoryName)
-		if category.Description != nil {
-			fmt.Println("Description:", *category.Description)
-		} else {
-			fmt.Println("Description: (none)")
-		}
-		fmt.Println("-----")
-	}
-	// fmt.Println(model.Category.GetID(id))
-	return nil
-}
+// 	categories, err := facade.AcceptanceTest.GetCategoriesByIDs(categoryIDs)
+// 	if err != nil {
+// 		return err
+// 	}
 
-func PrintCategoriesByTOR(torID uint, facade *controller.ProcurementControllerFacade) error {
-	details, err := facade.AcceptanceTest.GetQuotationDetailsByTOR(torID)
-	if err != nil {
-		return err
-	}
+// 	for _, category := range categories {
+// 		fmt.Println("Category Name:", category.CategoryName)
+// 		if category.Description != nil {
+// 			fmt.Println("Description:", *category.Description)
+// 		} else {
+// 			fmt.Println("Description: (none)")
+// 		}
+// 		fmt.Println("-----")
+// 	}
 
-	categorySet := make(map[uint]bool)
-	for _, d := range details {
-		categorySet[d.CategoryID] = true
-	}
-
-	var categoryIDs []uint
-	for id := range categorySet {
-		categoryIDs = append(categoryIDs, id)
-	}
-
-	categories, err := facade.AcceptanceTest.GetCategoriesByIDs(categoryIDs)
-	if err != nil {
-		return err
-	}
-
-	for _, category := range categories {
-		fmt.Println("Category Name:", category.CategoryName)
-		if category.Description != nil {
-			fmt.Println("Description:", *category.Description)
-		} else {
-			fmt.Println("Description: (none)")
-		}
-		fmt.Println("-----")
-	}
-
-	return nil
-}
+// 	return nil
+// }
