bdf964747da80f6eb05f1ca144865e09db9cdda4
Pooripat Apirukchotima <pooripat.apirukchotima@gmail.com>
2025-05-10 17:15:48 +0700

    edit exam model

--- ModEd/eval/model/Exam.go
+++ ModEd/eval/model/Exam.go
@@ -14,7 +14,7 @@ type Exam struct {
 	InstructorID 		uint 							`gorm:"type:integer;not null" json:"instructor_id" csv:"instructor_id"`
 	Instructor  		instructorModel.Instructor 		`gorm:"foreignKey:InstructorID;references:ID" csv:"instructor" json:"instructor"`
 	ClassID    			uint            				`gorm:"type:integer;not null" json:"class_id" csv:"class_id"`
-	Class      			curriculumModel.Course 			`gorm:"foreignKey:ClassID;references:ID" csv:"class" json:"class"`
+	Class      			curriculumModel.Class 			`gorm:"foreignKey:ClassID;references:ID" csv:"class" json:"class"`
 	Description 		string 							`gorm:"type:text;not null" json:"description" csv:"description"`
 	ExamStatus          ExamStatus    					`gorm:"type:text;not null" json:"exam_status" csv:"exam_status"` // draft, published, closed
 	Attempt  			uint     						`gorm:"type:integer;not null" json:"attempt" csv:"attempt"`
