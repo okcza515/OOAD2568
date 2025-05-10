01e04125c7f43be53741a548a82241eff25b2fed
Chanawat3445 <veerawatlwat@gmail.com>
2025-05-10 14:57:33 +0700

    fix model

--- ModEd/eval/model/EvaluationModel.go
+++ ModEd/eval/model/EvaluationModel.go
@@ -4,84 +4,43 @@
 package model
 
 import (
-	"encoding/csv"
-	"os"
-	"strconv"
+	commonModel "ModEd/common/model"
+	"ModEd/core"
+	"fmt"
 	"time"
 )
 
 type Evaluation struct {
+	core.BaseModel
 	StudentCode    string
+	Student        commonModel.Student `gorm:"foreignKey:StudentCode;references:StudentCode"`
 	InstructorCode string
-	AssessmentID   uint
-	AssessmentType string // "assignment" or "quiz"
+	Instructor     commonModel.Instructor `gorm:"foreignKey:InstructorCode;references:InstructorCode"`
+	AssessmentId   uint                   //assignment
+	Assessment     Assessment             `gorm:"foreignKey:AssessmentId;references:AssessmentId"`
 	Score          uint
 	Comment        string
 	EvaluatedAt    time.Time
 }
 
-func LoadEvaluationsFromCSV(filePath string) ([]*Evaluation, error) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	reader := csv.NewReader(file)
-	records, err := reader.ReadAll()
-	if err != nil {
-		return nil, err
-	}
-
-	var evaluations []*Evaluation
-	for i, record := range records {
-		if i == 0 {
-			continue
-		}
-
-		score, _ := strconv.Atoi(record[4])
-		assessmentID, _ := strconv.ParseUint(record[2], 10, 32)
-		assessmentType := record[3]
-
-		evaluatedAt, _ := time.Parse(time.RFC3339, record[6])
-
-		evaluations = append(evaluations, &Evaluation{
-			StudentCode:    record[0],
-			InstructorCode: record[1],
-			AssessmentID:   uint(assessmentID),
-			AssessmentType: assessmentType,
-			Score:          uint(score),
-			Comment:        record[5],
-			EvaluatedAt:    evaluatedAt,
-		})
-	}
-
-	return evaluations, nil
+func (e Evaluation) GetID() uint {
+	return e.ID
 }
 
-func SaveEvaluationsToCSV(filePath string, evaluations []*Evaluation) error {
-	file, err := os.Create(filePath)
-	if err != nil {
-		return err
+func (e Evaluation) ToString() string {
+	return fmt.Sprintf("Evaluation{ID: %d, StudentCode: %s, InstructorCode: %s, AssessmentId: %d, Score: %d, Comment: %s, EvaluatedAt: %v}",
+		e.ID, e.StudentCode, e.InstructorCode, e.AssessmentId, e.Score, e.Comment, e.EvaluatedAt)
+}
+
+func (e Evaluation) Validate() error {
+	if e.StudentCode == "" {
+		return fmt.Errorf("StudentCode is required")
 	}
-	defer file.Close()
-
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	writer.Write([]string{"student_code", "instructor_code", "assessment_id", "assessment_type", "score", "comment", "evaluated_at"})
-
-	for _, e := range evaluations {
-		writer.Write([]string{
-			e.StudentCode,
-			e.InstructorCode,
-			strconv.FormatUint(uint64(e.AssessmentID), 10),
-			e.AssessmentType,
-			strconv.FormatUint(uint64(e.Score), 10),
-			e.Comment,
-			e.EvaluatedAt.Format(time.RFC3339),
-		})
+	if e.InstructorCode == "" {
+		return fmt.Errorf("InstructorCode is required")
 	}
-
+	if e.AssessmentId == 0 {
+		return fmt.Errorf("AssessmentId is required")
+	}
 	return nil
 }
