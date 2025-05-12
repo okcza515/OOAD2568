5cb224ca2f23beb5490c52c389f818fd16928dab
Supajit Chantamul <supajit.tttt@gmail.com>
2025-05-12 22:16:01 +0700

    fix type in examSection model

--- ModEd/eval/model/ExamSection.go
+++ ModEd/eval/model/ExamSection.go
@@ -11,7 +11,7 @@ type ExamSection struct {
 	Exam         Exam       `gorm:"foreignKey:ExamID;references:ID" json:"exam" csv:"exam"`
 	SectionNo    uint       `gorm:"type:integer;not null" json:"section_no" csv:"section_no"`
 	Description  string     `gorm:"type:text;not null" json:"description" csv:"description"`
-	NumQuestions int        `gorm:"type:integer;not null" json:"num_questions" csv:"num_questions"`
+	NumQuestions uint       `gorm:"type:integer;not null" json:"num_questions" csv:"num_questions"`
 	Score        float64    `gorm:"type:decimal(5,2);not null" json:"score" csv:"score"`
 	Questions    []Question `gorm:"foreignKey:SectionID;references:ID" json:"-" csv:"-"`
 }
