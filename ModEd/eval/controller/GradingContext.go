32cf1450f9f303094fcf70995590672c1e468aac
Supajit Chantamul <supajit.tttt@gmail.com>
2025-05-13 01:29:08 +0700

    refactor strategy submission controller

--- ModEd/eval/controller/GradingContext.go
+++ ModEd/eval/controller/GradingContext.go
@@ -1,14 +1,34 @@
 // MEP-1007
 package controller
 
+import "ModEd/eval/model"
+
 type GradingContext struct {
-	Strategy GradingStrategy
+	strategyMap map[model.QuestionType]IGradingStrategy
 }
 
-func (g *GradingContext) SetStrategy(strategy GradingStrategy) {
-	g.Strategy = strategy
+func NewGradingContext() *GradingContext {
+	return &GradingContext{
+		strategyMap: make(map[model.QuestionType]IGradingStrategy),
+	}
+}
+func (g *GradingContext) Grade(questionType model.QuestionType, submissionID uint) (float64, error) {
+	return g.strategyMap[questionType].Grade(submissionID)
 }
 
-func (g *GradingContext) Grade(submissionID uint) (float64, error) {
-	return g.Strategy.Grade(submissionID)
+func (g *GradingContext) AddGradingStrategy(questionType model.QuestionType, strategy IGradingStrategy) {
+	g.strategyMap[questionType] = strategy
+}
+
+func (g *GradingContext) GradeAll(submissionID uint) (float64, error) {
+	var totalScore float64
+	for _, v := range g.strategyMap {
+		score, err := v.Grade(submissionID)
+		if err != nil {
+			return 0, err
+		}
+		totalScore += score
+	}
+
+	return totalScore, nil
 }
--- ModEd/eval/controller/SubmissionController.go
+++ ModEd/eval/controller/SubmissionController.go
@@ -11,44 +11,34 @@ import (
 type SubmissionController struct {
 	db *gorm.DB
 	*core.BaseController[*model.AnswerSubmission]
-	MCAnsSubController *MultipleChoiceAnswerSubmissionController
-	TFAnsSubController *TrueFalseAnswerSubmissionController
-	SAAnsSubController *ShortAnswerSubmissionController
+	GradingContext *GradingContext
 }
 
 func NewSubmissionController(db *gorm.DB) *SubmissionController {
+	context := NewGradingContext()
+	context.AddGradingStrategy(model.MultipleChoiceQuestion, NewMultipleChoiceAnswerSubmissionController(db))
+	context.AddGradingStrategy(model.TrueFalseQuestion, NewTrueFalseAnswerSubmissionController(db))
+	context.AddGradingStrategy(model.ShortAnswerQuestion, NewShortAnswerSubmissionController(db))
 	return &SubmissionController{
-		db:                 db,
-		BaseController:     core.NewBaseController[*model.AnswerSubmission](db),
-		MCAnsSubController: NewMultipleChoiceAnswerSubmissionController(db),
-		TFAnsSubController: NewTrueFalseAnswerSubmissionController(db),
-		SAAnsSubController: NewShortAnswerSubmissionController(db),
+		db:             db,
+		BaseController: core.NewBaseController[*model.AnswerSubmission](db),
+		GradingContext: context,
 	}
 }
 func (c *SubmissionController) GradingSubmission(submissionID uint) (*model.AnswerSubmission, error) {
-	var totalScore float64
-
-	context := &GradingContext{}
-
-	strategies := []GradingStrategy{
-		c.MCAnsSubController,
-		c.TFAnsSubController,
-		c.SAAnsSubController,
-	}
-
-	for _, strategy := range strategies {
-		context.SetStrategy(strategy)
-		score, err := context.Grade(submissionID)
-		if err != nil {
-			return nil, err
-		}
-		totalScore += score
-	}
 
 	submission, err := c.BaseController.RetrieveByCondition(map[string]interface{}{"id": submissionID})
+
 	if err != nil {
 		return nil, err
 	}
+
+	totalScore, err := c.GradingContext.GradeAll(submissionID)
+
+	if err != nil {
+		return nil, err
+	}
+
 	submission.Score = totalScore
 	if err := c.BaseController.UpdateByCondition(map[string]interface{}{"id": submissionID}, submission); err != nil {
 		return nil, err
