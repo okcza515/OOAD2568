d75710cd7b576297e309e3300dbf48e0a6898ba2
ITKmz <ittikorn.mutu@kmutt.ac.th>
2025-05-13 10:41:53 +0700

    feat: Add validation rules to all model

--- ModEd/hr/model/RequestLeaveInstructor.go
+++ ModEd/hr/model/RequestLeaveInstructor.go
@@ -1,6 +1,16 @@
 package model
 
+import "github.com/go-playground/validator/v10"
+
 type RequestLeaveInstructor struct {
 	BaseLeaveRequest
-	InstructorCode string `gorm:"not null"`
+	InstructorCode string `gorm:"not null" validate:"required"`
+}
+
+func (requestLeaveInstructor RequestLeaveInstructor) Validate() error {
+	validate := validator.New()
+	if err := validate.Struct(requestLeaveInstructor); err != nil {
+		return err
+	}
+	return nil
 }
--- ModEd/hr/model/RequestResignationStudent.go
+++ ModEd/hr/model/RequestResignationStudent.go
@@ -1,6 +1,16 @@
 package model
 
+import "github.com/go-playground/validator/v10"
+
 type RequestResignationStudent struct {
 	BaseStandardRequest
-	StudentCode string `gorm:"type:text;default:'';not null"`
+	StudentCode string `gorm:"type:text;default:'';not null" validate:"required"`
+}
+
+func (requestResignationStudent RequestResignationStudent) Validate() error {
+	validate := validator.New()
+	if err := validate.Struct(requestResignationStudent); err != nil {
+		return err
+	}
+	return nil
 }
